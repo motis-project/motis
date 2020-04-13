c7e133e34d0fb97bc451629d9db013c2dd696076
Felix Gündling <felixguendling@gmail.com>
2020-04-14 00:14:11 +0200

    run pkg every time

--- cmake/pkg.cmake
+++ cmake/pkg.cmake
@@ -1,32 +1,30 @@
-if(NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/deps")
-  set(pkg-bin "${CMAKE_BINARY_DIR}/dl/pkg")
-  if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-    set(pkg-url "pkg")
-  elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-    set(pkg-url "pkg.exe")
-  else()
-    message(STATUS "Not downloading pkg tool. Using pkg from PATH.")
-    set(pkg-bin "pkg")
-  endif()
-
-  if (pkg-url)
-    if (NOT EXISTS ${pkg-bin})
-      message(STATUS "Downloading pkg binary from https://github.com/motis-project/pkg/releases/latest/download/${pkg-url}")
-      file(DOWNLOAD "https://github.com/motis-project/pkg/releases/latest/download/${pkg-url}" ${pkg-bin})
-      if (UNIX)
-        execute_process(COMMAND chmod +x ${pkg-bin})
-      endif()
-    else()
-      message(STATUS "Pkg binary located in project.")
-    endif()
-  endif()
-
-  message(STATUS "${pkg-bin} -l -h")
-  execute_process(
-    COMMAND ${pkg-bin} -l -h
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  )
+set(pkg-bin "${CMAKE_BINARY_DIR}/dl/pkg")
+if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  set(pkg-url "pkg")
+elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  set(pkg-url "pkg.exe")
+else()
+  message(STATUS "Not downloading pkg tool. Using pkg from PATH.")
+  set(pkg-bin "pkg")
 endif()
+
+if (pkg-url)
+  if (NOT EXISTS ${pkg-bin})
+    message(STATUS "Downloading pkg binary from https://github.com/motis-project/pkg/releases/latest/download/${pkg-url}")
+    file(DOWNLOAD "https://github.com/motis-project/pkg/releases/latest/download/${pkg-url}" ${pkg-bin})
+    if (UNIX)
+      execute_process(COMMAND chmod +x ${pkg-bin})
+    endif()
+  else()
+    message(STATUS "Pkg binary located in project.")
+  endif()
+endif()
+
+message(STATUS "${pkg-bin} -l -h")
+execute_process(
+  COMMAND ${pkg-bin} -l -h
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+)
 
 if (IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/deps")
   add_subdirectory(deps)
