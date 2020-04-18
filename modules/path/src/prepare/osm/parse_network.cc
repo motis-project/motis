b164bc16a276d23abf9ec88c162537f95bd9d0d7
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-04-18 14:55:55 +0200

    path: apparently light_rail and narrow_gauge do exist

--- modules/path/src/prepare/osm/parse_network.cc
+++ modules/path/src/prepare/osm/parse_network.cc
@@ -74,14 +74,16 @@ std::vector<osm_way> parse_network(std::string const& osm_file,
 }
 
 std::vector<osm_way> parse_rail(std::string const& osm_file) {
-  std::string rail{"rail"};
+  std::vector<std::string> included_railway{"rail", "light_rail", "narrow_gauge"};
   std::string yes{"yes"};
   std::vector<std::string> excluded_usages{"industrial", "military", "test",
                                            "tourism"};
   std::vector<std::string> excluded_services{"yard", "spur"};  // , "siding"
 
   return parse_network(osm_file, [&](auto&& way) {
-    if (rail != way.get_value_by_key("railway", "")) {
+    auto const railway = way.get_value_by_key("railway", "");
+    if (std::none_of(begin(included_railway), end(included_railway),
+                     [&](auto&& r) { return r == railway; })) {
       return false;
     }
 
