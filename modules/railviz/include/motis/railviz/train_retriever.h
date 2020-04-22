9f9c6163a36fbabefc1952c95b87c6857f8716d7
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-04-22 09:33:34 +0200

    railviz: update edge_geo_index on rt update (and refactor to more sane code)

--- modules/railviz/include/motis/railviz/train_retriever.h
+++ modules/railviz/include/motis/railviz/train_retriever.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <shared_mutex>
 #include <vector>
 
 #include "motis/hash_map.h"
@@ -10,6 +11,7 @@
 #include "motis/railviz/geo.h"
 
 #include "motis/protocol/PathBoxesResponse_generated.h"
+#include "motis/protocol/RtUpdate_generated.h"
 
 namespace motis {
 
@@ -21,19 +23,21 @@ struct edge_geo_index;
 
 struct train_retriever {
   train_retriever(schedule const& s,
-                  mcd::hash_map<std::pair<int, int>, geo::box> const&);
+                  mcd::hash_map<std::pair<int, int>, geo::box>);
   ~train_retriever();
 
   train_retriever(train_retriever const&) = delete;
   train_retriever& operator=(train_retriever const&) = delete;
 
-  train_retriever(train_retriever&&) = default;
-  train_retriever& operator=(train_retriever&&) = default;
+  train_retriever(train_retriever&&) = delete;
+  train_retriever& operator=(train_retriever&&) = delete;
 
+  void update(rt::RtUpdates const*);
   std::vector<ev_key> trains(time from, time to, unsigned max_count,
                              geo::box const& area);
 
 private:
+  std::shared_mutex mutable mutex_;
   std::vector<std::unique_ptr<edge_geo_index>> edge_index_;
 };
 
