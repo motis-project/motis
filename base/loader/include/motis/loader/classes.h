4f4851533ced478f54cc5bb987d43d3d7d23dba1
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-04-23 15:54:19 +0200

    loader: add missing classes from the switzerland dataset

--- base/loader/include/motis/loader/classes.h
+++ base/loader/include/motis/loader/classes.h
@@ -19,6 +19,7 @@ inline mcd::hash_map<mcd::string, uint8_t> class_mapping() {
     { "EC", 1 },
     { "IC", 1 },
     { "EX", 1 },
+    { "EXT", 1 },
     { "D", 1 },
     { "IR", 1},
     { "InterRegio", 1},
@@ -48,6 +49,7 @@ inline mcd::hash_map<mcd::string, uint8_t> class_mapping() {
     { "Os", 4 },
     { "Regionalzug", 4 },
     { "RZ", 4 },
+    { "CC", 4}, // narrow-gauge mountain train
 
     // metro
     { "S", 5 },
@@ -59,6 +61,7 @@ inline mcd::hash_map<mcd::string, uint8_t> class_mapping() {
     // subway
     { "U", 6 },
     { "STB", 6 },
+    { "M", 6 },  // subway in Lausanne
 
     // street-car
     { "Tram", 7 },
@@ -67,6 +70,12 @@ inline mcd::hash_map<mcd::string, uint8_t> class_mapping() {
 
     // bus
     { "Bus", 8 },
+    { "B", 8 },
+    { "BN", 8 },
+    { "BP", 8 },
+    { "CAR", 8 },
+    { "EXB", 8 }, // long-distance bus
+    { "KB", 8 },
 
     // other
     { "Flug", 9 },
@@ -74,6 +83,7 @@ inline mcd::hash_map<mcd::string, uint8_t> class_mapping() {
     { "ZahnR", 9 },
     { "Schw-B", 9 },
     { "Fähre", 9 },
+    { "BAT", 9 }, // "bateau"
     { "KAT", 9 },
     { "EZ", 9 },
     { "ALT", 9 },
@@ -81,10 +91,14 @@ inline mcd::hash_map<mcd::string, uint8_t> class_mapping() {
     { "RFB", 9 },
     { "RT", 9 },
     {"Drahtseilbahn", 9},
+    { "GB", 9 }, // ski lift?
     {"Standseilbahn", 9},
+    {"FUN", 9}, // "funicular"
+    {"GB", 9}, // "funicular"
     {"Sesselbahn", 9},
     {"Taxi", 9},
     {"Aufzug", 9},
+    {"ASC", 9}, // some elevator in Bern
     {"Schiff", 9}
   };
   // clang-format on
