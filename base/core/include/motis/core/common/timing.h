b12fe23e7af732fd9d8fb2088e8fb14cc3938016
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2020-05-22 17:40:25 +0200

    fix warning

--- base/core/include/motis/core/common/timing.h
+++ base/core/include/motis/core/common/timing.h
@@ -3,8 +3,8 @@
 #include <chrono>
 
 #define MOTIS_START_TIMING(_X) \
-  auto _X##_start = std::chrono::steady_clock::now(), _X##_stop = _X##_start
-#define MOTIS_STOP_TIMING(_X) _X##_stop = std::chrono::steady_clock::now()
+  auto _X##_start = std::chrono::steady_clock::now()
+#define MOTIS_STOP_TIMING(_X) auto _X##_stop = std::chrono::steady_clock::now()
 #define MOTIS_TIMING_MS(_X)                                          \
   (std::chrono::duration_cast<std::chrono::milliseconds>(_X##_stop - \
                                                          _X##_start) \
