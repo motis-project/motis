0ff123f3bf579dbb6677836cf0d3c39e694b1bc3
Pablo Hoch <pablo@melaxis.com>
2020-10-23 15:10:57 +0200

    paxforecast: faster behavior simulation (once per cpg, parallel)

--- modules/paxforecast/include/motis/paxforecast/simulate_behavior.h
+++ modules/paxforecast/include/motis/paxforecast/simulate_behavior.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <map>
+#include <mutex>
 
 #include "utl/zip.h"
 
 #include "motis/core/schedule/schedule.h"
+
+#include "motis/module/context/motis_parallel_for.h"
 
 #include "motis/paxmon/graph_access.h"
 #include "motis/paxmon/paxmon_data.h"
@@ -37,31 +40,26 @@ inline simulation_result simulate_behavior(
                       });
       };
 
-  auto const simulate_group =
-      [&](motis::paxmon::passenger_group const& grp,
-          std::vector<alternative> const& alternatives,
-          motis::paxmon::passenger_localization const& localization) {
-        auto const allocation =
-            pb.pick_routes(grp, alternatives, announcements);
-        auto& group_result = result.group_results_[&grp];
-        group_result.localization_ = &localization;
+  std::mutex result_mutex;
+  motis_parallel_for(combined_groups, [&](auto const& cpgs) {
+    for (auto const& cpg : cpgs.second) {
+      auto const allocation = pb.pick_routes(*cpg.groups_.front(),
+                                             cpg.alternatives_, announcements);
+      auto guard = std::lock_guard{result_mutex};
+      for (auto const& grp : cpg.groups_) {
+        auto& group_result = result.group_results_[grp];
+        group_result.localization_ = &cpg.localization_;
         for (auto const& [alt, probability] :
-             utl::zip(alternatives, allocation)) {
+             utl::zip(cpg.alternatives_, allocation)) {
           group_result.alternatives_.emplace_back(&alt, probability);
           if (probability == 0.0) {
             continue;
           }
-          add_group_to_alternative(grp, alt, probability);
+          add_group_to_alternative(*grp, alt, probability);
         }
-      };
-
-  for (auto const& cpgs : combined_groups) {
-    for (auto const& cpg : cpgs.second) {
-      for (auto const& grp : cpg.groups_) {
-        simulate_group(*grp, cpg.alternatives_, cpg.localization_);
       }
     }
-  }
+  });
 
   return result;
 }
