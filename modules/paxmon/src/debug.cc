b271531bd22f5c48408af9dc04c5a775ba2c67a9
Pablo Hoch <pablo@melaxis.com>
2021-03-18 13:37:48 +0100

    paxmon: debug output improvements

--- modules/paxmon/src/debug.cc
+++ modules/paxmon/src/debug.cc
@@ -8,23 +8,23 @@ namespace motis::paxmon {
 
 void print_trip(schedule const& sched, trip const* trp) {
   fmt::print(
-      "    trip: {:7} / {:10} / {:5} / {:7} / {:10} / {}\n",
+      "    trip: {:7} / {:10} / {:5} / {:7} / {:10} / {} [ptr={}, debug={}]\n",
       sched.stations_.at(trp->id_.primary_.get_station_id())->eva_nr_.view(),
       format_time(trp->id_.primary_.get_time()), trp->id_.primary_.train_nr_,
       sched.stations_.at(trp->id_.secondary_.target_station_id_)
           ->eva_nr_.view(),
       format_time(trp->id_.secondary_.target_time_),
-      trp->id_.secondary_.line_id_);
+      trp->id_.secondary_.line_id_, static_cast<void const*>(trp),
+      trp->dbg_.str());
 }
 
 void print_leg(schedule const& sched, journey_leg const& leg) {
-  auto const enter_station_name =
-      sched.stations_.at(leg.enter_station_id_)->name_.str();
-  auto const exit_station_name =
-      sched.stations_.at(leg.exit_station_id_)->name_.str();
-  fmt::print("  {:7} {:50} -> {:7} {:50}", format_time(leg.enter_time_),
-             enter_station_name, format_time(leg.exit_time_),
-             exit_station_name);
+  auto const& enter_station = sched.stations_.at(leg.enter_station_id_);
+  auto const& exit_station = sched.stations_.at(leg.exit_station_id_);
+  fmt::print("  {:7} {:7} {:50} -> {:7} {:7} {:50}",
+             format_time(leg.enter_time_), enter_station->eva_nr_.str(),
+             enter_station->name_.str(), format_time(leg.exit_time_),
+             exit_station->eva_nr_.str(), exit_station->name_.str());
   if (leg.enter_transfer_) {
     fmt::print("  enter_transfer={:2} {}\n", leg.enter_transfer_->duration_,
                leg.enter_transfer_->type_ == transfer_info::type::SAME_STATION
@@ -40,15 +40,19 @@ void print_trip_section(schedule const& sched,
                         motis::access::trip_section const& ts) {
   auto const cur_dep_time = ts.ev_key_from().get_time();
   auto const sched_dep_time = get_schedule_time(sched, ts.ev_key_from());
-  auto const from_station_name = ts.from_station(sched).name_.str();
+  auto const& from_station = ts.from_station(sched);
   auto const cur_arr_time = ts.ev_key_to().get_time();
   auto const sched_arr_time = get_schedule_time(sched, ts.ev_key_to());
-  auto const to_station_name = ts.to_station(sched).name_.str();
+  auto const& to_station = ts.to_station(sched);
   auto const& merged_trips = sched.merged_trips_.at(ts.lcon().trips_);
-  fmt::print("  {:7} [{:7}] {:50} -> {:7} [{:7}] {:50}",
-             format_time(cur_dep_time), format_time(sched_dep_time),
-             from_station_name, format_time(cur_arr_time),
-             format_time(sched_arr_time), to_station_name);
+  fmt::print(
+      "  {:7} [{:7}] {:7} {:50} -> {:7} [{:7}] {:7} {:50} [lc={}, "
+      "merged_trips={}]",
+      format_time(cur_dep_time), format_time(sched_dep_time),
+      from_station.eva_nr_.str(), from_station.name_.str(),
+      format_time(cur_arr_time), format_time(sched_arr_time),
+      to_station.eva_nr_.str(), to_station.name_.str(),
+      static_cast<void const*>(&ts.lcon()), ts.lcon().trips_);
   if (merged_trips->size() > 1) {
     fmt::print(" [{} trips]\n", merged_trips->size());
   } else {
@@ -59,15 +63,16 @@ void print_trip_section(schedule const& sched,
 void print_trip_edge(schedule const& sched, graph const& g, edge const* e) {
   auto const cur_dep_time = e->from(g)->current_time();
   auto const cur_sched_time = e->from(g)->schedule_time();
-  auto const from_station_name = e->from(g)->get_station(sched).name_.str();
+  auto const& from_station = e->from(g)->get_station(sched);
   auto const cur_arr_time = e->to(g)->current_time();
   auto const sched_arr_time = e->to(g)->schedule_time();
-  auto const to_station_name = e->to(g)->get_station(sched).name_.str();
+  auto const& to_station = e->to(g)->get_station(sched);
   auto const& merged_trips = sched.merged_trips_.at(e->get_merged_trips_idx());
-  fmt::print("  {:7} [{:7}] {:50} -> {:7} [{:7}] {:50}",
+  fmt::print("  {:7} [{:7}] {:7} {:50} -> {:7} [{:7}] {:7} {:50}",
              format_time(cur_dep_time), format_time(cur_sched_time),
-             from_station_name, format_time(cur_arr_time),
-             format_time(sched_arr_time), to_station_name);
+             from_station.eva_nr_.str(), from_station.name_.str(),
+             format_time(cur_arr_time), format_time(sched_arr_time),
+             to_station.eva_nr_.str(), to_station.name_.str());
   if (merged_trips->size() > 1) {
     fmt::print(" [{} trips]\n", merged_trips->size());
   } else {
