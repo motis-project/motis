150922822813ad279810477edbe2a6f51e99fe29
Pablo Hoch <pablo@melaxis.com>
2021-05-20 13:46:24 +0200

    clang-tidy

--- modules/paxforecast/include/motis/paxforecast/paxforecast.h
+++ modules/paxforecast/include/motis/paxforecast/paxforecast.h
@@ -45,7 +45,7 @@ private:
   bool deterministic_mode_{false};
   duration min_delay_improvement_{5};
 
-  std::string stats_file_{};
+  std::string stats_file_;
   std::unique_ptr<stats_writer> stats_writer_;
 };
 
