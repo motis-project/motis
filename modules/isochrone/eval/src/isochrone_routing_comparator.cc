cb0911500a986bbd65043855fb0069682c14a127
dgcl <dahitgcl@yahoo.de>
2021-06-19 02:28:15 +0200

    Comparator new file

---
+++ modules/isochrone/eval/src/isochrone_routing_comparator.cc
@@ -0,0 +1,6 @@
+//TODO: Generate routing requests from isochrone response, compare arrival times of isochrone responses and routing responses
+
+int main(int argc, char const** argv) {
+  return 0;
+};
+
