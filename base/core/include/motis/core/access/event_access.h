1e2f4f9453562af6493fc675a535c380d3ad4bc7
Pablo Hoch <pablo@melaxis.com>
2021-08-10 12:50:38 +0200

    rt: propagate delays along through edges

--- base/core/include/motis/core/access/event_access.h
+++ base/core/include/motis/core/access/event_access.h
@@ -9,6 +9,12 @@ void for_each_departure(ev_key const& arr, Fn fn) {
   for (auto const& e : arr.route_edge_->to_->edges_) {
     if (!e.empty()) {
       fn(ev_key{&e, arr.lcon_idx_, event_type::DEP});
+    } else if (e.type() == edge::THROUGH_EDGE) {
+      for (auto const& ne : e.to_->edges_) {
+        if (!ne.empty()) {
+          fn(ev_key{&ne, arr.lcon_idx_, event_type::DEP});
+        }
+      }
     }
   }
 }
@@ -18,6 +24,12 @@ void for_each_arrival(ev_key const& dep, Fn fn) {
   for (auto const& e : dep.route_edge_->from_->incoming_edges_) {
     if (!e->empty()) {
       fn(ev_key{trip::route_edge{e}, dep.lcon_idx_, event_type::ARR});
+    } else if (e->type() == edge::THROUGH_EDGE) {
+      for (auto const& pe : e->from_->incoming_edges_) {
+        if (!pe->empty()) {
+          fn(ev_key{trip::route_edge{pe}, dep.lcon_idx_, event_type::ARR});
+        }
+      }
     }
   }
 }
