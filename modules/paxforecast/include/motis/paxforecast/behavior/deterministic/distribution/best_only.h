e0f3c6280fe33361c804c353a9ef33bd57ff757b
Pablo Hoch <pablo@melaxis.com>
2021-08-13 15:33:35 +0200

    simplify code

--- modules/paxforecast/include/motis/paxforecast/behavior/deterministic/distribution/best_only.h
+++ modules/paxforecast/include/motis/paxforecast/behavior/deterministic/distribution/best_only.h
@@ -1,27 +1,18 @@
 #pragma once
 
-#include <limits>
-#include <numeric>
+#include <algorithm>
+#include <iterator>
 #include <vector>
-
-#include "utl/enumerate.h"
 
 namespace motis::paxforecast::behavior::deterministic::distribution {
 
 struct best_only {
   static std::vector<float> get_probabilities(
       std::vector<double> const& scores) {
-    auto best_idx = 0ULL;
-    auto best_score = std::numeric_limits<double>::max();
-    for (auto const [idx, score] : utl::enumerate(scores)) {
-      if (score < best_score) {
-        best_idx = idx;
-        best_score = score;
-      }
-    }
     std::vector<float> result;
     result.resize(scores.size());
-    result[best_idx] = 1.0F;
+    result[std::distance(begin(scores),
+                         std::min_element(begin(scores), end(scores)))] = 1.0F;
     return result;
   }
 };
