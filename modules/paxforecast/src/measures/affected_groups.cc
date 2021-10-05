a1e5c7656d0d693aaee3914acf5bf77b58776f7a
Pablo Hoch <pablo@melaxis.com>
2021-10-05 11:51:37 +0200

    paxforecast: trip recommendation measures: filter by destination

--- modules/paxforecast/src/measures/affected_groups.cc
+++ modules/paxforecast/src/measures/affected_groups.cc
@@ -40,20 +40,31 @@ passenger_localization localize(schedule const& sched, universe& uv,
   }
 }
 
+template <typename CheckFn>
 void add_affected_groups(schedule const& sched, universe& uv,
                          affected_groups_info& result, recipients const& rcpts,
-                         time const loc_time, measure_variant const* m) {
+                         time const loc_time, measure_variant const* m,
+                         CheckFn const& check_fn) {
   // TODO(pablo): performance optimizations - currently localizing every group
   for (auto const* pg : uv.passenger_groups_) {
     if (pg == nullptr || !pg->valid() || pg->probability_ == 0.0F) {
       continue;
     }
     auto const loc = localize(sched, uv, result, pg, loc_time);
-    if (is_recipient(sched, uv, *pg, loc, rcpts)) {
+    if (is_recipient(sched, uv, *pg, loc, rcpts) && check_fn(pg)) {
       result.localization_[pg] = loc;
       result.measures_[pg].emplace_back(m);
     }
   }
+}
+
+bool matches_destination(passenger_group const* pg,
+                         trip_recommendation const& m) {
+  // TODO(pablo): check planned_trips
+  return !pg->compact_planned_journey_.legs_.empty() &&
+         std::find(begin(m.planned_destinations_), end(m.planned_destinations_),
+                   pg->compact_planned_journey_.destination_station_id()) !=
+             end(m.planned_destinations_);
 }
 
 affected_groups_info get_affected_groups(schedule const& sched, universe& uv,
@@ -63,12 +74,16 @@ affected_groups_info get_affected_groups(schedule const& sched, universe& uv,
   for (auto const& mv : measures) {
     std::visit(
         utl::overloaded{[&](trip_recommendation const& m) {
-                          add_affected_groups(sched, uv, result, m.recipients_,
-                                              loc_time, &mv);
+                          add_affected_groups(
+                              sched, uv, result, m.recipients_, loc_time, &mv,
+                              [&m](passenger_group const* pg) {
+                                return matches_destination(pg, m);
+                              });
                         },
                         [&](trip_load_information const& m) {
-                          add_affected_groups(sched, uv, result, m.recipients_,
-                                              loc_time, &mv);
+                          add_affected_groups(
+                              sched, uv, result, m.recipients_, loc_time, &mv,
+                              [](passenger_group const*) { return true; });
                         }},
         mv);
   }
