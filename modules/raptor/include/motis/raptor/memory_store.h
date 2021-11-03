7814b42091de4feba379178c527b726effeb6e06
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2021-11-03 17:14:16 +0100

    finish memory store

--- modules/raptor/include/motis/raptor/memory_store.h
+++ modules/raptor/include/motis/raptor/memory_store.h
@@ -1,42 +1,90 @@
 #pragma once
 
 #include <atomic>
+#include <mutex>
 #include <thread>
 
 #include "cuda_runtime.h"
 
 #include "utl/verify.h"
+#include "utl/zip.h"
 
 #include "motis/raptor/raptor_result.h"
 
 namespace motis::raptor {
 
+using device_id = int32_t;
+
+inline auto get_launch_paramters(cudaDeviceProp const& prop,
+                                 int32_t const concurrency_per_device) {
+  int32_t block_dim_x = 32;  // must always be 32!
+  int32_t block_dim_y = 32;  // range [1, ..., 32]
+  int32_t block_size = block_dim_x * block_dim_y;
+  int32_t max_blocks_per_sm = prop.maxThreadsPerMultiProcessor / block_size;
+
+  auto const mp_count = prop.multiProcessorCount / concurrency_per_device;
+
+  int32_t num_blocks = mp_count * max_blocks_per_sm;
+
+  dim3 threads_per_block(block_dim_x, block_dim_y, 1);
+  dim3 grid(num_blocks, 1, 1);
+
+  return std::make_tuple(threads_per_block, grid);
+}
+
 struct device_context {
-  int32_t id_;
-  cudaDeviceProp props_;
+  device_context() = delete;
+  device_context(device_id const device_id,
+                 int32_t const concurrency_per_device)
+      : id_(device_id) {
+    cudaSetDevice(id_);
+    cc();
+
+    cudaGetDeviceProperties(&props_, device_id);
+    cc();
+
+    std::tie(threads_per_block_, grid_) =
+        get_launch_paramters(props_, concurrency_per_device);
+
+    cudaStreamCreate(&proc_stream_);
+    cc();
+    cudaStreamCreate(&transfer_stream_);
+    cc();
+  }
+
+  void destroy() {
+    cudaSetDevice(id_);
+    cudaStreamDestroy(proc_stream_);
+    cudaStreamDestroy(transfer_stream_);
+    cc();
+  }
+
+  device_id id_{};
+  cudaDeviceProp props_{};
 
   dim3 threads_per_block_;
   dim3 grid_;
 
-  cudaStream_t proc_stream_;
-  cudaStream_t transfer_stream_;
+  cudaStream_t proc_stream_{};
+  cudaStream_t transfer_stream_{};
 };
 
 struct host_memory {
   host_memory() = delete;
   explicit host_memory(stop_id const stop_count) {
     cudaMallocHost(&any_station_marked_, sizeof(bool));
-    *any_station_marked_ = false;
 
     result_ = new raptor_result_pinned(stop_count);
+
+    *any_station_marked_ = false;
   }
 
-  void free() {
-    cudaFree(any_station_marked_);
+  void destroy() {
+    cudaFreeHost(any_station_marked_);
     delete result_;
   }
 
-  void reset() {
+  void reset() const {
     *any_station_marked_ = false;
     result_->reset();
   }
@@ -47,7 +95,6 @@ struct host_memory {
 
 struct device_memory {
   device_memory() = delete;
-
   device_memory(stop_id const stop_count, route_id const route_count)
       : stop_count_(stop_count), route_count_(route_count) {
 
@@ -60,16 +107,19 @@ struct device_memory {
     cudaMalloc(&station_marks_, get_station_mark_bytes());
     cudaMalloc(&route_marks_, get_route_mark_bytes());
     cudaMalloc(&any_station_marked_, sizeof(bool));
+    cc();
+
+    this->reset_async(nullptr);
   }
 
   size_t get_result_bytes() const {
     return stop_count_ * sizeof(time) * max_raptor_round;
   }
   size_t get_station_mark_bytes() const { return ((stop_count_ / 32) + 1) * 4; }
-  size_t get_route_mark_bytes() const { return ((stop_count_ / 32) + 1) * 4; }
+  size_t get_route_mark_bytes() const { return ((route_count_ / 32) + 1) * 4; }
   size_t get_scratchpad_bytes() const { return stop_count_ * sizeof(time); }
 
-  void free() {
+  void destroy() {
     cudaFree(result_.front());
     cudaFree(footpaths_scratchpad_);
     cudaFree(station_marks_);
@@ -77,7 +127,7 @@ struct device_memory {
     cudaFree(any_station_marked_);
   }
 
-  void reset_async(cudaStream_t s) {
+  void reset_async(cudaStream_t s) const {
     cudaMemsetAsync(result_.front(), 0xFF, get_result_bytes(), s);
     cudaMemsetAsync(footpaths_scratchpad_, 0xFF, get_scratchpad_bytes(), s);
     cudaMemsetAsync(station_marks_, 0, get_station_mark_bytes(), s);
@@ -86,113 +136,84 @@ struct device_memory {
   }
 
   //  device_gpu_timetable d_gtt_;
-  stop_id stop_count_;
-  route_id route_count_;
+  stop_id stop_count_{invalid<stop_id>};
+  route_id route_count_{invalid<route_id>};
 
-  device_result result_;
+  device_result result_{};
 
   // TODO(julian) move from uint32_t to char or something
-  uint32_t* route_marks_;
-  uint32_t* station_marks_;
-  bool* any_station_marked_;
-  time* footpaths_scratchpad_;
+  uint32_t* route_marks_{};
+  uint32_t* station_marks_{};
+  bool* any_station_marked_{};
+  time* footpaths_scratchpad_{};
 };
 
-auto get_launch_paramters(cudaDeviceProp const& prop,
-                          int32_t const concurrency_per_device) {
-  int32_t block_dim_x = 32;  // must always be 32!
-  int32_t block_dim_y = 32;  // range [1, ..., 32]
-  int32_t block_size = block_dim_x * block_dim_y;
-  int32_t max_blocks_per_sm = prop.maxThreadsPerMultiProcessor / block_size;
+struct mem {
+  mem() = delete;
+  mem(stop_id const stop_count, route_id const route_count,
+      device_id const device_id, int32_t const concurrency_per_device)
+      : host_(stop_count),
+        device_(stop_count, route_count),
+        context_(device_id, concurrency_per_device) {}
 
-  auto const mp_count =
-      std::min(concurrency_per_device, prop.multiProcessorCount);
-
-  int32_t num_blocks = mp_count * max_blocks_per_sm;
-
-  dim3 threads_per_block(block_dim_x, block_dim_y, 1);
-  dim3 grid(num_blocks, 1, 1);
-
-  return std::make_tuple(threads_per_block, grid);
-}
-
-std::vector<device_context> get_device_contexts(
-    int32_t concurrency_per_device) {
-  std::vector<device_context> contexts;
-
-  int device_count = 0;
-  cudaGetDeviceCount(&device_count);
-  cc();
-
-  for (int device_id = 0; device_id < device_count; ++device_id) {
-    cudaSetDevice(device_id);
-    cc();
-
-    cudaDeviceProp prop{};
-    cudaGetDeviceProperties(&prop, device_id);
-    cc();
-
-    auto const& [threads_per_block, grid] = get_launch_paramters(
-        prop, static_cast<int32_t>(concurrency_per_device));
-
-    for (auto i = 0; i < concurrency_per_device; ++i) {
-      device_context dc;
-
-      dc.id_ = device_id;
-      dc.props_ = prop;
-
-      dc.threads_per_block_ = threads_per_block;
-      dc.grid_ = grid;
-
-      cudaStreamCreate(&dc.proc_stream_);
-      cc();
-      cudaStreamCreate(&dc.transfer_stream_);
-      cc();
-    }
+  ~mem() {
+    host_.destroy();
+    device_.destroy();
+    context_.destroy();
   }
 
-  return contexts;
-}
+  host_memory host_;
+  device_memory device_;
+  device_context context_;
+};
 
 struct memory_store {
   using mem_idx = uint32_t;
   static_assert(std::is_unsigned_v<mem_idx>);
 
-  void init(raptor_timetable const& tt, uint32_t const concurrency_per_device) {
+  void init(raptor_timetable const& tt, int32_t const concurrency_per_device) {
     int32_t device_count = 0;
     cudaGetDeviceCount(&device_count);
 
-    concurrency_per_device_ = concurrency_per_device;
-    max_concurrency_ = concurrency_per_device_ * device_count;
+    memory_.reserve(device_count * concurrency_per_device);
+    for (auto device_id = 0; device_id < device_count; ++device_id) {
+      for (auto i = 0; i < concurrency_per_device; ++i) {
+        memory_.emplace_back(tt.stop_count(), tt.route_count(), device_id,
+                             concurrency_per_device);
+      }
+    }
 
-    device_ = std::vector<device_memory>(
-        max_concurrency_, device_memory(tt.stop_count(), tt.route_count()));
-    host_ = std::vector<host_memory>(max_concurrency_,
-                                     host_memory(tt.stop_count()));
-
-    context_ =
-        get_device_contexts(static_cast<int32_t>(concurrency_per_device_));
-
-    utl::verify(device_.size() == host_.size(),
-                "Memory stores do not have the same size!");
-    utl::verify(host_.size() == context_.size(),
-                "Memory stores do not have the same size!");
+    memory_mutexes_ = std::vector<std::mutex>(memory_.size());
   }
 
-  mem_idx get_mem_idx() { return current_idx_.fetch_add(1) % max_concurrency_; }
-
-  mem_idx max_concurrency_{0};
-  mem_idx concurrency_per_device_{0};
+  mem_idx get_mem_idx() { return current_idx_.fetch_add(1) % memory_.size(); }
 
   std::atomic<mem_idx> current_idx_{0};
   static_assert(std::is_unsigned_v<decltype(current_idx_)::value_type>);
 
-  std::vector<device_memory> device_;
-  std::vector<host_memory> host_;
-  std::vector<device_context> context_;
+  std::vector<mem> memory_;
+  std::vector<std::mutex> memory_mutexes_;
 };
 
 static_assert(
     std::is_unsigned_v<decltype(std::declval<memory_store>().get_mem_idx())>);
+
+struct loaned_mem {
+  loaned_mem() = default;
+  explicit loaned_mem(memory_store& store) {
+    auto const idx = store.get_mem_idx();
+    lock_ = std::unique_lock(store.memory_mutexes_[idx]);
+    mem_ = &store.memory_[idx];
+  }
+
+  ~loaned_mem() {
+    mem_->device_.reset_async(mem_->context_.proc_stream_);
+    mem_->host_.reset();
+    cuda_sync_stream(mem_->context_.proc_stream_);
+  }
+
+  mem* mem_{nullptr};
+  std::unique_lock<std::mutex> lock_{};
+};
 
 }  // namespace motis::raptor
