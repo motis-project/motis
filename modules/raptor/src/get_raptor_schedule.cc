1c7cfb993e614ab0deda80c1d427082c575e15b3
Felix Gündling <felix.guendling@gmail.com>
2021-11-08 15:25:21 +0100

    raptor: get_raptor_schedule refactor
    move intermediate data structures to cc file

--- modules/raptor/src/get_raptor_schedule.cc
+++ modules/raptor/src/get_raptor_schedule.cc
@@ -16,6 +16,61 @@ namespace motis::raptor {
 
 namespace log = motis::logging;
 
+struct transformable_footpath {
+  transformable_footpath(stop_id const from, stop_id const to,
+                         time const duration)
+      : from_(from), to_(to), duration_(duration) {}
+
+  stop_id from_;
+  stop_id to_;
+  time duration_;
+};
+
+struct transformable_stop {
+  std::vector<transformable_footpath> footpaths_;
+  std::vector<transformable_footpath> incoming_footpaths_;
+  std::vector<route_id> stop_routes_;
+  std::vector<stop_id> equivalent_;
+  time transfer_time_{invalid<time>};
+  std::string eva_{};
+  unsigned motis_station_index_{invalid<unsigned>};
+};
+
+struct raptor_lcon {
+  raptor_lcon(stop_id const from, stop_id const to, time const dep,
+              time const arrival, bool const in_allowed, bool const out_allowed,
+              light_connection const* lc)
+      : from_(from),
+        to_(to),
+        departure_(dep),
+        arrival_(arrival),
+        in_allowed_(in_allowed),
+        out_allowed_(out_allowed),
+        lcon_(lc) {}
+  stop_id from_;
+  stop_id to_;
+  time departure_;
+  time arrival_;
+  bool in_allowed_;
+  bool out_allowed_;
+  light_connection const* lcon_;
+};
+
+struct transformable_trip {
+  std::vector<raptor_lcon> lcons_;
+  std::vector<stop_time> stop_times_;
+};
+
+struct transformable_route {
+  std::vector<transformable_trip> trips_;
+  std::vector<stop_id> route_stops_;
+};
+
+struct transformable_timetable {
+  std::vector<transformable_stop> stations_;
+  std::vector<transformable_route> routes_;
+};
+
 std::vector<stop_time> get_stop_times_from_lcons(
     std::vector<raptor_lcon> const& lcons) {
   std::vector<stop_time> stop_times(lcons.size() + 1);
