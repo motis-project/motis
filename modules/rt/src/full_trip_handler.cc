39db5b6e48644e109235282941acf6b2bb1a664b
Pablo Hoch <pablo@melaxis.com>
2021-11-23 13:45:00 +0100

    rt: full trip messages: support platforms

--- modules/rt/src/full_trip_handler.cc
+++ modules/rt/src/full_trip_handler.cc
@@ -366,6 +366,10 @@ private:
       auto const con_info =
           get_con_info(sched_, con_infos_, ts->category()->code()->str(),
                        ts->line_id()->str(), ts->train_nr());
+      // NOTE: connections are always created with track=schedule_track first.
+      // if a different current_track is supplied, it will be updated in
+      // update_event (so that both schedule + current track information is
+      // stored).
       lc.full_con_ = get_full_con(sched_, con_info, sec.dep_.schedule_track_,
                                   sec.arr_.schedule_track_);
       lcs.emplace_back(lc);
@@ -427,11 +431,40 @@ private:
 
     for (auto i = 0ULL; i < sections.size(); ++i) {
       auto& sec = sections[i];  // NOLINT
+      auto const& lc = lcs.at(i);
+
+      // NOTE: at this point, the connections still have track=schedule_track,
+      // but we connect the route nodes to the platform nodes for current_track.
+      // the track inside the connections is updated later in update_event.
+
       auto const from_route_node = route_nodes[i];  // NOLINT
+      auto const from_station_node = from_route_node->get_station();
+      auto const from_station = sched_.stations_[from_station_node->id_].get();
+      auto const from_platform =
+          from_station->get_platform(sec.dep_.current_track_);
+
       auto const to_route_node = route_nodes[i + 1];  // NOLINT
+      auto const to_station_node = to_route_node->get_station();
+      auto const to_station = sched_.stations_[to_route_node->id_].get();
+      auto const to_platform =
+          to_station->get_platform(sec.arr_.current_track_);
+
+      if (from_platform) {
+        auto const pn = add_platform_enter_edge(
+            sched_, from_route_node, from_station_node,
+            from_station->platform_transfer_time_, from_platform.value());
+        add_outgoing_edge(&pn->edges_.back(), incoming);
+      }
+
+      if (to_platform) {
+        add_platform_exit_edge(sched_, to_route_node, to_station_node,
+                               to_station->platform_transfer_time_,
+                               to_platform.value());
+        add_outgoing_edge(&to_route_node->edges_.back(), incoming);
+      }
 
       auto const& route_edge = from_route_node->edges_.emplace_back(
-          make_route_edge(from_route_node, to_route_node, {lcs.at(i)}));
+          make_route_edge(from_route_node, to_route_node, {lc}));
       add_outgoing_edge(&route_edge, incoming);
       trip_edges.emplace_back(&route_edge);
       constant_graph_add_route_edge(sched_, &route_edge);
@@ -592,19 +625,22 @@ private:
     }
   }
 
-  static bool is_same_route_stop(event_info const& a, event_info const& b) {
+  bool is_same_route_stop(event_info const& a, event_info const& b) {
+    auto const st = sched_.stations_[a.station_->id_].get();
     return a.station_ == b.station_ &&
-           a.interchange_allowed_ == b.interchange_allowed_;
+           a.interchange_allowed_ == b.interchange_allowed_ &&
+           st->get_platform(a.current_track_) ==
+               st->get_platform(b.current_track_);
   }
 
-  static bool is_same_route(std::vector<section> const& a,
-                            std::vector<section> const& b) {
+  bool is_same_route(std::vector<section> const& a,
+                     std::vector<section> const& b) {
     if (a.size() != b.size()) {
       return false;
     }
 
     return std::all_of(begin(utl::zip(a, b)), end(utl::zip(a, b)),
-                       [](auto const& tup) {
+                       [&](auto const& tup) {
                          auto const& [sa, sb] = tup;
                          return is_same_route_stop(sa.dep_, sb.dep_) &&
                                 is_same_route_stop(sa.arr_, sb.arr_);
