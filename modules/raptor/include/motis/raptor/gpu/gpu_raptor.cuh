fa2491066186a65b8adb04426206f2343ebf186c
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2021-11-30 13:55:23 +0100

    fix header

--- modules/raptor/include/motis/raptor/gpu/gpu_raptor.cuh
+++ modules/raptor/include/motis/raptor/gpu/gpu_raptor.cuh
@@ -37,7 +37,6 @@ __device__ void init_arrivals_dev(base_query const& query,
 __device__ void update_routes_dev(time const* prev_arrivals, time* arrivals,
                                   unsigned int* station_marks,
                                   unsigned int* route_marks,
-                                  bool* any_station_marked,
                                   device_gpu_timetable const& tt);
 
 __device__ void update_footpaths_dev(device_memory const& device_mem,
