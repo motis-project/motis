c2005b49ef4dba0978943ba1cd279ef17b09bb8e
Alexander Sinkovic <alexander.sinkovic@gmail.de>
2021-12-03 17:28:58 +0100

    codebeispiele

--- ui/web/pages/App/App.tsx
+++ ui/web/pages/App/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 
+import Maybe, { nothing } from 'true-myth/maybe';
+import DatePicker from 'sassy-datepicker';
+
 import { OverlayView } from '../Views/OverlayView';
 
+let visible = false;
+
+
 export const Main: React.FC = () => {
+    
+    let Model: Boolean = true;
+
+    const [showMap, setShowMap] = React.useState(false);
+
+    // Switch between Dark and Light Mode
+    const [darkMode, setDarkMode] = React.useState(true);
+
+    React.useEffect(() =>{
+        console.log("Hello World")
+        visible = !visible
+    }, [showMap])
+
+    React.useEffect(() =>{
+        Model = darkMode
+    }, [darkMode])
+    
     return(
         <div className='app'>
-            {//<MapView />
+            { visible && <MapView />
             }
-            <OverlayView />
+            <OverlayView 
+                model={darkMode} />
             {//<StationSearchView />
             }
             {//<SimTimePickerView />
