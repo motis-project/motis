4e4ac43602b9b84075176936d25e91385179c287
Oleksandr Havrylov <sashka.havr@gmail.com>
2021-12-06 15:29:48 +0100

    Fixed $router was not recognised by vscode #22

--- ui/web/src/main.ts
+++ ui/web/src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+var app = createApp(App)
+app.use(router)
+
+
+app.mount('#app')
--- ui/web/src/router/index.ts
+++ ui/web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import ConnectionSearch from "../views/ConnectionSearch.vue"
 import TrainSearch from "../views/TrainSearch.vue"
+import {Router} from "vue-router"
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/trips',
     components : {
+      overlay: ConnectionSearch,
       subOverlay: TrainSearch
     }
   }
@@ -21,5 +23,11 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+      $router: Router
+  }
+}
 
 export default router
