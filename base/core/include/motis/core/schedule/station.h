517e9b75f148c82f6e4d630040c32db247c29285
Pablo Hoch <pablo@melaxis.com>
2022-01-12 17:26:05 +0100

    add missing include

--- base/core/include/motis/core/schedule/station.h
+++ base/core/include/motis/core/schedule/station.h
@@ -12,6 +12,7 @@
 #include "motis/string.h"
 #include "motis/vector.h"
 
+#include "motis/core/schedule/connection.h"
 #include "motis/core/schedule/footpath.h"
 #include "motis/core/schedule/timezone.h"
 
