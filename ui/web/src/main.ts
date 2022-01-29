49f56096b492e01c490940e19aaa6c9b5ad39af0
Khaliakbarov, Rafkhad <rafhadh@gmail.com>
2022-01-29 22:48:23 +0600

    Created initial request #44
    Initial request created in the main file;
    The begin and end dates are accessible from $ds service;
    Separate field for the end date created in the Date Service;

--- ui/web/src/main.ts
+++ ui/web/src/main.ts
@@ -5,19 +5,27 @@ import DateTimeService from './services/DateTimeService';
 import MOTISPostService from './services/MOTISPostService';
 import TranslationService from './services/TranslationService';
 import store from './store';
+import Interval from './models/SmallTypes/Interval';
+import InitialScheduleInfoResponseContent from './models/InitRequestResponseContent';
 
 const app = createApp(App)
 app.use(store);
 app.use(TranslationService, "de-DE");
+app.use(MOTISPostService);
+
+let initTime: Interval = {begin: 0, end: 0};
+app.config.globalProperties.$postService.getInitialRequestScheduleInfo().then((resp: InitialScheduleInfoResponseContent) => {
+    initTime = {begin: resp.begin, end: resp.end};
+});
+
 const interval = setInterval(() => {
-    if (TranslationService.service !== null && TranslationService.service.isLoaded) {
+    if (TranslationService.service !== null && TranslationService.service.isLoaded && initTime.begin > 0) {
         app.use(router(TranslationService.service));
-        app.use(MOTISPostService);
+        const initDate = new Date(initTime.begin * 1000);
         const now = new Date();
-        const initialDateTime = new Date(2022, 0, 23, now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()).valueOf()
-        app.use(DateTimeService, initialDateTime);
+        const initialDateTime = new Date(initDate.getFullYear(), initDate.getMonth(), initDate.getDate(), now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()).valueOf();
+        app.use(DateTimeService, initialDateTime, initTime.end * 1000);
         app.mount('#app');
         clearInterval(interval);
     }
 }, 10);
-
--- ui/web/src/services/DateTimeService.ts
+++ ui/web/src/services/DateTimeService.ts
@@ -4,15 +4,21 @@ import { DateTime } from 'luxon'
 
 export class DateTimeService {
   public dateTime: number;
+  public endingTime: number;
   private readonly timeFormat: string = "HH:mm";
   private readonly dateFormat: string = "dd.MM.yyyy";
 
-  public constructor(initialDateTime: number) {
+  public constructor(initialDateTime: number, endingTime: number) {
     this.dateTime = initialDateTime;
+    this.endingTime = endingTime;
   }
 
   public get date(): Date {
     return new Date(this.dateTime);
+  }
+
+  public get endDate(): string {
+    return DateTime.fromMillis(this.endingTime).toFormat(this.dateFormat);
   }
 
   public get dateTimeInSeconds(): number {
@@ -23,7 +29,6 @@ export class DateTimeService {
     if(!dateTime) {
       dateTime = this.dateTime;
     }
-
     return DateTime.fromMillis(dateTime).toFormat(this.dateFormat);
   }
 
@@ -46,7 +51,6 @@ export class DateTimeService {
     const t = DateTime.fromFormat(timeToParse, this.timeFormat).toJSDate();
     return new Date(d.getFullYear(), d.getMonth(), d.getDate(), t.getHours(), t.getMinutes());
   }
-
 }
 
 
@@ -61,8 +65,8 @@ declare module '@vue/runtime-core' {
 }
 
 export default {
-  install: (app: App, initialDateTime: number): void => {
-    const service = reactive(new DateTimeService(initialDateTime));
+  install: (app: App, initialDateTime: number, endingTime: number): void => {
+    const service = reactive(new DateTimeService(initialDateTime, endingTime));
     window.setTimeout(() => { service.dateTime += 1000; window.setInterval(() => service.dateTime += 1000, 1000) }, 1000 - new Date().getMilliseconds())
     app.config.globalProperties.$ds = service;
   }
