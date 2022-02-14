bef4e8e4f52c3e8f1740bc6becd8181d461a74b2
Pablo Hoch <pablo@melaxis.com>
2022-02-14 19:04:09 +0100

    paxmon: get_interchanges: add arrival+departure stations

--- ui/rsl/src/api/protocol/motis/paxmon.ts
+++ ui/rsl/src/api/protocol/motis/paxmon.ts
@@ -211,6 +211,7 @@ export interface PaxMonTripStopInfo {
   schedule_time: number;
   current_time: number;
   trips: TripServiceInfo[];
+  station: Station;
 }
 
 // paxmon/PaxMonGetInterchangesResponse.fbs
