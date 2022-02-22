b062204e771dd42c0bdf0d460bd5586a21fe2780
muetzenjonas <jonasdoff@web.de>
2022-02-22 14:18:03 +0100

    - added back files  for markers - contextmenu kann einen Marker setzen

--- ui/web/pages/Modules/RailvizContextMenu.tsx
+++ ui/web/pages/Modules/RailvizContextMenu.tsx
@@ -4,12 +4,16 @@ export const RailvizContextMenu: React.FC = () => {
 
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
+    const [lat, setLat] = useState(0);
+    const [lng, setLng] = useState(0);
     const [contextMenuHidden, setContextMenuHidden] = useState<Boolean>(true);
 
     React.useEffect(() => {
         window.portEvents.sub('mapShowContextMenu', function(data){
             setX(data.mouseX);
             setY(data.mouseY);
+            setLat(data.lat);
+            setLng(data.lng);
             setContextMenuHidden(false);
         });
         window.portEvents.sub('mapCloseContextMenu', function(data){
@@ -17,8 +21,15 @@ export const RailvizContextMenu: React.FC = () => {
         })
     })
     return (
-    <div className={contextMenuHidden ? 'railviz-contextmenu hidden': 'railviz-contextmenu'} style={{ top: y+'px', left: x+'px' }}>
-        <div className='item' onClick={() => setContextMenuHidden(true)}>Routen von hier</div>
-        <div className='item' onClick={() => setContextMenuHidden(true)}>Routen hierher</div>
-    </div>);
+        //wir brauchen hier start und Ziel positionen aus der Suche
+        <div className={contextMenuHidden ? 'railviz-contextmenu hidden': 'railviz-contextmenu'} style={{ top: y+'px', left: x+'px' }}>
+            <div className='item' onClick={() => {
+                setContextMenuHidden(true);
+                window.portEvents.pub('mapSetMarkers', {'startPosition':{'lat': lat,'lng': lng}, 'startName': lat+';'+lng, 'destinationPosition': null, 'destinationName': null});
+            }}>Routen von hier</div>
+            <div className='item' onClick={() => {
+                setContextMenuHidden(true);
+                window.portEvents.pub('mapSetMarkers', {'startPosition': null, 'startName': null, 'destinationPosition':{'lat': lat, 'lng': lng}, 'destinationName':lat+';'+lng});
+            }}>Routen hierher</div>
+        </div>);
 };
