64a741dada87d787c9a3282cca01233d182af703
Pablo Hoch <pablo@melaxis.com>
2022-03-01 12:24:21 +0100

    ui/rsl: sim results fix

--- ui/rsl/src/components/SimResultsPanel.tsx
+++ ui/rsl/src/components/SimResultsPanel.tsx
@@ -70,7 +70,7 @@ function SimResultsList(): JSX.Element {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="absolute z-20 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
               {simResultsList.map((resultAtom, resultIdx) => (
                 <Listbox.Option
                   key={resultIdx}
