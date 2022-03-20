06b0e707c09f3a24266a00afe0d901aad225100c
ssyum <sung.soo@icloud.com>
2022-03-20 22:01:41 +0100

    TODO: extendbefore after button, getTimeFromSearch

--- ui/web/components/StationSearch/StationEvent.tsx
+++ ui/web/components/StationSearch/StationEvent.tsx
@@ -20,25 +20,26 @@ const getStationEvent = (byScheduleTime: boolean, direction: string, eventCount:
 };
 
 
-const stationEventDivGenerator = (eventStations: StationEvents) => {
+const stationEventDivGenerator = (eventStations: StationEvents, translation: Translations, direction: string) => {
 
     let events = eventStations.events;
     let divs = []
 
     for (let index = 0; index < events.length; index++) {
-        divs.push(
-            <div className='station-event'>
-                <div className='event-time'>{events[index].event.time}</div>
-                <div className='event-train'><span>
-                    <div className={'train-box train-class-' + events[index].trips[0].transport.clasz + ' with-tooltip'} data-tooltip='Betreiber: DB Regio AG S-Bahn Rhein-Main Zugnummer: 35324'><svg className='train-icon'>
-                        <use xlinkHref={classToId(events[index].trips[0].transport.clasz)}></use>
-                    </svg><span className='train-name'>{events[index].trips[0].transport.name}</span></div>
-                </span></div>
-                <div className='event-direction' title='Bad Soden(Taunus)'><i className='icon'>arrow_forward</i>Bad
-                    Soden(Taunus)</div>
-                <div className='event-track'></div>
-            </div>
-        );
+        if(events[index].type === direction){
+            divs.push(
+                <div className='station-event'>
+                    <div className='event-time'>{moment.unix(events[index].event.time).format('HH:mm')}</div>
+                    <div className='event-train'><span>
+                        <div className={'train-box train-class-' + events[index].trips[0].transport.clasz + ' with-tooltip'} data-tooltip={translation.connections.provider + ': ' + events[index].trips[0].transport.provider + '\n' + translation.connections.trainNr + ': ' + events[index].trips[0].transport.train_nr}><svg className='train-icon'>
+                            <use xlinkHref={classToId(events[index].trips[0].transport.clasz)}></use>
+                        </svg><span className='train-name'>{events[index].trips[0].transport.name}</span></div>
+                    </span></div>
+                    <div className='event-direction' title={events[index].trips[0].transport.direction}><i className='icon'>arrow_forward</i>{events[index].trips[0].transport.direction}</div>
+                    <div className='event-track'></div>
+                </div>
+            );
+        }
     }
 
     return divs;
@@ -47,16 +48,16 @@ const stationEventDivGenerator = (eventStations: StationEvents) => {
 export const StationEvent: React.FC<{ 'translation': Translations, 'station': (Station | Address), 'stationEventTrigger': boolean, 'setSubOverlayHidden': React.Dispatch<React.SetStateAction<boolean>>, 'setStationEventTrigger': React.Dispatch<React.SetStateAction<boolean>> }> = (props) => {
 
     const [eventStations, setEventStations] = useState<StationEvents>({ station: {id: '', name: ''}, events: []});
-    console.log(eventStations);
-
+    
     let byScheduleTime = true;
     let direction = 'BOTH';
     let eventCount = 20;
     let stationID = (props.station as Station).id;
     let time = 1646226000;
+    const [displayDirection, setDisplayDirection] = useState<string>('DEP');
 
     useEffect(() => {
-        if (props.stationEventTrigger) {
+        if (props.stationEventTrigger && stationID !== '') {
             let requestURL = 'https://europe.motis-project.de/?elm=StationEvents';
             fetch(requestURL, getStationEvent(byScheduleTime, direction, eventCount, stationID, time))
                 .then(res => res.json())
@@ -74,10 +75,14 @@ export const StationEvent: React.FC<{ 'translation': Translations, 'station': (S
                 <div className='back' onClick={() => { props.setSubOverlayHidden(true); props.setStationEventTrigger(false) }}><i className='icon'>arrow_back</i></div>
                 <div className='station'>{props.station.name}</div>
                 <div className='event-type-picker'>
-                    <div><input type='radio' id='station-departures' name='station-event-types' />
-                        <label htmlFor='station-departures'>{props.translation.search.departure}</label></div>
-                    <div><input type='radio' id='station-arrivals' name='station-event-types' />
-                        <label htmlFor='station-arrivals'>{props.translation.search.arrival}</label></div>
+                    <div>
+                        <input type='radio' id='station-departures' name='station-event-types' onClick={() => {setDisplayDirection('DEP')}}/>
+                        <label htmlFor='station-departures'>{props.translation.search.departure}</label>
+                    </div>
+                    <div>
+                        <input type='radio' id='station-arrivals' name='station-event-types' onClick={() => {setDisplayDirection('ARR')}}/>
+                        <label htmlFor='station-arrivals'>{props.translation.search.arrival}</label>
+                    </div>
                 </div>
             </div>
             <div className='events'>
@@ -85,7 +90,7 @@ export const StationEvent: React.FC<{ 'translation': Translations, 'station': (S
                     <div className='extend-search-interval search-before'><a>{props.translation.connections.extendBefore}</a></div>
                     <div className='event-list'>
                         <div className='date-header divider'><span>2.3.2022</span></div>
-                        {(eventStations.station.id === '') ? <></> : stationEventDivGenerator(eventStations)}
+                        {(eventStations.station.id === '') ? <></> : stationEventDivGenerator(eventStations, props.translation, displayDirection)}
                     </div>
                     <div className='divider footer'></div>
                     <div className='extend-search-interval search-after'><a>{props.translation.connections.extendAfter}</a></div>
