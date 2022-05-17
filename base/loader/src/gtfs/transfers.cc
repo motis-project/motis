243dfc5086bd732038437ac9448074674c6771c3
Felix Gündling <felixguendling@gmail.com>
2022-05-17 15:42:51 +0200

    add footpaths if input footpaths are not symmetric (#245)

--- base/loader/src/gtfs/transfers.cc
+++ base/loader/src/gtfs/transfers.cc
@@ -37,12 +37,16 @@ std::map<stop_pair, transfer> read_transfers(loaded_file f,
         transfers.insert(std::make_pair(
             std::pair{stops.at(get<from_stop_id>(t).to_str()).get(),
                       stops.at(get<to_stop_id>(t).to_str()).get()},
-            transfer(get<min_transfer_time>(t) / 60, get<transfer_type>(t))));
+            transfer(get<transfer_type>(t) == transfer::MIN_TRANSFER_TIME
+                         ? get<min_transfer_time>(t) / 60
+                         : 0,
+                     get<transfer_type>(t))));
       }
-    } catch (...) {
+    } catch (std::exception const& e) {
       LOG(logging::warn) << "skipping transfer (" << f.name() << ":" << i
-                         << ") between unknown stop pair "
-                         << get<from_stop_id>(t) << " - " << get<to_stop_id>(t);
+                         << ") between stop pair "
+                         << get<from_stop_id>(t).to_str() << " - "
+                         << get<to_stop_id>(t).to_str() << ": " << e.what();
     }
   }
   return transfers;
