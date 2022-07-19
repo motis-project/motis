016642530500ba7ed8b6cc6a3a8bd7c2e642c9c6
Pablo Hoch <pablo@melaxis.com>
2022-07-19 15:36:01 +0200

    clang-tidy

--- modules/rt/include/motis/rt/expanded_trips.h
+++ modules/rt/include/motis/rt/expanded_trips.h
@@ -12,7 +12,7 @@ struct expanded_trip_index {
 };
 
 inline expanded_trip_index add_trip_to_new_expanded_route(
-    schedule& sched, trip const* trp, std::uint32_t new_route_id) {
+    schedule& sched, trip* trp, std::uint32_t new_route_id) {
   auto new_exp_route = sched.expanded_trips_.emplace_back();
   new_exp_route.emplace_back(trp);
   sched.route_to_expanded_routes_[new_route_id].emplace_back(
