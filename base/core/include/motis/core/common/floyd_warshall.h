2301eb4208498c39627e9285341ac58ca99b6b53
Felix Gündling <felixguendling@gmail.com>
2022-07-23 17:22:43 +0200

    enable flat_matrix with std::vector

--- base/core/include/motis/core/common/floyd_warshall.h
+++ base/core/include/motis/core/common/floyd_warshall.h
@@ -7,15 +7,17 @@
 namespace motis {
 
 template <typename T>
-void floyd_warshall(flat_matrix<T>& mat) {
+void floyd_warshall(T&& mat) {
   utl::verify(mat.entries_.size() == mat.column_count_ * mat.column_count_,
               "floyd_warshall: input is not a square matrix.");
-  constexpr uint64_t const kMaxDistance = std::numeric_limits<T>::max();
+  using value_type = typename std::decay_t<T>::value_type;
+  constexpr uint64_t const kMaxDistance =
+      std::numeric_limits<value_type>::max();
 
   for (auto k = 0UL; k < mat.column_count_; ++k) {
     for (auto i = 0UL; i < mat.column_count_; ++i) {
       for (auto j = 0UL; j < mat.column_count_; ++j) {
-        auto const distance = static_cast<T>(
+        auto const distance = static_cast<value_type>(
             std::min(kMaxDistance, static_cast<uint64_t>(mat(i, k)) +
                                        static_cast<uint64_t>(mat(k, j))));
         if (mat(i, j) > distance) {
