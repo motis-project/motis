29c5c907aaf9bda73c61614ac078b2ab6911b022
Felix Gündling <felixguendling@gmail.com>
2022-07-23 17:22:43 +0200

    wip

--- base/core/include/motis/core/common/flat_matrix.h
+++ base/core/include/motis/core/common/flat_matrix.h
@@ -6,13 +6,15 @@
 
 namespace motis {
 
-template <typename T>
+template <typename Vector>
 struct flat_matrix {
+  using value_type = typename Vector::value_type;
+
   struct row {
     row(flat_matrix& matrix, int row_index)
         : matrix_(matrix), row_index_(row_index) {}
 
-    T& operator[](int column_index) {
+    value_type& operator[](int column_index) {
       auto pos = matrix_.column_count_ * row_index_ + column_index;
       return matrix_.entries_[pos];
     }
@@ -25,7 +27,7 @@ struct flat_matrix {
     const_row(flat_matrix const& matrix, int row_index)
         : matrix_(matrix), row_index_(row_index) {}
 
-    T const& operator[](int column_index) const {
+    value_type const& operator[](int column_index) const {
       auto pos = matrix_.column_count_ * row_index_ + column_index;
       return matrix_.entries_[pos];
     }
@@ -37,20 +39,28 @@ struct flat_matrix {
   row operator[](int row_index) { return {*this, row_index}; }
   const_row operator[](int row_index) const { return {*this, row_index}; }
 
-  T& operator()(int const row_index, int const column_index) {
+  value_type& operator()(int const row_index, int const column_index) {
     return entries_[column_count_ * row_index + column_index];
   }
 
   uint32_t column_count_{0U};
-  mcd::vector<T> entries_;
+  Vector entries_;
 };
 
 template <typename T>
-inline flat_matrix<T> make_flat_matrix(uint32_t const column_count,
-                                       T const& init = T{}) {
+inline flat_matrix<mcd::vector<T>> make_flat_matrix(uint32_t const column_count,
+                                                    T const& init = T{}) {
   auto v = mcd::vector<T>{};
   v.resize(column_count * column_count, init);
-  return flat_matrix<T>{column_count, std::move(v)};
+  return flat_matrix<mcd::vector<T>>{column_count, std::move(v)};
+}
+
+template <typename T>
+inline flat_matrix<std::vector<T>> make_std_flat_matrix(
+    uint32_t const column_count, T const& init = T{}) {
+  auto v = std::vector<T>{};
+  v.resize(column_count * column_count, init);
+  return flat_matrix<std::vector<T>>{column_count, std::move(v)};
 }
 
 }  // namespace motis
