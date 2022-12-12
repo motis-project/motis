36a5f2e9f57abef7f9704da21215c780cf465119
Pablo Hoch <pablohoch@users.noreply.github.com>
2022-12-12 11:55:14 +0100

    RSL UI Fixes + Improvements (#280)

--- ui/rsl/src/data/durationFormat.ts
+++ ui/rsl/src/data/durationFormat.ts
@@ -1,12 +1,19 @@
-export function formatShortDuration(minutes: number): string {
+export function formatShortDuration(
+  minutes: number,
+  forceSign = false
+): string {
   let mins = Math.round(minutes);
   let text = "";
   if (mins >= 60) {
     const hours = Math.floor(mins / 60);
     mins = mins % 60;
     text = `${hours}h${mins}m`;
+  } else if (mins <= -60) {
+    const hours = Math.ceil(mins / 60);
+    mins = mins % 60;
+    text = `${hours}h${-mins}m`;
   } else {
     text = `${mins}m`;
   }
-  return text;
+  return forceSign && mins >= 0 ? `+${text}` : text;
 }
