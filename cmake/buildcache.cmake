ab6db1ff43956e788eca6ba48bb9148da5df20e6
Julian Harbarth <julian@harbarth.io>
2023-01-16 15:51:54 +0000

    update buildcache (#288)

--- cmake/buildcache.cmake
+++ cmake/buildcache.cmake
@@ -23,7 +23,7 @@ else()
       message(FATAL "Error: NO_BUILDCACHE was not set but buildcache was not in path and system OS detection failed")
     endif()
 
-    set(buildcache-url "https://github.com/mbitsnbites/buildcache/releases/download/v0.27.6/${buildcache-archive}")
+    set(buildcache-url "https://github.com/mbitsnbites/buildcache/releases/download/v0.28.2/${buildcache-archive}")
     message(STATUS "Downloading buildcache binary from ${buildcache-url}")
     file(DOWNLOAD "${buildcache-url}" ${CMAKE_CURRENT_BINARY_DIR}/${buildcache-archive})
     execute_process(
