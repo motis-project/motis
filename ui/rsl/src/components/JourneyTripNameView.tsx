103d9c009ab924f92355503b6027846aff6588a0
Pablo Hoch <pablo@melaxis.com>
2023-03-20 14:43:41 +0100

    ui/rsl: fix for api changes

--- ui/rsl/src/components/JourneyTripNameView.tsx
+++ ui/rsl/src/components/JourneyTripNameView.tsx
@@ -5,9 +5,7 @@ type JourneyTripNameViewProps = {
 };
 
 function JourneyTripNameView({ jt }: JourneyTripNameViewProps): JSX.Element {
-  const names = [
-    ...new Set(jt.transports.map((t) => `${t.category_name} ${t.train_nr}`)),
-  ];
+  const names = [...new Set(jt.transports.map((t) => `${t.name}`))];
   // return <span>{names.join(", ")}</span>;
   return <span>{names.length > 0 ? names[0] : "???"}</span>;
 }
