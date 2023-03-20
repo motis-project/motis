f2ca2584236a6e908f0ee7156123ee5aa27e0f74
Pablo Hoch <pablo@melaxis.com>
2023-03-20 14:42:40 +0100

    update api types after nigiri merge

--- ui/rsl/src/api/protocol/motis.ts
+++ ui/rsl/src/api/protocol/motis.ts
@@ -157,10 +157,7 @@ export interface Range {
 // base/Connection.fbs
 export interface Transport {
   range: Range;
-  category_name: string;
-  category_id: number;
   clasz: number;
-  train_nr: number;
   line_id: string;
   name: string;
   provider: string;
