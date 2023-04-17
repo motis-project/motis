ab33989b122b17758a83ecaf32a4a9fc50098209
Felix Gündling <felixguendling@gmail.com>
2023-04-17 05:54:04 +0200

    wip

--- modules/routing/src/eval/xtract.cc
+++ modules/routing/src/eval/xtract.cc
@@ -43,36 +43,10 @@ std::tuple<std::string, int, int> get_service_source(std::string const& d) {
           std::stoi(d.substr(line_end + 1))};
 }
 
-struct hrd_footpath {
-  CISTA_COMPARABLE()
-  std::string from_id_, to_id_;
-  int duration_;
-};
-
-auto const parser_config = hrd_5_20_26;
-
-int xtract(int argc, char const** argv) {
-  if (argc < 4) {
-    std::cout << "usage: " << argv[0]
-              << " src-schedule target-schedule response [response, ...]";
-    return 0;
-  }
-
-  auto const schedule_path = fs::path{argv[1]};
-  auto const new_schedule_path = fs::path{argv[2]};
-  auto const response_files = [&]() {
-    std::vector<std::string> r;
-    for (auto i = 3U; i != argc; ++i) {
-      r.emplace_back(argv[i]);
-    }
-    return r;
-  }();
-
-  std::set<hrd_footpath> footpaths;
-  std::map<std::string /* filename / trip_id */,
-           std::set<std::pair<int, int>> /* first/last line */>
-      services;
-  std::set<std::string> meta_candidates;
+std::map<std::string /* filename / trip_id */,
+         std::set<std::pair<int, int>> /* first/last line */>
+get_services(std::vector<fs::path> const& response_files) {
+  std::map<std::string, std::set<std::pair<int, int>>> services;
   for (auto const& [res_idx, path] : utl::enumerate(response_files)) {
     if (!std::filesystem::is_regular_file(path)) {
       std::cout << "not a regular file: " << path << "\n";
@@ -91,10 +65,6 @@ int xtract(int argc, char const** argv) {
 
       auto c_idx = 0U;
       for (auto const& c : *routing_res->connections()) {
-        meta_candidates.emplace(c->stops()->Get(0)->station()->id()->str());
-        meta_candidates.emplace(
-            c->stops()->Get(c->stops()->size() - 1)->station()->id()->str());
-
         for (auto const& trip : *c->trips()) {
           auto const source = trip->debug()->str();
           if (source.empty()) {
@@ -116,82 +86,35 @@ int xtract(int argc, char const** argv) {
       }
     });
   }
+  return services;
+}
 
-  auto const number_of_trips = std::accumulate(
-      begin(services), end(services), 0U, [](unsigned const sum, auto&& entry) {
-        return sum + entry.second.size();
-      });
-  std::cout << "number of trips: " << number_of_trips << "\n";
+void xtract_hrd(
+    fs::path const& schedule_path, fs::path const& new_schedule_path,
+    std::map<std::string, std::set<std::pair<int, int>>> const& services) {
+  fs::remove_all(new_schedule_path);
+  fs::create_directories(new_schedule_path / SCHEDULE_DATA);
+  fs::create_symlink(absolute(schedule_path) / CORE_DATA,
+                     new_schedule_path / CORE_DATA);
 
-  if (hrd_parser{}.applicable(schedule_path)) {
-    fs::create_directories(new_schedule_path / SCHEDULE_DATA);
-    std::cout << "writing services.txt\n";
-    std::ofstream services_file{
-        (new_schedule_path / SCHEDULE_DATA / "services.txt").c_str()};
-    for (auto const& [filename, line_ranges] : services) {
-      if (!fs::is_regular_file(schedule_path / SCHEDULE_DATA / filename)) {
-        std::cout << "Error: Schedule file " << filename << " not found\n";
-        continue;
-      }
-
-      cista::mmap f{
-          (schedule_path / SCHEDULE_DATA / filename).generic_string().c_str(),
-          cista::mmap::protection::READ};
-      auto const file_content = utl::cstr{f.data(), f.size()};
-
-      auto line_number = 0U;
-      auto line_range_it = begin(line_ranges);
-      for (auto const& line : utl::lines(file_content)) {
-        ++line_number;
-
-        if (line_number > line_range_it->second) {
-          ++line_range_it;
-          if (line_range_it == end(line_ranges)) {
-            break;
-          }
-        }
-        if (line_number < line_range_it->first) {
-          continue;
-        }
-
-        services_file << line.view() << "\n";
-      }
+  std::cout << "writing services.txt\n";
+  std::ofstream services_file{
+      (new_schedule_path / SCHEDULE_DATA / "services.txt").c_str()};
+  for (auto const& [filename, line_ranges] : services) {
+    if (!fs::is_regular_file(schedule_path / SCHEDULE_DATA / filename)) {
+      std::cout << "Error: Schedule file " << filename << " not found\n";
+      continue;
     }
-  } else if (gtfs_parser{}.applicable(schedule_path)) {
-    fs::remove_all(new_schedule_path);
-    fs::create_directories(new_schedule_path);
 
-    fs::create_symlink(absolute(schedule_path) / AGENCY_FILE,
-                       new_schedule_path / AGENCY_FILE);
-    fs::create_symlink(absolute(schedule_path) / STOPS_FILE,
-                       new_schedule_path / STOPS_FILE);
-    fs::create_symlink(absolute(schedule_path) / CALENDAR_FILE,
-                       new_schedule_path / CALENDAR_FILE);
-    fs::create_symlink(absolute(schedule_path) / CALENDAR_DATES_FILE,
-                       new_schedule_path / CALENDAR_DATES_FILE);
-    fs::create_symlink(absolute(schedule_path) / TRANSFERS_FILE,
-                       new_schedule_path / TRANSFERS_FILE);
-    fs::create_symlink(absolute(schedule_path) / FEED_INFO_FILE,
-                       new_schedule_path / FEED_INFO_FILE);
-    fs::create_symlink(absolute(schedule_path) / FREQUENCIES_FILE,
-                       new_schedule_path / FREQUENCIES_FILE);
+    cista::mmap f{
+        (schedule_path / SCHEDULE_DATA / filename).generic_string().c_str(),
+        cista::mmap::protection::READ};
+    auto const file_content = utl::cstr{f.data(), f.size()};
 
-    std::cout << "writing stop_times.txt\n";
-    std::ofstream services_file{(new_schedule_path / "stop_times.txt").c_str()};
-    std::set<std::pair<int, int>> line_ranges;
-    for (auto const& [trip_id, ranges] : services) {
-      line_ranges.insert(ranges.begin(), ranges.end());
-    }
-    cista::mmap f{(schedule_path / STOP_TIMES_FILE).generic_string().c_str(),
-                  cista::mmap::protection::READ};
     auto line_number = 0U;
     auto line_range_it = begin(line_ranges);
-    for (auto const& line : utl::lines(utl::cstr{f.data(), f.size()})) {
+    for (auto const& line : utl::lines(file_content)) {
       ++line_number;
-
-      if (line_number == 1) {
-        services_file << line.view() << "\n";
-      }
 
       if (line_number > line_range_it->second) {
         ++line_range_it;
@@ -199,13 +122,98 @@ int xtract(int argc, char const** argv) {
           break;
         }
       }
-
       if (line_number < line_range_it->first) {
         continue;
       }
 
       services_file << line.view() << "\n";
     }
+  }
+}
+
+void xtract_gtfs(
+    fs::path const& schedule_path, fs::path const& new_schedule_path,
+    std::map<std::string, std::set<std::pair<int, int>>> const& services) {
+  fs::remove_all(new_schedule_path);
+  fs::create_directories(new_schedule_path);
+
+  fs::create_symlink(absolute(schedule_path) / AGENCY_FILE,
+                     new_schedule_path / AGENCY_FILE);
+  fs::create_symlink(absolute(schedule_path) / STOPS_FILE,
+                     new_schedule_path / STOPS_FILE);
+  fs::create_symlink(absolute(schedule_path) / CALENDAR_FILE,
+                     new_schedule_path / CALENDAR_FILE);
+  fs::create_symlink(absolute(schedule_path) / CALENDAR_DATES_FILE,
+                     new_schedule_path / CALENDAR_DATES_FILE);
+  fs::create_symlink(absolute(schedule_path) / TRANSFERS_FILE,
+                     new_schedule_path / TRANSFERS_FILE);
+  fs::create_symlink(absolute(schedule_path) / FREQUENCIES_FILE,
+                     new_schedule_path / FREQUENCIES_FILE);
+  fs::create_symlink(absolute(schedule_path) / ROUTES_FILE,
+                     new_schedule_path / ROUTES_FILE);
+  fs::create_symlink(absolute(schedule_path) / TRIPS_FILE,
+                     new_schedule_path / TRIPS_FILE);
+
+  std::cout << "writing stop_times.txt\n";
+  std::ofstream services_file{(new_schedule_path / "stop_times.txt").c_str()};
+  std::set<std::pair<int, int>> line_ranges;
+  for (auto const& [trip_id, ranges] : services) {
+    line_ranges.insert(ranges.begin(), ranges.end());
+  }
+  cista::mmap f{(schedule_path / STOP_TIMES_FILE).generic_string().c_str(),
+                cista::mmap::protection::READ};
+  auto line_number = 0U;
+  auto line_range_it = begin(line_ranges);
+  for (auto const& line : utl::lines(utl::cstr{f.data(), f.size()})) {
+    ++line_number;
+
+    if (line_number == 1) {
+      services_file << line.view() << "\n";
+    }
+
+    if (line_number > line_range_it->second) {
+      ++line_range_it;
+      if (line_range_it == end(line_ranges)) {
+        break;
+      }
+    }
+
+    if (line_number < line_range_it->first) {
+      continue;
+    }
+
+    services_file << line.view() << "\n";
+  }
+}
+
+int xtract(int argc, char const** argv) {
+  if (argc < 4) {
+    std::cout << "usage: " << argv[0]
+              << " src-schedule target-schedule response [response, ...]";
+    return 0;
+  }
+
+  auto const schedule_path = fs::path{argv[1]};
+  auto const new_schedule_path = fs::path{argv[2]};
+  auto const response_files = [&]() {
+    std::vector<fs::path> r;
+    for (auto i = 3U; i != argc; ++i) {
+      r.emplace_back(argv[i]);
+    }
+    return r;
+  }();
+
+  auto const services = get_services(response_files);
+  auto const number_of_trips = std::accumulate(
+      begin(services), end(services), 0U, [](unsigned const sum, auto&& entry) {
+        return sum + entry.second.size();
+      });
+  std::cout << "number of trips: " << number_of_trips << "\n";
+
+  if (hrd_parser{}.applicable(schedule_path)) {
+    xtract_hrd(schedule_path, new_schedule_path, services);
+  } else if (gtfs_parser{}.applicable(schedule_path)) {
+    xtract_gtfs(schedule_path, new_schedule_path, services);
   } else {
     std::cout << "no timetable format detected: " << schedule_path << "\n";
   }
