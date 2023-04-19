d4751dad6a44524875a8d5d7636d61b14fc1ca1f
Pablo Hoch <pablo@melaxis.com>
2023-04-19 14:51:34 +0200

    clang-tidy --fix-errors broke the code...

--- base/loader/test/hrd/test_spec_test.cc
+++ base/loader/test/hrd/test_spec_test.cc
@@ -8,14 +8,14 @@ namespace motis::loader::hrd {
 using namespace flatbuffers64;
 
 std::vector<specification> test_spec::get_specs() const {
-  std::vector<specification> const specs;
+  std::vector<specification> specs;  // NOLINT(misc-const-correctness)
   parse_specification(
       lf_, [&specs](specification const& spec) { specs.push_back(spec); });
   return specs;
 }
 
 std::vector<hrd_service> test_spec::get_hrd_services(config const& c) const {
-  std::vector<hrd_service> const services;
+  std::vector<hrd_service> services;  // NOLINT(misc-const-correctness)
   parse_specification(lf_, [&services, &c](specification const& spec) {
     services.emplace_back(spec, c);
   });
