da10db270dffce85d72c93c311a88392cab02977
Felix Gündling <felixguendling@gmail.com>
2023-06-18 10:21:14 +0200

    nigiri default timezone setting (#337)

--- modules/nigiri/include/motis/nigiri/nigiri.h
+++ modules/nigiri/include/motis/nigiri/nigiri.h
@@ -29,6 +29,7 @@ private:
   std::unique_ptr<impl> impl_;
   bool no_cache_{false};
   std::string first_day_;
+  std::string default_timezone_;
   std::uint16_t num_days_{2U};
   bool geo_lookup_{false};
   unsigned link_stop_distance_{100U};
