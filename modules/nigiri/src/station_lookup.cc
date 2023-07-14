e714cff1e35b0cdcff70d48d8521c3e0fde939a1
Felix Gündling <felixguendling@gmail.com>
2023-07-14 14:45:09 +0200

    wip

--- modules/nigiri/src/station_lookup.cc
+++ modules/nigiri/src/station_lookup.cc
@@ -22,13 +22,14 @@ nigiri_station_lookup::~nigiri_station_lookup() noexcept = default;
 lookup_station nigiri_station_lookup::get(std::size_t const idx) const {
   auto const l = n::location_idx_t{idx};
   auto const p = tt_.locations_.parents_[l];
-  return {
-      .tag_ = tags_.get_tag(tt_.locations_.src_[l]),
-      .id_ = tt_.locations_.ids_[l].view(),
-      .name_ = (p == n::location_idx_t::invalid() ? tt_.locations_.names_[l]
-                                                  : tt_.locations_.names_[p])
-                   .view(),
-      .pos_ = tt_.locations_.coordinates_[l]};
+  auto s = lookup_station{};
+  s.tag_ = tags_.get_tag(tt_.locations_.src_[l]),
+  s.id_ = tt_.locations_.ids_[l].view(),
+  s.name_ = (p == n::location_idx_t::invalid() ? tt_.locations_.names_[l]
+                                               : tt_.locations_.names_[p])
+                .view(),
+  s.pos_ = tt_.locations_.coordinates_[l];
+  return s;
 }
 
 lookup_station nigiri_station_lookup::get(std::string_view id) const {
