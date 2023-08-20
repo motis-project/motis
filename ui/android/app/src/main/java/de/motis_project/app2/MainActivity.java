80dec7e08dc22fbdd411ab14d343a4af7ca000d3
Felix Gündling <felix.guendling@gmail.com>
2023-08-20 11:50:06 +0200

    wip

--- ui/android/app/src/main/java/de/motis_project/app2/MainActivity.java
+++ ui/android/app/src/main/java/de/motis_project/app2/MainActivity.java
@@ -1,10 +1,16 @@
 package de.motis_project.app2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -42,6 +48,46 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager.beginTransaction()
             .replace(R.id.content_frame, queryFragment)
             .commit();
+
+        showPrivacyDialog();
+    }
+
+    void showPrivacyDialog() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean("show_privacy", true)) {
+            return;
+        }
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle(R.string.data_protection_headline);
+        alertDialog.setMessage(R.string.data_protection);
+        alertDialog.setPositiveButton(R.string.read,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(
+                        Intent.ACTION_VIEW, Uri
+                        .parse("https://motis-project.de/privacy")));
+                    prefs.edit().putBoolean("show_privacy", false).commit();
+                    dialog.dismiss();
+                }
+            });
+
+        alertDialog.setNeutralButton(R.string.dont_show_again,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    prefs.edit().putBoolean("show_privacy", false).commit();
+                    dialog.dismiss();
+                }
+            });
+
+        alertDialog.setNegativeButton(R.string.show_again_later,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+        alertDialog.show();
     }
 
     @Override
