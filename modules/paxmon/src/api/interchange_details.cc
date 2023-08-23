16537bde74a03098810af0bfa4d97f4d6bfe4658
Pablo Hoch <pablo@melaxis.com>
2023-08-23 12:55:26 +0200

    rsl: /paxmon/interchange_details: add option to include groups

--- modules/paxmon/src/api/interchange_details.cc
+++ modules/paxmon/src/api/interchange_details.cc
@@ -1,6 +1,9 @@
 #include "motis/paxmon/api/interchange_details.h"
 
-#include <iostream>
+#include <limits>
+#include <vector>
+
+#include "boost/range/join.hpp"
 
 #include "utl/verify.h"
 
@@ -28,11 +31,17 @@ msg_ptr interchange_details(paxmon_data& data, msg_ptr const& msg) {
 
   auto const include_disabled_group_routes =
       req->include_disabled_group_routes();
+  auto const include_full_groups = req->include_full_groups();
+  auto const include_reroute_log = req->include_reroute_log();
 
   auto const* e = ei.get(uv);
   utl::verify(e->is_interchange(), "interchange_details: invalid id");
 
   message_creator mc;
+
+  auto const normal_routes = uv.pax_connection_info_.group_routes(e->pci_);
+  auto const broken_routes =
+      uv.pax_connection_info_.broken_group_routes(e->pci_);
 
   auto const info = get_interchange_info(
       uv, sched, ei, mc,
@@ -41,11 +50,35 @@ msg_ptr interchange_details(paxmon_data& data, msg_ptr const& msg) {
           .include_disabled_group_routes_ = include_disabled_group_routes,
           .include_delay_info_ = true});
 
+  auto const get_fbs_groups = [&](auto const& groups) {
+    auto result = std::vector<Offset<PaxMonGroup>>{};
+    auto last_pg = std::numeric_limits<passenger_group_index>::max();
+    for (auto const& pgwr : groups) {
+      if (last_pg == pgwr.pg_) {
+        continue;
+      }
+      last_pg = pgwr.pg_;
+      auto const& pg = uv.passenger_groups_.at(pgwr.pg_);
+      result.emplace_back(
+          to_fbs(sched, uv.passenger_groups_, mc, *pg, include_reroute_log));
+    }
+    return result;
+  };
+
+  auto fbs_groups = std::vector<Offset<PaxMonGroup>>{};
+  if (include_full_groups) {
+    if (include_disabled_group_routes) {
+      fbs_groups = get_fbs_groups(boost::join(normal_routes, broken_routes));
+    } else {
+      fbs_groups = get_fbs_groups(normal_routes);
+    }
+  }
+
   mc.create_and_finish(
       MsgContent_PaxMonInterchangeDetailsResponse,
       CreatePaxMonInterchangeDetailsResponse(
           mc, info.to_fbs_interchange_info(mc, uv, sched, true),
-          info.normal_routes_, info.broken_routes_)
+          info.normal_routes_, info.broken_routes_, mc.CreateVector(fbs_groups))
           .Union());
   return make_msg(mc);
 }
