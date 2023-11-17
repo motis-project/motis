02eeacd56f147d4b9f0a4540239088fde62f554c
Pablo Hoch <pablo@melaxis.com>
2023-11-17 13:50:49 +0100

    ui/rsl: fixes for vite 5

--- ui/rsl/vite.config.ts
+++ ui/rsl/vite.config.ts
@@ -1,10 +1,10 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   base: "/rsl/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
