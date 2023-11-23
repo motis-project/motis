af2309a2c6f6fed94275e94800c9ab86e6c143a1
Felix Gündling <felixguendling@gmail.com>
2023-11-23 13:24:51 +0100

    wip

--- modules/ris/src/gtfs-rt/util.cc
+++ modules/ris/src/gtfs-rt/util.cc
@@ -9,7 +9,8 @@ namespace motis::ris::gtfsrt {
 
 std::string json_to_protobuf(std::string const& json) {
   transit_realtime::FeedMessage msg;
-  google::protobuf::util::JsonStringToMessage(json, &msg);
+  auto const status = google::protobuf::util::JsonStringToMessage(json, &msg);
+  utl::verify(status.ok(), "JsonStringToMessage: {}", status.message());
   return msg.SerializeAsString();
 }
 
@@ -21,7 +22,9 @@ std::string protobuf_to_json(std::string const& protobuf) {
   std::string output;
   google::protobuf::util::JsonPrintOptions options;
   options.add_whitespace = true;
-  google::protobuf::util::MessageToJsonString(feed_message, &output, options);
+  auto const status = google::protobuf::util::MessageToJsonString(
+      feed_message, &output, options);
+  utl::verify(status.ok(), "JsonStringToMessage: {}", status.message());
   return output;
 }
 
