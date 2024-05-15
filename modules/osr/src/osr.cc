f5739ca07748295bcc944b1719c9e5e1914872ae
Felix Gündling <felixguendling@gmail.com>
2024-05-15 15:12:21 +0200

    osr without memory mapped routing data (#507)

--- modules/osr/src/osr.cc
+++ modules/osr/src/osr.cc
@@ -345,9 +345,6 @@ void osr::import(mm::import_dispatcher& reg) {
         }
 
         auto w = std::make_unique<o::ways>(dir, cista::mmap::protection::READ);
-        if (lock_) {
-          w->lock();
-        }
         auto l = std::make_unique<o::lookup>(*w);
         impl_ = std::make_unique<impl>(std::move(w), std::move(l));
 
