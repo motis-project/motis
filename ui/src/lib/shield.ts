56a3612c2dfc27373ee237baf52f91cb5eeaeb39
Felix Gündling <felix.guendling@gmail.com>
2024-05-29 19:29:05 +0200

    wip

---
+++ ui/src/lib/shield.ts
@@ -0,0 +1,67 @@
+import { browser } from '$app/environment';
+
+class ShieldOptions {
+    fill!: string;
+    stroke!: string;
+}
+
+export function createShield(opt: ShieldOptions): [ImageData | null, Object | null] {
+    if (!browser) {
+        return [null, null];
+    }
+
+    const d = 32
+
+    const cv = document.createElement('canvas');
+    cv.width = d;
+    cv.height = d;
+    const ctx = cv.getContext('2d')!;
+
+    // coord of the line (front = near zero, back = opposite)
+    const l_front = 1;
+    const l_back = d - 1;
+
+    // coord start of the arc
+    const lr_front = l_front + 2;
+    const lr_back = l_back - 2;
+
+    // control point of the arc
+    const lp_front = l_front + 1;
+    const lp_back = l_back - 1;
+
+    let p = new Path2D();
+    p.moveTo(lr_front, l_front);
+
+    // top line
+    p.lineTo(lr_back, l_front);
+    // top right corner
+    p.bezierCurveTo(lp_back, lp_front, lp_back, lp_front, l_back, lr_front);
+    // right line
+    p.lineTo(l_back, lr_back);
+    // bottom right corner
+    p.bezierCurveTo(lp_back, lp_back, lp_back, lp_back, lr_back, l_back);
+    // bottom line
+    p.lineTo(lr_front, l_back);
+    // bottom left corner
+    p.bezierCurveTo(lp_front, lp_back, lp_front, lp_back, l_front, lr_back);
+    // left line
+    p.lineTo(l_front, lr_front);
+    // top left corner
+    p.bezierCurveTo(lp_front, lp_front, lp_front, lp_front, lr_front, l_front);
+
+    p.closePath();
+
+    ctx.fillStyle = opt.fill;
+    ctx.fill(p);
+    ctx.strokeStyle = opt.stroke;
+    ctx.stroke(p);
+
+    return [
+        ctx.getImageData(0, 0, d, d),
+        {
+            content: [lr_front, lr_front, lr_back, lr_back],
+            stretchX: [[lr_front, lr_back]],
+            stretchY: [[lr_front, lr_back]]
+        }
+    ];
+}
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import maplibregl from 'maplibre-gl';
+	import {
+		MapLibre,
+		Control,
+		ControlGroup,
+		ControlButton,
+		GeoJSON,
+		LineLayer
+	} from 'svelte-maplibre';
+	import { getStyle } from '$lib/style';
+	import { createShield } from '$lib/shield';
+
+	const baseUrl = 'https://osr.motis-project.de';
+
+	let zoom = $state(18);
+	let bounds = $state<undefined | maplibregl.LngLatBoundsLike>(undefined);
+
+	const getGraph = async (bounds: maplibregl.LngLatBounds) => {
+		const response = await fetch(`${baseUrl}/api/graph`, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				level: level,
+				waypoints: bounds.toArray().flat()
+			})
+		});
+		return await response.json();
+	};
+
+	const getLevels = async (bounds: maplibregl.LngLatBounds) => {
+		const response = await fetch(`${baseUrl}/api/levels`, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				waypoints: bounds.toArray().flat()
+			})
+		});
+		return await response.json();
+	};
+
+	let level = $state(0);
+	let levels = $state<null | Array<number>>(null);
+	$effect(async () => {
+		levels =
+			bounds === undefined || zoom < 18
+				? null
+				: await getLevels(maplibregl.LngLatBounds.convert(bounds!));
+	});
+
+	let map = $state<maplibregl.Map | null>(null);
+	let showGraph = $state(false);
+	let graph = $derived.by(async () => (showGraph && map ? await getGraph(map.getBounds()) : null));
+
+	const [shieldData, shieldOptions] = createShield({
+		fill: 'hsl(0, 0%, 98%)',
+		stroke: 'hsl(0, 0%, 75%)'
+	});
+</script>
+
+<MapLibre
+	bind:map
+	bind:bounds
+	bind:zoom
+	standardControls
+	transformRequest={(url, _resourceType) => {
+		if (url.startsWith('/')) {
+			return { url: `https://europe.motis-project.de/tiles${url}` };
+		}
+	}}
+	center={[8.663351200419433, 50.10680913598618]}
+	class="h-screen"
+	images={shieldData == null || shieldOptions == null
+		? []
+		: [{ id: 'shield', data: shieldData, options: shieldOptions }]}
+	style={getStyle(level)}
+>
+	<Control class="flex flex-col gap-y-2">
+		<ControlGroup>
+			<ControlButton
+				class={showGraph ? 'bg-green-200' : ''}
+				on:click={() => {
+					showGraph = !showGraph;
+				}}>G</ControlButton
+			>
+		</ControlGroup>
+
+		<ControlGroup>
+			{#if levels}
+				{#each levels as l}
+					<ControlButton
+						on:click={() => {
+							level = l;
+						}}
+					>
+						{l}
+					</ControlButton>
+				{/each}
+			{/if}
+		</ControlGroup>
+	</Control>
+
+	{#if graph != null}
+		<GeoJSON id="graph" data={graph} promoteId="GRAPH">
+			<LineLayer
+				filter={[
+					'all',
+					['==', 'type', 'geometry'],
+					['any', ['!has', 'level'], ['==', 'level', level]]
+				]}
+				layout={{
+					'line-join': 'round',
+					'line-cap': 'round'
+				}}
+				paint={{
+					'line-color': '#e55e5e',
+					'line-width': 3,
+					'line-opacity': 1
+				}}
+			/>
+		</GeoJSON>
+	{/if}
+</MapLibre>
