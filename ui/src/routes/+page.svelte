20a47af502e3008bd66916ef1d4dc54e5872bf62
Felix Gündling <felix.guendling@gmail.com>
2024-05-30 14:19:00 +0200

    wip

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -49,21 +49,17 @@
 	};
 
 	let level = $state(0);
-	let levels = $state<null | Array<number>>(null);
-	$effect(async () => {
-		levels =
-			bounds === undefined || zoom < 18
-				? null
-				: await getLevels(maplibregl.LngLatBounds.convert(bounds!));
-	});
+	let levels = $derived.by(async () =>
+		bounds === undefined || zoom < 18
+			? null
+			: await getLevels(maplibregl.LngLatBounds.convert(bounds!))
+	);
 
 	let map = $state<maplibregl.Map | null>(null);
 	let showGraph = $state(false);
-	let graph = $state<null | Object>(null);
-	$effect(async () => {
-		bounds; // trigger graph reload when bounds change
-		graph = showGraph && map ? await getGraph(map.getBounds()) : null;
-	});
+	let graph = $derived.by(async () =>
+		showGraph && map && bounds ? await getGraph(map.getBounds()) : null
+	);
 
 	const [shieldData, shieldOptions] = createShield({
 		fill: 'hsl(0, 0%, 98%)',
@@ -100,55 +96,63 @@
 
 		<ControlGroup>
 			{#if levels}
-				{#each levels as l}
-					<ControlButton
-						on:click={() => {
-							level = l;
-						}}
-					>
-						{l}
-					</ControlButton>
-				{/each}
+				{#await levels then lvls}
+					{#each lvls as l}
+						<ControlButton
+							on:click={() => {
+								level = l;
+							}}
+						>
+							{l}
+						</ControlButton>
+					{/each}
+				{/await}
 			{/if}
 		</ControlGroup>
 	</Control>
 
 	{#if graph != null}
-		<GeoJSON id="graph" data={graph} promoteId="GRAPH">
-			<LineLayer
-				filter={[
-					'all',
-					['==', 'type', 'geometry'],
-					['any', ['!has', 'level'], ['==', 'level', level]]
-				]}
-				layout={{
-					'line-join': 'round',
-					'line-cap': 'round'
-				}}
-				paint={{
-					'line-color': '#e55e5e',
-					'line-width': 3,
-					'line-opacity': 1
-				}}
-			/>
-			<LineLayer
-				filter={['all', ['==', 'type', 'edge'], ['any', ['!has', 'level'], ['==', 'level', level]]]}
-				layout={{
-					'line-join': 'round',
-					'line-cap': 'round'
-				}}
-				paint={{
-					'line-color': '#a300d9',
-					'line-width': 3
-				}}
-			/>
-			<CircleLayer
-				filter={['all', ['==', '$type', 'Point']]}
-				paint={{
-					'circle-color': ['match', ['get', 'label'], 'unreachable', '#ff1150', '#11ffaf'],
-					'circle-radius': 6
-				}}
-			/>
-		</GeoJSON>
+		{#await graph then g}
+			<GeoJSON id="graph" data={g} promoteId="GRAPH">
+				<LineLayer
+					filter={[
+						'all',
+						['==', 'type', 'geometry'],
+						['any', ['!has', 'level'], ['==', 'level', level]]
+					]}
+					layout={{
+						'line-join': 'round',
+						'line-cap': 'round'
+					}}
+					paint={{
+						'line-color': '#e55e5e',
+						'line-width': 3,
+						'line-opacity': 1
+					}}
+				/>
+				<LineLayer
+					filter={[
+						'all',
+						['==', 'type', 'edge'],
+						['any', ['!has', 'level'], ['==', 'level', level]]
+					]}
+					layout={{
+						'line-join': 'round',
+						'line-cap': 'round'
+					}}
+					paint={{
+						'line-color': '#a300d9',
+						'line-width': 3
+					}}
+				/>
+				<CircleLayer
+					filter={['all', ['==', '$type', 'Point']]}
+					paint={{
+						'circle-color': ['match', ['get', 'label'], 'unreachable', '#ff1150', '#11ffaf'],
+						'circle-radius': 6
+					}}
+				/>
+			</GeoJSON>
+		{/await}
 	{/if}
 </MapLibre>
