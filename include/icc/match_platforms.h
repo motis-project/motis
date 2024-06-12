05f792caf47c9f75f1257546683ce841e0fdf12b
Felix Gündling <felix.guendling@gmail.com>
2024-06-12 12:43:38 +0200

    wip

--- include/icc/match_platforms.h
+++ include/icc/match_platforms.h
@@ -7,9 +7,6 @@
 
 namespace icc {
 
-using matching_t =
-    cista::offset::vector_map<nigiri::location_idx_t, osr::platform_idx_t>;
-
 std::optional<geo::latlng> get_platform_center(osr::platforms const&,
                                                osr::ways const&,
                                                osr::platform_idx_t);
