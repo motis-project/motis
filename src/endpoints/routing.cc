bbb5aa9ea9680a8df7e9df0404665f7374e479b8
Felix Gündling <felix.guendling@gmail.com>
2024-07-02 21:49:32 +0200

    wip

--- src/endpoints/routing.cc
+++ src/endpoints/routing.cc
@@ -13,8 +13,7 @@ namespace icc::ep {
 using place_t = std::variant<osr::location, n::location_idx_t>;
 
 place_t to_place(n::timetable const& tt, std::string_view s) {
-  auto const location = parse_location(s);
-  if (location.has_value()) {
+  if (auto const location = parse_location(s); location.has_value()) {
     return *location;
   }
   try {
@@ -25,8 +24,7 @@ place_t to_place(n::timetable const& tt, std::string_view s) {
 }
 
 api::plan_response routing::operator()(boost::urls::url_view const& url) const {
-  auto const params = url.params();
-  auto const query = api::plan_params{params};
+  auto const query = api::plan_params{url.params()};
   auto const from = to_place(tt_, query.fromPlace_);
   auto const to = to_place(tt_, query.toPlace_);
   return api::plan_response{};
