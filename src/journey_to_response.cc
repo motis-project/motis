4d7ba30049bc55863dcbd74a7b59a8b879a9a0f7
Felix Gündling <felix.guendling@gmail.com>
2024-07-04 13:36:47 +0200

    wip

--- src/journey_to_response.cc
+++ src/journey_to_response.cc
@@ -11,7 +11,7 @@ namespace n = nigiri;
 
 namespace icc {
 
-std::int64_t to_unixtime_ms(n::unixtime_t const t) {
+std::int64_t to_ms(n::unixtime_t const t) {
   return std::chrono::duration_cast<std::chrono::milliseconds>(
              t.time_since_epoch())
       .count();
@@ -21,51 +21,89 @@ std::int64_t to_seconds(n::i32_minutes const t) {
   return std::chrono::duration_cast<std::chrono::seconds>(t).count();
 }
 
+std::int64_t to_ms(n::i32_minutes const t) {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(t).count();
+}
+
+api::Place to_place(n::timetable const& tt, n::location_idx_t const l) {
+  auto const pos = tt.locations_.coordinates_[l];
+  return {.name_ = std::string{tt.locations_.names_[l].view()},
+          .lat_ = pos.lat_,
+          .lon_ = pos.lng_};
+}
+
+std::string to_str(n::color_t const c) {
+  return fmt::format("{:06x}", to_idx(c) & 0x00ffffff);
+}
+
 api::Itinerary journey_to_response(n::timetable const& tt,
                                    n::rt_timetable const* rtt,
                                    n::routing::journey const& j) {
-  auto legs = std::vector<api::Leg>{};
-  for (auto const [i, leg] : utl::enumerate(j.legs_)) {
+  auto itinerary = api::Itinerary{
+      .duration_ = to_seconds(j.arrival_time() - j.departure_time()),
+      .startTime_ = to_ms(j.departure_time()),
+      .endTime_ = to_ms(j.arrival_time()),
+      .transfers_ = std::max(
+          0L, utl::count_if(j.legs_, [](auto&& leg) {
+                return holds_alternative<n::routing::journey::run_enter_exit>(
+                    leg.uses_);
+              }) - 1)};
+
+  for (auto const [leg_i, j_leg] : utl::enumerate(j.legs_)) {
+    auto const write_leg = [&](auto&& x,
+                               api::ModeEnum const mode) -> api::Leg& {
+      auto& leg = itinerary.legs_.emplace_back();
+      leg.mode_ = mode;
+      leg.from_ = to_place(tt, j_leg.from_);
+      leg.to_ = to_place(tt, j_leg.to_);
+      leg.from_.departure_ = leg.startTime_ = to_ms(j_leg.dep_time_);
+      leg.to_.arrival_ = leg.endTime_ = to_ms(j_leg.arr_time_);
+      leg.duration_ = to_seconds(j_leg.arr_time_ - j_leg.dep_time_);
+      return leg;
+    };
+
     std::visit(
-        utl::overloaded{[&](n::routing::journey::run_enter_exit const& t) {},
-                        [&](n::footpath const fp) {},  //
-                        [&](n::routing::offset const x) {}},
-        leg.uses_);
-    legs.emplace_back(api::Leg{
-        .startTime_ = to_unixtime_ms(leg.dep_time_),
-        .endTime_ = to_unixtime_ms(leg.arr_time_),
-        .departureDelay_ = 0,  // TODO
-        .arrivalDelay_ = 0,  // TODO
-        .realTime_ = false,  // TODO
-        .distance_ = 0.0,  // TODO
-        .interlineWithPreviousLeg_ = false,  // TODO
-        .route_ = "",  // TODO
-        .headsign_ = "",  // TODO
-        .agencyName_ = "",  // TODO
-        .agencyUrl_ = "",  // TODO
-        .routeColor_ = "",  // TODO
-        .routeTextColor_ = "",  // TODO
-        .routeType_ = "",  // TODO
-        .routeId_ = "",  // TODO
-        .agencyId_ = "",  // TODO
-        .tripId_ = "",  // TODO
-        .serviceDate_ = "",  // TODO
-        .duration_ = to_seconds(leg.arr_time_ - leg.dep_time_),
-        .intermediateStops_ = {},  // TODO
-        .from_ = api::Place{},  // TODO
-        .to_ = api::Place{},  // TODO
-        .legGeometry_ = api::EncodedPolyline{},  // TODO
-        .steps_ = {},  // TODO
-        .mode_ = api::ModeEnum::TRANSIT  // TODO
-    });
+        utl::overloaded{
+            [&](n::routing::journey::run_enter_exit const& t) {
+              // TODO interlining
+              auto const fr = n::rt::frun{tt, rtt, t.r_};
+              auto const enter_stop = fr[t.stop_range_.from_];
+              auto const exit_stop = fr[t.stop_range_.to_ - 1U];
+              auto const color =
+                  enter_stop.get_route_color(n::event_type::kDep);
+              auto const agency = enter_stop.get_provider(n::event_type::kDep);
+
+              auto& leg = write_leg(t, api::ModeEnum::TRANSIT);
+              leg.headsign_ = enter_stop.direction(n::event_type::kDep);
+              leg.routeColor_ = to_str(color.color_);
+              leg.routeTextColor_ = to_str(color.text_color_);
+              leg.realTime_ = fr.is_rt();
+              leg.tripId_ = fr.id().id_;  // TODO source_idx
+              leg.agencyName_ = agency.long_name_;
+              leg.agencyId_ = agency.short_name_;
+              leg.departureDelay_ =
+                  to_ms(enter_stop.delay(n::event_type::kDep));
+              leg.arrivalDelay_ = to_ms(exit_stop.delay(n::event_type::kArr));
+
+              for (auto i = t.stop_range_.from_ + 1U;
+                   i < t.stop_range_.to_ - 2U; ++i) {
+                auto const stop = fr[t.stop_range_.from_];
+                auto& p = leg.intermediateStops_.emplace_back(
+                    to_place(tt, stop.get_location_idx()));
+                p.departure_ = to_ms(stop.time(n::event_type::kDep));
+                p.departureDelay_ = to_ms(stop.delay(n::event_type::kDep));
+                p.arrival_ = to_ms(stop.time(n::event_type::kDep));
+                p.arrivalDelay_ = to_ms(stop.delay(n::event_type::kDep));
+              }
+            },
+            [&](n::footpath const fp) { write_leg(fp, api::ModeEnum::WALK); },
+            [&](n::routing::offset const x) {
+              write_leg(x, api::ModeEnum{x.transport_mode_id_});
+            }},
+        j_leg.uses_);
   }
-  return {.duration_ = to_seconds(j.arrival_time() - j.departure_time()),
-          .startTime_ = to_unixtime_ms(j.departure_time()),
-          .endTime_ = to_unixtime_ms(j.arrival_time()),
-          .walkTime_ = 0,  // TODO
-          .walkDistance_ = 0,  // TODO
-          .transfers_ = 0,  // TODO
-          .legs_ = std::move(legs)};
+
+  return itinerary;
 }
 
 }  // namespace icc
