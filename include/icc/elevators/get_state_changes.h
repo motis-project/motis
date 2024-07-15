275523adabff688f062b710706f17bc6e5c11e3f
Felix Gündling <felix.guendling@gmail.com>
2024-07-15 08:18:44 +0200

    wip

---
+++ include/icc/elevators/get_state_changes.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <ranges>
+#include <vector>
+
+#include "utl/enumerate.h"
+#include "utl/generator.h"
+#include "utl/to_vec.h"
+#include "utl/verify.h"
+
+namespace icc {
+
+template <typename Time>
+struct state_change {
+  Time valid_from_;
+  bool state_;
+};
+
+template <typename Time>
+utl::generator<std::pair<Time, std::vector<bool>>> get_state_changes(
+    std::vector<std::vector<state_change<Time>>> const& c) {
+  using It = std::vector<state_change<Time>>::const_iterator;
+
+  struct range {
+    bool is_finished() const { return curr_ == end_; }
+    bool state_;
+    It curr_, end_;
+  };
+
+  auto its = utl::to_vec(c, [](auto&& v) {
+    utl::verify(!v.empty(), "empty state vector not allowed");
+    return range{v[0].state_, v.begin(), v.end()};
+  });
+
+  auto const all_finished = [&]() {
+    return std::ranges::all_of(its, [&](auto&& r) { return r.is_finished(); });
+  };
+
+  auto const next = [&]() -> range& {
+    return *std::ranges::min_element(its, [&](auto&& a, auto&& b) {
+      return a.curr_->valid_from_ < b.curr_->valid_from_;
+    });
+  };
+
+  auto const get_state = [&]() -> std::vector<bool> {
+    auto s = std::vector<bool>(its.size());
+    for (auto const [i, r] : utl::enumerate(its)) {
+      s[i] = r.state_;
+    }
+    return s;
+  };
+
+  auto pred_t = std::optional<std::pair<Time, std::vector<bool>>>{};
+  while (!all_finished()) {
+    auto& n = next();
+    auto const t = n.curr_->valid_from_;
+    n.state_ = n.curr_->state_;
+    ++n.curr_;
+
+    auto const state = std::pair{t, get_state()};
+    if (!pred_t.has_value()) {
+      pred_t = state;
+      continue;
+    }
+
+    if (pred_t->first != state.first) {
+      co_yield *pred_t;
+    }
+    pred_t = state;
+  }
+  
+  if (pred_t.has_value()) {
+    co_yield *pred_t;
+  }
+}
+
+}  // namespace icc
---
+++ test/combinations_test.cc
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+
+#include "icc/elevators/get_state_changes.h"
+
+using namespace icc;
+
+std::ostream& operator<<(std::ostream& out, std::vector<bool> const& v) {
+  auto first = true;
+  for (auto const b : v) {
+    if (!first) {
+      out << ", ";
+    }
+    first = false;
+    out << std::boolalpha << b;
+  }
+  return out;
+}
+
+TEST(icc, int_state_changes) {
+  auto const changes = std::vector<std::vector<state_change<int>>>{
+      {{0, true}, {10, false}, {20, true}},
+      {{0, false},
+       {5, true},
+       {10, false},
+       {15, true},
+       {20, false},
+       {25, true},
+       {30, false}}};
+  auto g = get_state_changes<int>(changes);
+  auto const expected = std::array<std::pair<int, std::vector<bool>>, 7>{
+      std::pair<int, std::vector<bool>>{0, {true, false}},
+      std::pair<int, std::vector<bool>>{5, {true, true}},
+      std::pair<int, std::vector<bool>>{10, {false, false}},
+      std::pair<int, std::vector<bool>>{15, {false, true}},
+      std::pair<int, std::vector<bool>>{20, {true, false}},
+      std::pair<int, std::vector<bool>>{25, {true, true}},
+      std::pair<int, std::vector<bool>>{30, {true, false}}};
+  auto i = 0U;
+  while (g) {
+    auto const [t, states] = g();
+    std::cerr << t << ": " << states << " VS " << expected[i++] << "\n";
+    //    EXPECT_EQ(expected[i++], g());
+  }
+  EXPECT_EQ(i, expected.size());
+}
