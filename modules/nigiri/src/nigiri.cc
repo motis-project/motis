ba5940a85ecc4e81c893bfcffd7e71056dad6480
mority <mority@posteo.net>
2024-09-06 12:51:06 +0200

    merge duplicates params

--- modules/nigiri/src/nigiri.cc
+++ modules/nigiri/src/nigiri.cc
@@ -129,8 +129,12 @@ nigiri::nigiri() : module("Next Generation Routing", "nigiri") {
   param(no_cache_, "no_cache", "disable timetable caching");
   param(adjust_footpaths_, "adjust_footpaths",
         "adjust footpaths if they are too fast for the distance");
-  param(merge_duplicates_, "match_duplicates",
-        "match and merge duplicate trips");
+  param(merge_dupes_inter_src_, "match_duplicates",
+        "match and merge duplicate trips from different timetable sources");
+  param(merge_dupes_intra_src_, "merge_dupes_intra_src",
+        "match and merge duplicate trips with a single timetable source");
+  param(merge_dupes_inter_src_, "merge_dupes_inter_src",
+        "match and merge duplicate trips from different timetable sources");
   param(max_footpath_length_, "max_footpath_length",
         "maximum footpath length in minutes");
   param(first_day_, "first_day",
@@ -448,7 +452,7 @@ void nigiri::import(motis::module::import_dispatcher& reg) {
                 (*loader)->load({.link_stop_distance_ = link_stop_distance_,
                                  .default_tz_ = default_timezone_},
                                 src, *dir, **impl_->tt_, traffic_day_bitfields,
-                                nullptr);
+                                nullptr, nullptr);
                 progress_tracker->status("FINISHED").show_progress(false);
               } catch (std::exception const& e) {
                 progress_tracker->status(fmt::format("ERROR: {}", e.what()))
@@ -462,7 +466,8 @@ void nigiri::import(motis::module::import_dispatcher& reg) {
             }
 
             n::loader::finalize(**impl_->tt_, adjust_footpaths_,
-                                merge_duplicates_, max_footpath_length_);
+                                merge_dupes_intra_src_, merge_dupes_inter_src_,
+                                max_footpath_length_);
 
             if (no_cache_) {
               loaded = true;
