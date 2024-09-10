9d038257739f7f0f2f4e09fe43cf5ce9ce418c76
Pablo Hoch <pablo@melaxis.com>
2024-09-10 17:05:22 +0200

    add more buckets for routing time histograms

--- modules/nigiri/src/metrics.cc
+++ modules/nigiri/src/metrics.cc
@@ -3,7 +3,8 @@
 namespace motis::nigiri {
 
 auto const routing_time_buckets = prometheus::Histogram::BucketBoundaries{
-    .05, .1, .25, .5, .75, 1.0, 2.5, 5.0, 7.5, 10.0, 20.0, 30.0};
+    .05, .1,  .25, .5,  .75,  1.0,  2.0,  3.0,  4.0, 5.0,
+    6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 30.0, 45.0, 60.0};
 
 metrics::metrics(prometheus::Registry& registry)
     : registry_{registry},
