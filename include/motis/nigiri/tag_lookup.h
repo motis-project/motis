b550a969cd5da71961631b5c0929a317d5114418
Felix Gündling <felix.guendling@gmail.com>
2024-10-01 17:12:40 +0200

    copy tag lookup from motis

---
+++ include/motis/nigiri/tag_lookup.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#include "nigiri/types.h"
+
+namespace motis {
+
+struct tag_lookup {
+  void add(nigiri::source_idx_t, std::string_view str);
+
+  nigiri::source_idx_t get_src(std::string_view tag) const;
+  std::string_view get_tag(nigiri::source_idx_t) const;
+  std::string_view get_tag_clean(nigiri::source_idx_t) const;
+
+  friend std::ostream& operator<<(std::ostream&, tag_lookup const&);
+
+  nigiri::vecvec<nigiri::source_idx_t, char, std::uint32_t> src_to_tag_;
+  nigiri::hash_map<std::string, nigiri::source_idx_t> tag_to_src_;
+};
+
+}  // namespace motis
