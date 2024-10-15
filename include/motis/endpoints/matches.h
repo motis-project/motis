65f1b6943575589032ec244d3bccb68dcd73d118
Felix Gündling <felixguendling@gmail.com>
2024-10-15 22:35:20 +0200

    debug apis tags lookup (#564)

--- include/motis/endpoints/matches.h
+++ include/motis/endpoints/matches.h
@@ -13,6 +13,7 @@ struct matches {
   boost::json::value operator()(boost::json::value const&) const;
 
   point_rtree<nigiri::location_idx_t> const& loc_rtree_;
+  tag_lookup const& tags_;
   nigiri::timetable const& tt_;
   osr::ways const& w_;
   osr::lookup const& l_;
