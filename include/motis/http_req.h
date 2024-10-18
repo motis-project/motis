5a20b8db71df730ce40e65aa584723654ad7a352
Felix Gündling <felix.guendling@gmail.com>
2024-10-19 00:55:13 +0200

    try improve rt update

--- include/motis/http_req.h
+++ include/motis/http_req.h
@@ -1,14 +1,12 @@
 #pragma once
 
+#include <chrono>
 #include <map>
 #include <string>
 
 #include "boost/asio/awaitable.hpp"
-#include "boost/asio/co_spawn.hpp"
-#include "boost/asio/io_context.hpp"
-#include "boost/beast/core.hpp"
-#include "boost/beast/http.hpp"
-#include "boost/beast/version.hpp"
+#include "boost/beast/http/dynamic_body.hpp"
+#include "boost/beast/http/message.hpp"
 #include "boost/url/url.hpp"
 
 namespace motis {
@@ -17,6 +15,8 @@ using http_response =
     boost::beast::http::response<boost::beast::http::dynamic_body>;
 
 boost::asio::awaitable<http_response> http_GET(
-    boost::urls::url, std::map<std::string, std::string> const& headers);
+    boost::urls::url,
+    std::map<std::string, std::string> const& headers,
+    std::chrono::seconds timeout);
 
 }  // namespace motis
