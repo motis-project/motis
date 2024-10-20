173591c82e78e94303f26e0938c7a0ca46b43930
Felix Gündling <felix.guendling@gmail.com>
2024-10-20 20:23:29 +0200

    make routing work without osr

--- include/motis/endpoints/adr/reverse_geocode.h
+++ include/motis/endpoints/adr/reverse_geocode.h
@@ -2,6 +2,7 @@
 
 #include "motis-api/motis-api.h"
 #include "motis/fwd.h"
+#include "motis/match_platforms.h"
 
 namespace motis::ep {
 
@@ -9,8 +10,11 @@ struct reverse_geocode {
   api::reverseGeocode_response operator()(
       boost::urls::url_view const& url) const;
 
-  nigiri::timetable const& tt_;
-  tag_lookup const& tags_;
+  osr::ways const* w_;
+  osr::platforms const* pl_;
+  platform_matches_t const* matches_;
+  nigiri::timetable const* tt_;
+  tag_lookup const* tags_;
   adr::typeahead const& t_;
   adr::reverse const& r_;
 };
