56cf2a267c0737cf683712233613d697b64de622
Pablo Hoch <pablo@melaxis.com>
2024-11-01 12:24:48 +0100

    remove most debug output, add mode without pipelining

--- include/motis/http_client.h
+++ include/motis/http_client.h
@@ -30,6 +30,13 @@
 namespace motis {
 
 struct http_client {
+  static boost::asio::awaitable<void> timeout(
+      std::chrono::steady_clock::duration const duration) {
+    boost::asio::steady_timer timer(co_await boost::asio::this_coro::executor);
+    timer.expires_after(duration);
+    co_await timer.async_wait();
+  }
+
   struct request {
     template <typename Executor>
     request(boost::urls::url&& url,
@@ -58,8 +65,10 @@ struct http_client {
 
   struct connection : public std::enable_shared_from_this<connection> {
     template <typename Executor>
-    connection(connection_key key, Executor const& executor)
-        : key_{std::move(key)}, request_channel_{executor} {
+    connection(Executor const& executor, connection_key key)
+        : key_{std::move(key)},
+          request_channel_{executor},
+          request_lock_{executor, 1} {
       ssl_ctx_.set_default_verify_paths();
       ssl_ctx_.set_verify_mode(boost::asio::ssl::verify_none);
       ssl_ctx_.set_options(boost::asio::ssl::context::default_workarounds |
@@ -68,20 +77,12 @@ struct http_client {
                            boost::asio::ssl::context::single_dh_use);
     }
 
-    ~connection() {
-      std::cout << "[http] connection dtor(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << ")" << std::endl;
-    }
-
-    void close() {
-      std::cout << "[http] close(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << ")" << std::endl;
-
-      if (key_.ssl_) {
+    void close() const {
+      std::cout << "[http] closing connection to " << key_.host_ << ":"
+                << key_.port_ << ": sent " << n_sent_ << " requests, received "
+                << n_received_ << " responses, " << n_connects_ << " connects"
+                << std::endl;
+      if (ssl()) {
         auto ec = boost::beast::error_code{};
         boost::beast::get_lowest_layer(*ssl_stream_)
             .socket()
@@ -94,20 +95,13 @@ struct http_client {
     }
 
     boost::asio::awaitable<void> connect() {
-      std::cout << "[http] connect(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << ")" << std::endl;
       auto executor = co_await boost::asio::this_coro::executor;
       auto resolver = boost::asio::ip::tcp::resolver{executor};
 
       auto const results =
           co_await resolver.async_resolve(key_.host_, key_.port_);
-      if (key_.ssl_) {
-        std::cout << "[http] connect(" << key_.host_ << ":" << key_.port_
-                  << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): creating ssl stream" << std::endl;
+      ++n_connects_;
+      if (ssl()) {
         ssl_stream_ = std::make_unique<
             boost::asio::ssl::stream<boost::beast::tcp_stream>>(executor,
                                                                 ssl_ctx_);
@@ -123,39 +117,18 @@ struct http_client {
             .async_connect(results);
         co_await ssl_stream_->async_handshake(
             boost::asio::ssl::stream_base::client);
-        std::cout << "[http] connect(" << key_.host_ << ":" << key_.port_
-                  << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): ssl stream established" << std::endl;
       } else {
-        std::cout << "[http] connect(" << key_.host_ << ":" << key_.port_
-                  << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): creating tcp stream" << std::endl;
         stream_ = std::make_unique<boost::beast::tcp_stream>(executor);
         co_await stream_->async_connect(results);
-        std::cout << "[http] connect(" << key_.host_ << ":" << key_.port_
-                  << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): tcp stream established" << std::endl;
       }
+
+      request_lock_.try_receive([](auto const&) {});
     }
 
     boost::asio::awaitable<void> send_requests() {
-      std::cout << "[http] send_requests(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << ")" << std::endl;
       try {
         auto const send_request = [&](std::shared_ptr<request> request)
             -> boost::asio::awaitable<void> {
-          std::cout << "[http] send_requests(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): got request: " << request->url_.encoded_target()
-                    << std::endl;
-
           auto req =
               boost::beast::http::request<boost::beast::http::string_body>{
                   boost::beast::http::verb::get, request->url_.encoded_target(),
@@ -169,37 +142,26 @@ struct http_client {
           }
           req.keep_alive(true);
 
-          if (key_.ssl_) {
+          if (!pipelining_) {
+            co_await request_lock_.async_send(boost::system::error_code{});
+          }
+
+          if (ssl()) {
             co_await boost::beast::http::async_write(*ssl_stream_, req);
           } else {
             co_await boost::beast::http::async_write(*stream_, req);
           }
           ++n_sent_;
-          std::cout << "[http] send_requests(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size() << "): request sent"
-                    << std::endl;
         };
 
         if (!pending_requests_.empty()) {
-          std::cout << "[http] send_requests(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): processing pending requests after reconnect"
-                    << std::endl;
+          // after reconnect, send pending requests again
           for (auto const& request : pending_requests_) {
             co_await send_request(request);
           }
         }
 
         while (request_channel_.is_open()) {
-          std::cout << "[http] send_requests(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): waiting for request" << std::endl;
           auto request = co_await request_channel_.async_receive();
           pending_requests_.push_back(request);
           co_await send_request(request);
@@ -209,59 +171,33 @@ struct http_client {
                   << std::endl;
         // throw ex;
       }
-      std::cout << "[http] send_requests(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << "): END" << std::endl;
     }
 
     boost::asio::awaitable<void> receive_responses() {
-      std::cout << "[http] receive_response(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << ")" << std::endl;
       try {
         for (;;) {
           auto buffer = boost::beast::flat_buffer{};
           auto res =
               boost::beast::http::response<boost::beast::http::dynamic_body>{};
 
-          std::cout << "[http] receive_response(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): waiting for response" << std::endl;
-          if (key_.ssl_) {
+          if (ssl()) {
             co_await boost::beast::http::async_read(*ssl_stream_, buffer, res);
           } else {
             co_await boost::beast::http::async_read(*stream_, buffer, res);
           }
           ++n_received_;
 
-          std::cout << "[http] receive_response(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): response received, connection header: "
-                    << res[boost::beast::http::field::connection] << std::endl;
+          if (!pipelining_) {
+            request_lock_.try_receive([](auto const&) {});
+          }
 
           utl::verify(!pending_requests_.empty(),
                       "received response without pending request");
           auto req = pending_requests_.front();
           pending_requests_.pop_front();
 
-          std::cout << "[http] receive_response(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): finishing pending request" << std::endl;
           co_await req->response_channel_.async_send(
               boost::system::error_code{}, std::move(res));
-          std::cout << "[http] receive_response(" << key_.host_ << ":"
-                    << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                    << ", r=" << n_received_
-                    << ", p=" << pending_requests_.size()
-                    << "): pending request finished" << std::endl;
         }
       } catch (std::exception const& ex) {
         std::cout << "[http] exception in receive_responses: " << ex.what()
@@ -269,72 +205,43 @@ struct http_client {
         // throw ex;
       }
       // TODO: boost::system::system_error
-      std::cout << "[http] receive_response(" << key_.host_ << ":" << key_.port_
-                << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << "): END" << std::endl;
     }
 
     boost::asio::awaitable<void> run() {
       using namespace boost::asio::experimental::awaitable_operators;
-      std::cout << "[http] handle_connection(" << key_.host_ << ":"
-                << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << ")" << std::endl;
       auto const self = shared_from_this();
       do {
-        std::cout << "[http] handle_connection(" << key_.host_ << ":"
-                  << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): (re)connecting" << std::endl;
         co_await self->connect();
-        std::cout << "[http] handle_connection(" << key_.host_ << ":"
-                  << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): connected, running send_requests + receive_responses"
-                  << std::endl;
         co_await (self->receive_responses() || self->send_requests());
-        std::cout << "[http] handle_connection(" << key_.host_ << ":"
-                  << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                  << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                  << "): send / receive done" << std::endl;
+        close();
       } while (!pending_requests_.empty());
-      std::cout << "[http] handle_connection(" << key_.host_ << ":"
-                << key_.port_ << ", ssl=" << key_.ssl_ << ", s=" << n_sent_
-                << ", r=" << n_received_ << ", p=" << pending_requests_.size()
-                << "): END" << std::endl;
     }
 
+    bool ssl() const { return key_.ssl_; }
+
     connection_key key_{};
+    bool pipelining_{false};
     std::unique_ptr<boost::beast::tcp_stream> stream_;
     std::unique_ptr<boost::asio::ssl::stream<boost::beast::tcp_stream>>
         ssl_stream_;
     boost::asio::experimental::channel<void(boost::system::error_code,
                                             std::shared_ptr<request>)>
         request_channel_;
+    boost::asio::experimental::channel<void(boost::system::error_code)>
+        request_lock_;
     std::deque<std::shared_ptr<request>> pending_requests_;
     boost::asio::ssl::context ssl_ctx_{
         boost::asio::ssl::context::tlsv12_client};
 
     unsigned n_sent_{};
     unsigned n_received_{};
+    unsigned n_connects_{};
   };
 
-  http_client() { std::cout << "[http] client ctor" << std::endl; }
-
   ~http_client() {
-    std::cout << "[http] client dtor" << std::endl;
     for (auto const& [key, conn] : connections_) {
       conn->close();
     }
-    std::cout << "[http] client dtor END" << std::endl;
-  }
-
-  boost::asio::awaitable<void> timeout(
-      std::chrono::steady_clock::duration duration) {
-    boost::asio::steady_timer timer(co_await boost::asio::this_coro::executor);
-    timer.expires_after(duration);
-    co_await timer.async_wait();
   }
 
   boost::asio::awaitable<http_response> get(
@@ -350,34 +257,16 @@ struct http_client {
 
     auto executor = co_await boost::asio::this_coro::executor;
     if (auto const it = connections_.find(key); it == connections_.end()) {
-      std::cout << "[http] get(" << key.host_ << ":" << key.port_
-                << ", ssl=" << key.ssl_
-                << "): connection not found, spawning new connection"
-                << std::endl;
-      auto conn = std::make_shared<connection>(key, executor);
+      auto conn = std::make_shared<connection>(executor, key);
       connections_[key] = conn;
-
       boost::asio::co_spawn(executor, conn->run(), boost::asio::detached);
-
-      std::cout << "[http] get(" << key.host_ << ":" << key.port_
-                << ", ssl=" << key.ssl_ << "): connection spawned" << std::endl;
-    } else {
-      std::cout << "[http] get(" << key.host_ << ":" << key.port_
-                << ", ssl=" << key.ssl_ << "): existing connection found"
-                << std::endl;
     }
 
     auto req =
         std::make_shared<request>(std::move(url), std::move(headers), executor);
-    std::cout << "[http] get(" << key.host_ << ":" << key.port_
-              << ", ssl=" << key.ssl_ << "): writing request" << std::endl;
     co_await connections_[key]->request_channel_.async_send(
         boost::system::error_code{}, req);
-    std::cout << "[http] get(" << key.host_ << ":" << key.port_
-              << ", ssl=" << key.ssl_ << "): waiting for response" << std::endl;
     auto response = co_await req->response_channel_.async_receive();
-    std::cout << "[http] get(" << key.host_ << ":" << key.port_
-              << ", ssl=" << key.ssl_ << "): response received" << std::endl;
     co_return response;
   }
 
