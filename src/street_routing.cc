740c95f57924a62b1d822fc4004fbd4155ac30d6
Felix Gündling <felix.guendling@gmail.com>
2024-11-02 21:43:42 +0100

    wip

--- src/street_routing.cc
+++ src/street_routing.cc
@@ -65,8 +65,8 @@ std::vector<api::StepInstruction> get_step_instructions(
         .relativeDirection_ = api::RelativeDirectionEnum::CONTINUE,  // TODO
         .absoluteDirection_ = api::AbsoluteDirectionEnum::NORTH,  // TODO
         .distance_ = static_cast<double>(s.dist_),
-        .fromLevel_ = to_float(s.from_level_),
-        .toLevel_ = to_float(s.to_level_),
+        .fromLevel_ = s.from_level_.to_float(),
+        .toLevel_ = s.to_level_.to_float(),
         .osmWay_ = s.way_ == osr::way_idx_t ::invalid()
                        ? std::nullopt
                        : std::optional{static_cast<std::int64_t>(
