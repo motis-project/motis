eece6ebdecc64a7560bec87a4bb62ab2980b9c60
Nicolas Derive <nicolas.derive-ext@aphp.fr>
2024-11-21 11:20:12 +0100

    Add French translation.

---
+++ ui/src/lib/i18n/fr.ts
@@ -0,0 +1,33 @@
+import type { Translations } from './translation';
+
+const translations: Translations = {
+	walk: 'Walk',
+	bike: 'Bike',
+	car: 'Car',
+	from: 'From',
+	to: 'To',
+	arrival: 'Arrival',
+	departure: 'Departure',
+	duration: 'Duration',
+	arrivals: 'Arrivals',
+	later: 'later',
+	earlier: 'earlier',
+	departures: 'Departures',
+	switchToArrivals: 'Wechsel zu Ankünften',
+	switchToDepartures: 'Wechsel zu Abfahrten',
+	track: 'Track',
+	arrivalOnTrack: 'Arrival on track',
+	tripIntermediateStops: (n: number) => {
+		switch (n) {
+			case 0:
+				return 'No intermediate stops';
+			case 1:
+				return '1 intermediate stop';
+			default:
+				return `${n}  intermediate stops`;
+		}
+	},
+	sharingProvider: 'Provider'
+};
+
+export default translations;
