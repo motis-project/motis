1f1fc39ba7fa2cd2a867b6764e84682c9ea6bd8c
mority <mority@posteo.net>
2024-12-11 12:37:51 +0100

    wip

--- src/endpoints/routing.cc
+++ src/endpoints/routing.cc
@@ -468,6 +468,20 @@ api::plan_response routing::operator()(boost::urls::url_view const& url) const {
           : std::pair{std::vector<api::Itinerary>{}, kInfinityDuration};
   UTL_STOP_TIMING(direct);
 
+  auto const odm_pre_transit = std::find(begin(pre_transit_modes), end(pre_transit_modes), api::ModeEnum::ODM) != end(pre_transit_modes);
+  auto const odm_post_transit = std::find(begin(post_transit_modes), end(post_transit_modes), api::ModeEnum::ODM) != end(post_transit_modes);
+  auto const odm_direct = std::find(begin(direct_modes), end(direct_modes), api::ModeEnum::ODM) != end(direct_modes);
+  auto const odm_any = odm_pre_transit || odm_post_transit || odm_direct;
+  auto odm_journeys = std::optional<std::vector<n::routing::journey>>{}; // this should probably be the future, we get later, i.e., once regular routing is finished
+
+  // if ODM mode set: spawn fiber for ODM start/dest offset generation
+  auto const odm_routing = [&]()  {
+
+  };
+  if(odm_any) {
+
+  }
+
   if (!query.transitModes_.empty() && fastest_direct > 5min) {
     utl::verify(tt_ != nullptr && tags_ != nullptr,
                 "mode=TRANSIT requires timetable to be loaded");
@@ -598,6 +612,8 @@ api::plan_response routing::operator()(boost::urls::url_view const& url) const {
         query.timeout_.has_value()
             ? std::optional<std::chrono::seconds>{*query.timeout_}
             : std::nullopt);
+
+    // if ODM mode set extend routing result with ODM journeys, so we join the fiber here again
 
     return {
         .debugOutput_ = join(
