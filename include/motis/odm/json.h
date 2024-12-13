e6a7e960336f1b2eac4e41c817fa9f52033862b0
mority <mority@posteo.net>
2024-12-13 17:31:45 +0100

    wip

--- include/motis/odm/json.h
+++ include/motis/odm/json.h
@@ -1,3 +1,12 @@
 #pragma once
 
-namespace motis::ep {}  // namespace motis::ep
+#include <string>
+
+// TODO or use boost json?
+#include "rapidjson/document.h"
+
+namespace motis::ep {
+
+std::string blacklist_payload() { return std::string{}; }
+
+}  // namespace motis::ep
