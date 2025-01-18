3c361bb4dfaeee97f6d8222b40dd50b01ec155c1
Traines <git@traines.eu>
2025-01-19 00:22:06 +0100

    Fix index out of range for stop_times last arrival of multi-section run (#704)
    * fix index out of range for stop_times last arrival of multi-section run
    
    * follow style guide
    
    Co-authored-by: Felix Gündling <felix.guendling@gmail.com>
    
    ---------
    
    Co-authored-by: Felix Gündling <felix.guendling@gmail.com>

--- include/motis/tag_lookup.h
+++ include/motis/tag_lookup.h
@@ -18,7 +18,9 @@ struct tag_lookup {
   nigiri::source_idx_t get_src(std::string_view tag) const;
   std::string_view get_tag(nigiri::source_idx_t) const;
   std::string id(nigiri::timetable const&, nigiri::location_idx_t) const;
-  std::string id(nigiri::timetable const&, nigiri::rt::run_stop) const;
+  std::string id(nigiri::timetable const&,
+                 nigiri::rt::run_stop,
+                 nigiri::event_type) const;
   nigiri::location_idx_t get_location(nigiri::timetable const&,
                                       std::string_view) const;
   std::pair<nigiri::rt::run, nigiri::trip_idx_t> get_trip(
