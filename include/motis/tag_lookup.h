0211dae2cab356b1a6d8da78ade671f0532c7a09
Traines <git@traines.eu>
2025-01-19 00:02:36 +0100

    follow style guide
    Co-authored-by: Felix Gündling <felix.guendling@gmail.com>

--- include/motis/tag_lookup.h
+++ include/motis/tag_lookup.h
@@ -20,7 +20,7 @@ struct tag_lookup {
   std::string id(nigiri::timetable const&, nigiri::location_idx_t) const;
   std::string id(nigiri::timetable const&,
                  nigiri::rt::run_stop,
-                 nigiri::event_type const) const;
+                 nigiri::event_type) const;
   nigiri::location_idx_t get_location(nigiri::timetable const&,
                                       std::string_view) const;
   std::pair<nigiri::rt::run, nigiri::trip_idx_t> get_trip(
