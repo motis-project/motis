503f53fb300bef1afc4008d24a2c3841395d1e87
mority <mority@posteo.net>
2025-02-06 14:57:11 +0100

    wip

--- src/odm/mixer.cc
+++ src/odm/mixer.cc
@@ -139,7 +139,7 @@ void mixer::productivity_domination(
 void mixer::mix(n::pareto_set<n::routing::journey> const& pt_journeys,
                 std::vector<n::routing::journey>& odm_journeys) const {
   cost_domination(pt_journeys, odm_journeys);
-  // productivity_domination(odm_journeys);
+  productivity_domination(odm_journeys);
   odm_journeys.append_range(pt_journeys);
   utl::sort(odm_journeys, [](auto const& a, auto const& b) {
     return a.departure_time() < b.departure_time();
