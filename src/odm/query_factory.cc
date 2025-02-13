c9261a7cf7829296cfcbbf7e4c3823b5d9cdd794
mority <mority@posteo.net>
2025-02-13 15:21:54 +0100

    Disables taxi-taxi subqueries

--- src/odm/query_factory.cc
+++ src/odm/query_factory.cc
@@ -16,10 +16,10 @@ std::vector<n::routing::query> query_factory::make_queries(
     queries.push_back(make(start_walk_, td_start_walk_, {}, odm_dest_long_));
     queries.push_back(make({}, odm_start_short_, dest_walk_, td_dest_walk_));
     queries.push_back(make({}, odm_start_long_, dest_walk_, td_dest_walk_));
-    queries.push_back(make({}, odm_start_short_, {}, odm_dest_short_));
-    queries.push_back(make({}, odm_start_short_, {}, odm_dest_long_));
-    queries.push_back(make({}, odm_start_long_, {}, odm_dest_short_));
-    queries.push_back(make({}, odm_start_long_, {}, odm_dest_long_));
+    //    queries.push_back(make({}, odm_start_short_, {}, odm_dest_short_));
+    //    queries.push_back(make({}, odm_start_short_, {}, odm_dest_long_));
+    //    queries.push_back(make({}, odm_start_long_, {}, odm_dest_short_));
+    //    queries.push_back(make({}, odm_start_long_, {}, odm_dest_long_));
   }
   return queries;
 }
