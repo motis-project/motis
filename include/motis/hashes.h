7c11514a3d9250232bdb7a0f31455844148154d5
Felix Gündling <felix.guendling@gmail.com>
2025-03-06 17:17:43 +0100

    nigiri: clamp coordinates

--- include/motis/hashes.h
+++ include/motis/hashes.h
@@ -17,7 +17,7 @@ constexpr auto const adr_version = []() {
   return meta_entry_t{"adr_bin_ver", 4U};
 };
 constexpr auto const n_version = []() {
-  return meta_entry_t{"nigiri_bin_ver", 8U};
+  return meta_entry_t{"nigiri_bin_ver", 9U};
 };
 constexpr auto const matches_version = []() {
   return meta_entry_t{"matches_bin_ver", 4U};
