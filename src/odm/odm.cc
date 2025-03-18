d49d51a40154fd0045f1d6488e932842068d7e75
Moritz Fischer <57353722+mority@users.noreply.github.com>
2025-03-18 15:24:42 +0100

    Merge pull request #782 from mority/direct-odm
    Relax direct odm penalty

--- src/odm/odm.cc
+++ src/odm/odm.cc
@@ -1,5 +1,7 @@
 #include "motis/odm/odm.h"
 
+#include <ranges>
+
 #include "nigiri/routing/journey.h"
 
 #include "motis/transport_mode_ids.h"
@@ -15,21 +17,23 @@ bool is_odm_leg(nr::journey::leg const& l) {
              kOdmTransportModeId;
 }
 
-n::duration_t odm_time(nr::journey const& j) {
-  return j.legs_.empty() ? n::duration_t{0}
-         : is_odm_leg(j.legs_.front())
-             ? std::get<nr::offset>(j.legs_.front().uses_).duration()
-             : n::duration_t{0} +
-                   ((j.legs_.size() > 1 && is_odm_leg(j.legs_.back()))
-                        ? std::get<nr::offset>(j.legs_.back().uses_).duration()
-                        : n::duration_t{0});
-}
+bool uses_odm(nr::journey const& j) { return utl::any_of(j.legs_, is_odm_leg); }
+
+bool is_pure_pt(nr::journey const& j) { return !uses_odm(j); };
 
 bool is_direct_odm(nr::journey const& j) {
-  return j.legs_.size() == 1U &&
-         std::holds_alternative<nr::offset>(j.legs_.front().uses_) &&
-         std::get<nr::offset>(j.legs_.front().uses_).transport_mode_id_ ==
-             kOdmTransportModeId;
+  return j.legs_.size() == 1U && uses_odm(j);
+}
+
+n::duration_t odm_time(nr::journey::leg const& l) {
+  return is_odm_leg(l) ? std::get<nr::offset>(l.uses_).duration()
+                       : n::duration_t{0};
+}
+
+n::duration_t odm_time(nr::journey const& j) {
+  return std::transform_reduce(begin(j.legs_), end(j.legs_), n::duration_t{0},
+                               std::plus{},
+                               [](auto const& l) { return odm_time(l); });
 }
 
 }  // namespace motis::odm
