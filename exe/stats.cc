828c58811856c191a9f08ce3d5f01b7e79cb63a6
mority <mority@posteo.net>
2025-04-05 00:11:13 +0200

    refactor

--- exe/stats.cc
+++ exe/stats.cc
@@ -55,7 +55,6 @@ int stats(int ac, char** av) {
     }
     return f;
   };
-
   auto const read_line = [](std::ifstream& f) -> std::optional<std::string> {
     if (f.peek() == EOF || f.eof()) {
       return std::nullopt;
@@ -64,78 +63,66 @@ int stats(int ac, char** av) {
     std::getline(f, line);
     return line;
   };
-
   auto responses_files =
       utl::to_vec(responses_paths, [&](auto&& p) { return open_file(p); });
-
-  struct stats {
-    std::vector<std::chrono::milliseconds> wall_time_{};
-    std::vector<std::chrono::minutes> travel_time_{};
-    std::vector<std::uint8_t> transfers_{};
-    std::vector<std::chrono::minutes> walk_time_{};
-    std::vector<std::chrono::minutes> bike_time_{};
-    std::vector<std::chrono::minutes> car_time_{};
-  };
-
-  auto s = std::vector<stats>{responses_paths.size()};
+  std::vector<std::vector<std::chrono::milliseconds>> wall_time{
+      responses_paths.size()};
+  std::vector<std::vector<std::chrono::minutes>> travel_time{
+      responses_paths.size()};
+  std::vector<std::vector<std::uint8_t>> transfers{responses_paths.size()};
+  std::vector<std::vector<std::chrono::minutes>> walk_time{
+      responses_paths.size()};
+  std::vector<std::vector<std::chrono::minutes>> bike_time{
+      responses_paths.size()};
+  std::vector<std::vector<std::chrono::minutes>> car_time{
+      responses_paths.size()};
   for (auto const [i, res_file] : utl::enumerate(responses_files)) {
     fmt::println("({}) {}", i, responses_paths[i]);
     while (auto const r = read_line(res_file)) {
       auto res =
           boost::json::value_to<api::plan_response>(boost::json::parse(*r));
-      s[i].wall_time_.emplace_back(res.debugOutput_["execute_time"]);
+      wall_time[i].emplace_back(res.debugOutput_["execute_time"]);
       for (auto const& j : res.itineraries_) {
-        s[i].travel_time_.emplace_back(std::chrono::round<std::chrono::minutes>(
+        travel_time[i].emplace_back(std::chrono::round<std::chrono::minutes>(
             std::chrono::seconds{j.duration_}));
-        s[i].transfers_.emplace_back(j.transfers_);
-        switch (j.legs_.front().mode_) {
-          case api::ModeEnum::WALK:
-            s[i].walk_time_.emplace_back(
-                std::chrono::round<std::chrono::minutes>(
-                    std::chrono::seconds{j.legs_.front().duration_}));
-            break;
-          case api::ModeEnum::BIKE:
-            s[i].bike_time_.emplace_back(
-                std::chrono::round<std::chrono::minutes>(
-                    std::chrono::seconds{j.legs_.front().duration_}));
-            break;
-          case api::ModeEnum::CAR:
-            s[i].car_time_.emplace_back(
-                std::chrono::round<std::chrono::minutes>(
-                    std::chrono::seconds{j.legs_.front().duration_}));
-            break;
-          default: break;
-        }
-        switch (j.legs_.back().mode_) {
-          case api::ModeEnum::WALK:
-            s[i].walk_time_.emplace_back(
-                std::chrono::round<std::chrono::minutes>(
-                    std::chrono::seconds{j.legs_.back().duration_}));
-            break;
-          case api::ModeEnum::BIKE:
-            s[i].bike_time_.emplace_back(
-                std::chrono::round<std::chrono::minutes>(
-                    std::chrono::seconds{j.legs_.back().duration_}));
-            break;
-          case api::ModeEnum::CAR:
-            s[i].car_time_.emplace_back(
-                std::chrono::round<std::chrono::minutes>(
-                    std::chrono::seconds{j.legs_.back().duration_}));
-            break;
-          default: break;
-        }
+        transfers[i].emplace_back(j.transfers_);
+        auto const add_intermodal = [&](auto const& l) {
+          switch (l.mode_) {
+            case api::ModeEnum::WALK:
+              walk_time[i].emplace_back(
+                  std::chrono::round<std::chrono::minutes>(
+                      std::chrono::seconds{l.duration_}));
+              break;
+            case api::ModeEnum::BIKE:
+              bike_time[i].emplace_back(
+                  std::chrono::round<std::chrono::minutes>(
+                      std::chrono::seconds{l.duration_}));
+              break;
+            case api::ModeEnum::CAR:
+              car_time[i].emplace_back(std::chrono::round<std::chrono::minutes>(
+                  std::chrono::seconds{l.duration_}));
+              break;
+            default: break;
+          }
+        };
+        add_intermodal(j.legs_.front());
+        add_intermodal(j.legs_.back());
       }
     }
   }
 
-  for (auto& u : s) {
-    utl::sort(u.wall_time_);
-    utl::sort(u.travel_time_);
-    utl::sort(u.transfers_);
-    utl::sort(u.walk_time_);
-    utl::sort(u.bike_time_);
-    utl::sort(u.car_time_);
-  }
+  auto const sort_all = [](auto& attr) {
+    for (auto& vals : attr) {
+      utl::sort(vals);
+    }
+  };
+  sort_all(wall_time);
+  sort_all(travel_time);
+  sort_all(transfers);
+  sort_all(walk_time);
+  sort_all(bike_time);
+  sort_all(car_time);
+
   auto const cols =
       std::vector<std::string>{"25%", "50%", "75%", "99%", "99.9%", "100%"};
   auto const quantiles = std::vector<double>{0.25, 0.5, 0.75, 0.99, 0.999, 1.0};
@@ -160,72 +147,24 @@ int stats(int ac, char** av) {
     }
     std::cout << "\n";
   };
-
-  fmt::println("\nWall Time");
-  print_header();
-  for (auto const [i, u] : utl::enumerate(s)) {
-    std::cout << std::format("({})", i);
-    if (u.wall_time_.empty()) {
-      std::cout << "\n";
-    } else {
-      print_quantiles(u.wall_time_);
+  auto const print_attr = [&](auto const& attr, std::string_view const name) {
+    fmt::println("\n{}", name);
+    print_header();
+    for (auto const [i, vals] : utl::enumerate(attr)) {
+      std::cout << std::format("({})", i);
+      if (vals.empty()) {
+        std::cout << "\n";
+      } else {
+        print_quantiles(vals);
+      }
     }
-  }
-
-  fmt::println("\nTravel Time");
-  print_header();
-  for (auto const [i, u] : utl::enumerate(s)) {
-    std::cout << std::format("({})", i);
-    if (u.travel_time_.empty()) {
-      std::cout << "\n";
-    } else {
-      print_quantiles(u.travel_time_);
-    }
-  }
-
-  fmt::println("\nTransfers");
-  print_header();
-  for (auto const [i, u] : utl::enumerate(s)) {
-    std::cout << std::format("({})", i);
-    if (u.transfers_.empty()) {
-      std::cout << "\n";
-    } else {
-      print_quantiles(u.transfers_);
-    }
-  }
-
-  fmt::println("\nWalk Time");
-  print_header();
-  for (auto const [i, u] : utl::enumerate(s)) {
-    std::cout << std::format("({})", i);
-    if (u.walk_time_.empty()) {
-      std::cout << "\n";
-    } else {
-      print_quantiles(u.walk_time_);
-    }
-  }
-
-  fmt::println("\nBike Time");
-  print_header();
-  for (auto const [i, u] : utl::enumerate(s)) {
-    std::cout << std::format("({})", i);
-    if (u.bike_time_.empty()) {
-      std::cout << "\n";
-    } else {
-      print_quantiles(u.bike_time_);
-    }
-  }
-
-  fmt::println("\nCar Time");
-  print_header();
-  for (auto const [i, u] : utl::enumerate(s)) {
-    std::cout << std::format("({})", i);
-    if (u.car_time_.empty()) {
-      std::cout << "\n";
-    } else {
-      print_quantiles(u.car_time_);
-    }
-  }
+  };
+  print_attr(wall_time, "Wall Time");
+  print_attr(travel_time, "Travel Time");
+  print_attr(transfers, "Transfers");
+  print_attr(walk_time, "Walk Time");
+  print_attr(bike_time, "Bike Time");
+  print_attr(car_time, "Car Time");
 
   return 0;
 }
