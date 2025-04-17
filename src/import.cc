7829b5cc6e10a8e4c5a7b94ea8aa7ef3f5bad2fc
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-04-17 12:56:32 +0200

    Add hash for osr import

--- src/import.cc
+++ src/import.cc
@@ -160,6 +160,27 @@ data import(config const& c, fs::path const& data_path, bool const write) {
     osm_hash.second = hash_file(*c.osm_);
   }
 
+  auto const elevation_dir =
+      c.get_street_routing()
+          .transform([](config::street_routing const& sr) {
+            return sr.height_data_dir_.value_or(fs::path{});
+          })
+          .value_or(fs::path{});
+  auto elevation_dir_hash = std::pair{"elevation_dir"s, cista::BASE_HASH};
+  if (!elevation_dir.empty() && fs::exists(elevation_dir)) {
+    auto files = std::vector<std::string>{};
+    for (auto const& f : fs::recursive_directory_iterator(elevation_dir)) {
+      if (f.is_regular_file()) {
+        files.emplace_back(f.path().relative_path());
+      }
+    }
+    std::ranges::sort(files);
+    auto& h = elevation_dir_hash.second;
+    for (auto const& f : files) {
+      h = cista::build_hash(h, f);
+    }
+  }
+
   auto tiles_hash = std::pair{"tiles_profile", cista::BASE_HASH};
   if (c.tiles_.has_value()) {
     auto& h = tiles_hash.second;
@@ -171,21 +192,16 @@ data import(config const& c, fs::path const& data_path, bool const write) {
 
   auto d = data{data_path};
 
-  auto osr =
-      task{"osr",
-           [&]() { return c.use_street_routing(); },
-           [&]() { return true; },
-           [&]() {
-             osr::extract(true, fs::path{*c.osm_}, data_path / "osr",
-                          c.get_street_routing()
-                              .transform([](config::street_routing const& sr) {
-                                return sr.height_data_dir_.value_or(fs::path{});
-                              })
-                              .value_or(fs::path{}));
-             d.load_osr();
-           },
-           [&]() { d.load_osr(); },
-           {osm_hash, osr_version()}};
+  auto osr = task{"osr",
+                  [&]() { return c.use_street_routing(); },
+                  [&]() { return true; },
+                  [&]() {
+                    osr::extract(true, fs::path{*c.osm_}, data_path / "osr",
+                                 elevation_dir);
+                    d.load_osr();
+                  },
+                  [&]() { d.load_osr(); },
+                  {osm_hash, osr_version(), elevation_dir_hash}};
 
   auto adr =
       task{"adr",
