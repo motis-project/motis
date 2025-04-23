54bf4352773760e6f5fb7c344cc614011010c2c4
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-04-23 13:12:22 +0200

    Remove duplicated value check

--- src/import.cc
+++ src/import.cc
@@ -162,8 +162,8 @@ data import(config const& c, fs::path const& data_path, bool const write) {
 
   auto const elevation_dir =
       c.get_street_routing()
-          .transform([](config::street_routing const& sr) {
-            return sr.elevation_data_dir_.value_or(fs::path{});
+          .and_then([](config::street_routing const& sr) {
+            return sr.elevation_data_dir_;
           })
           .value_or(fs::path{});
   auto elevation_dir_hash = std::pair{"elevation_dir"s, cista::BASE_HASH};
