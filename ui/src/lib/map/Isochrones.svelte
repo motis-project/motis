f83c448314e7a1dc431d63d16dd4054872c77960
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-05-21 14:30:49 +0200

    WIP: Show better circles

--- ui/src/lib/map/Isochrones.svelte
+++ ui/src/lib/map/Isochrones.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import circle from "@turf/circle";
 	import maplibregl, { type LngLatBoundsLike } from "maplibre-gl";
+
+    interface Pos {
+        lat: number;
+        lng: number;
+        duration: number;
+        // name?: string;
+    };
 
 	let {
 		bounds,
@@ -24,6 +32,51 @@
 		console.log(`BOX VIEW: ${bv}  –  ${b}`);
 		return bv;
 	});
+
+	const factor = 0.06;  // TODO Calculate factor for duration ~~> radius (in kilometers)
+	const box2 = $derived(maplibregl.LngLatBounds.convert(bounds ? bounds : [[0, 0], [0, 0]]));
+    function reachable_kilemeters(pos: Pos) {
+        return pos.duration * factor;
+    }
+    function is_visible(data: Pos) {
+        const r = reachable_kilemeters(data);
+        return box2._sw.lat <= data.lat + r && data.lat - r <= box2._ne.lat
+			&& box2._sw.lng <= data.lng + r && data.lng - r <= box2._ne.lat;
+    }
+	function transform(p: number[]) {
+		return [p[0] - box2._sw.lng, box2._ne.lat - p[1]]
+	}
+	const circles2 = $derived(
+		isochronesData
+			.filter(is_visible)
+            .map((data) => {
+                const r = reachable_kilemeters(data);
+                const c = circle(
+                    [data.lng, data.lat],
+                    r,
+                    {
+                        // steps: steps,
+                        // steps: 10,
+                        // steps: 3,
+                        units: "kilometers",
+                        properties: {
+                            // "name": data.name ?? 'unknown',
+                        }
+                    }
+                );
+				const coords = c.geometry.coordinates[0].map(transform);
+				// console.log(coords);
+				// console.log(c.geometry.coordinates);
+				// const x = c.geometry.coordinates.map(p => p.map(q => q.toString())).join(' ');
+				// const x = c.geometry.coordinates.map(p => p.join(',')).join(' ') + ' ' + c.geometry.coordinates[0][0] + ',' + c.geometry.coordinates[0][1];
+				// const x = c.geometry.coordinates[0].join(' ') + ' ' + c.geometry.coordinates[0][0];
+				const x = coords.join(' ') + ' ' + coords[0];
+				// console.log("x:", x);
+				return x;
+				// return c;
+            })
+    );
+
 	const circles = $derived.by(() => {
 		if (bounds === undefined) {
 			console.log('NO VIEW');
@@ -52,7 +105,8 @@
 </script>
 
 <svg class="opacity-25 pointer-events-none {user_class}" viewBox="{box}" preserveAspectRatio="none">
-	{#each circles as circle}
-		<circle cx="{circle.cx}" cy="{circle.cy}" r="{circle.r}" fill="green" />
+	{#each circles2 as circle}
+		<polygon points="{circle}" style="fill:green" />
+		<!-- <circle cx="{circle.cx}" cy="{circle.cy}" r="{circle.r}" fill="green" /> -->
 	{/each}
 </svg>
