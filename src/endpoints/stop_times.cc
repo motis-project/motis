4f90b0ab5c87ce707f73209f736ca78a19662027
Hanns Adrian Böhme <hannsadrian@gmail.com>
2025-05-29 22:01:47 +0200

    fix stoptimes radius filtering

--- src/endpoints/stop_times.cc
+++ src/endpoints/stop_times.cc
@@ -297,9 +297,9 @@ api::stoptimes_response stop_times::operator()(
   };
 
   if (query.radius_) {
-    loc_rtree_.in_radius(tt_.locations_.coordinates_[x],
-                         static_cast<double>(*query.radius_),
-                         [&](n::location_idx_t const y) { add(y); });
+    loc_rtree_.in_radius(
+        tt_.locations_.coordinates_[x], static_cast<double>(*query.radius_),
+        [&](n::location_idx_t const y) { locations.emplace_back(y); });
   } else {
     add(x);
   }
