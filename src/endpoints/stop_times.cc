f5c9cad0cb980b7393d51a2bc0a5f7947ae33d70
Hanns Adrian Böhme <hannsadrian@gmail.com>
2025-05-30 12:43:26 +0200

    introduce exact radius parameter

--- src/endpoints/stop_times.cc
+++ src/endpoints/stop_times.cc
@@ -297,9 +297,15 @@ api::stoptimes_response stop_times::operator()(
   };
 
   if (query.radius_) {
-    loc_rtree_.in_radius(
-        tt_.locations_.coordinates_[x], static_cast<double>(*query.radius_),
-        [&](n::location_idx_t const y) { locations.emplace_back(y); });
+    loc_rtree_.in_radius(tt_.locations_.coordinates_[x],
+                         static_cast<double>(*query.radius_),
+                         [&](n::location_idx_t const y) {
+                           if (query.exactRadius_ == true) {
+                             locations.emplace_back(y);
+                           } else {
+                             add(y);
+                           }
+                         });
   } else {
     add(x);
   }
