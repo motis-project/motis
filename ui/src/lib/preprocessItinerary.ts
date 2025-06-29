2db3fd8f6d16a98b8f999256c557038f94d79920
Felix Gündling <felix.guendling@gmail.com>
2025-06-29 15:42:44 +0200

    wip

--- ui/src/lib/preprocessItinerary.ts
+++ ui/src/lib/preprocessItinerary.ts
@@ -15,7 +15,7 @@ export const preprocessItinerary = (from: Location, to: Location) => {
 			if (it.legs[i].interlineWithPreviousLeg) {
 				const pred = joinedLegs[joinedLegs.length - 1];
 				const curr = it.legs[i];
-				pred.intermediateStops!.push({ ...pred.to, switchTo: curr.routeShortName } as Place);
+				pred.intermediateStops!.push({ ...pred.to, switchTo: curr } as Place);
 				pred.to = curr.to;
 				pred.duration += curr.duration;
 				pred.endTime = curr.endTime;
