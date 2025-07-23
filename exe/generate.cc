2bfa767ff45a5a2cf942625b697057c2f209544c
mority <mority@posteo.net>
2025-07-23 13:20:14 +0200

    date range option

--- exe/generate.cc
+++ exe/generate.cc
@@ -1,27 +1,19 @@
+#include "boost/format/format_fwd.hpp"
+
 #include "conf/configuration.h"
 
 #include <fstream>
 #include <iostream>
-#include <mutex>
 #include <random>
 #include <ranges>
 
-#include "boost/json/parse.hpp"
 #include "boost/json/serialize.hpp"
-#include "boost/json/value_from.hpp"
-#include "boost/json/value_to.hpp"
+#include "boost/timer/timer.hpp"
 #include "boost/url/url.hpp"
 
 #include "fmt/std.h"
 
 #include "utl/enumerate.h"
-#include "utl/get_or_create.h"
-#include "utl/helpers/algorithm.h"
-#include "utl/init_from.h"
-#include "utl/parallel_for.h"
-#include "utl/parser/cstr.h"
-#include "utl/sorted_diff.h"
-#include "utl/timing.h"
 
 #include "nigiri/routing/query.h"
 #include "nigiri/timetable.h"
@@ -82,11 +74,34 @@ n::location_idx_t random_stop(n::timetable const& tt,
 int generate(int ac, char** av) {
   auto data_path = fs::path{"data"};
   auto n = 100U;
+  auto first_day = std::optional<date::sys_days>{};
+  auto last_day = std::optional<date::sys_days>{};
+
+  auto const parse_date = [](std::string_view const s) {
+    std::stringstream in;
+    in.exceptions(std::ios::badbit | std::ios::failbit);
+    in << s;
+    auto d = date::sys_days{};
+    in >> date::parse("%Y-%m-%d", d);
+    return d;
+  };
+
+  auto const set_first_day = [&](std::string_view const s) {
+    first_day = parse_date(s);
+  };
+
+  auto const set_last_day = [&](std::string_view const s) {
+    last_day = parse_date(s);
+  };
 
   auto desc = po::options_description{"Options"};
   desc.add_options()  //
       ("help", "Prints this help message")  //
-      ("n,n", po::value(&n)->default_value(n), "number of queries");
+      ("n,n", po::value(&n)->default_value(n), "number of queries")  //
+      ("first_day", po::value<std::string>()->notifier(set_first_day),
+       "first day of query generation, format: YYYY-MM-DD")  //
+      ("last_day", po::value<std::string>()->notifier(set_last_day),
+       "last day of query generation, format: YYYY-MM-DD");
   add_data_path_opt(desc, data_path);
   auto vm = parse_opt(ac, av, desc);
 
@@ -100,6 +115,21 @@ int generate(int ac, char** av) {
 
   auto d = data{data_path, c};
   utl::verify(d.tt_, "timetable required");
+
+  first_day = first_day ? d.tt_->date_range_.clamp(*first_day)
+                        : d.tt_->date_range_.from_;
+  last_day = last_day ? d.tt_->date_range_.clamp(
+                            std::max(*first_day + date::days{1U}, *last_day))
+                      : d.tt_->date_range_.clamp(*first_day + date::days{14U});
+  if (*first_day == *last_day) {
+    fmt::println(
+        "can not generate queries: date range [{}, {}] has zero "
+        "length after clamping",
+        *first_day, *last_day);
+    return -1;
+  }
+  fmt::println("generating queries for date range [{}, {}]", *first_day,
+               *last_day);
 
   auto stops = std::vector<n::location_idx_t>{};
   stops.resize(d.tt_->n_locations());
@@ -109,246 +139,19 @@ int generate(int ac, char** av) {
 
   {
     auto out = std::ofstream{"queries.txt"};
-    auto const last_day = std::min(
-        14U, static_cast<unsigned>(d.tt_->date_range_.size().count()) - 1U);
     for (auto i = 0U; i != n; ++i) {
       auto p = api::plan_params{};
       using namespace std::chrono_literals;
       p.fromPlace_ = d.tags_->id(*d.tt_, random_stop(*d.tt_, stops));
       p.toPlace_ = d.tags_->id(*d.tt_, random_stop(*d.tt_, stops));
-      p.time_ = d.tt_->date_range_.from_ +
-                rand_in(0U, last_day) * date::days{1} + rand_in(6U, 18U) * 1h;
+      p.time_ = *first_day +
+                rand_in(0U, static_cast<std::uint32_t>(
+                                (*last_day - *first_day).count())) *
+                    date::days{1U} +
+                rand_in(6U, 18U) * 1h;
       out << p.to_url("/api/v1/plan") << "\n";
     }
   }
-
-  return 0;
-}
-
-int batch(int ac, char** av) {
-  auto data_path = fs::path{"data"};
-  auto queries_path = fs::path{"queries.txt"};
-  auto responses_path = fs::path{"responses.txt"};
-  auto mt = true;
-
-  auto desc = po::options_description{"Options"};
-  desc.add_options()  //
-      ("help", "Prints this help message")  //
-      ("multithreading,mt", po::value(&mt)->default_value(mt))  //
-      ("queries,q", po::value(&queries_path)->default_value(queries_path),
-       "queries file")  //
-      ("responses,r", po::value(&responses_path)->default_value(responses_path),
-       "response file");
-  add_data_path_opt(desc, data_path);
-
-  auto vm = parse_opt(ac, av, desc);
-  if (vm.count("help")) {
-    std::cout << desc << "\n";
-    return 0;
-  }
-
-  auto queries = std::vector<api::plan_params>{};
-  {
-    auto f = cista::mmap{queries_path.generic_string().c_str(),
-                         cista::mmap::protection::READ};
-    utl::for_each_token(utl::cstr{f.view()}, '\n', [&](utl::cstr s) {
-      queries.push_back(api::plan_params{boost::urls::url{s.view()}.params()});
-    });
-  }
-
-  auto const c = config::read(data_path / "config.yml");
-  utl::verify(c.timetable_.has_value(), "timetable required");
-
-  auto d = data{data_path, c};
-  utl::verify(d.tt_, "timetable required");
-
-  auto mtx = std::mutex{};
-  auto out = std::ofstream{responses_path};
-  auto total = std::atomic_uint64_t{};
-  auto const routing = utl::init_from<ep::routing>(d).value();
-  auto const compute_response = [&](std::size_t const id) {
-    UTL_START_TIMING(total);
-    auto response = routing(queries.at(id).to_url("/api/v1/plan"));
-    UTL_STOP_TIMING(total);
-
-    auto const timing = static_cast<std::uint64_t>(UTL_TIMING_MS(total));
-    response.debugOutput_.emplace("id", id);
-    response.debugOutput_.emplace("timing", timing);
-    {
-      auto const lock = std::scoped_lock{mtx};
-      out << json::serialize(json::value_from(response)) << "\n";
-    }
-    total += timing;
-  };
-
-  if (mt) {
-    utl::parallel_for_run(queries.size(), compute_response);
-  } else {
-    for (auto i = 0U; i != queries.size(); ++i) {
-      compute_response(i);
-    }
-  }
-
-  std::cout << "AVG: "
-            << (static_cast<double>(total) /
-                static_cast<double>(queries.size()))
-            << "\n";
-
-  return 0U;
-}
-
-int compare(int ac, char** av) {
-  auto queries_path = fs::path{"queries.txt"};
-  auto responses_paths = std::vector<std::string>{};
-  auto desc = po::options_description{"Options"};
-  desc.add_options()  //
-      ("help", "Prints this help message")  //
-      ("queries,q", po::value(&queries_path)->default_value(queries_path),
-       "queries file")  //
-      ("responses,r",
-       po::value(&responses_paths)
-           ->multitoken()
-           ->default_value(responses_paths),
-       "response files");
-
-  auto vm = parse_opt(ac, av, desc);
-  if (vm.count("help")) {
-    std::cout << desc << "\n";
-    return 0;
-  }
-
-  auto const open_file = [](fs::path const& p) {
-    auto f = std::ifstream{};
-    f.exceptions(std::ios_base::failbit | std::ios_base::badbit);
-    try {
-      f.open(p);
-    } catch (std::exception const& e) {
-      throw utl::fail("could not open file \"{}\": {}", p, e.what());
-    }
-    return f;
-  };
-
-  auto const read_line = [](std::ifstream& f) -> std::optional<std::string> {
-    if (f.peek() == EOF || f.eof()) {
-      return std::nullopt;
-    }
-    std::string line;
-    std::getline(f, line);
-    return line;
-  };
-
-  struct info {
-    unsigned id_;
-    std::optional<api::plan_params> params_{};
-    std::vector<std::optional<api::plan_response>> responses_{};
-  };
-  auto response_buf = hash_map<unsigned, info>{};
-  auto const get = [&](unsigned const id) -> info& {
-    return utl::get_or_create(response_buf, id, [&]() {
-      auto x = info{.id_ = id};
-      x.responses_.resize(responses_paths.size());
-      return x;
-    });
-  };
-  auto const is_finished = [](info const& x) {
-    return x.params_.has_value() &&
-           utl::all_of(x.responses_, [](auto&& r) { return r.has_value(); });
-  };
-  auto const params = [](api::Itinerary const& x) {
-    return std::tie(x.startTime_, x.endTime_, x.transfers_);
-  };
-  auto const print_params = [](api::Itinerary const& x) {
-    std::cout << x.startTime_ << ", " << x.endTime_
-              << ", transfers=" << x.transfers_;
-  };
-  auto const print_none = []() { std::cout << "\t\t\t\t\t\t"; };
-  auto n_equal = 0U;
-  auto const print_differences = [&](info const& x) {
-    auto const& ref = x.responses_[0].value().itineraries_;
-    for (auto i = 1U; i < x.responses_.size(); ++i) {
-      auto const uut = x.responses_[i].value().itineraries_;
-      if (std::ranges::equal(ref | std::views::transform(params),
-                             uut | std::views::transform(params))) {
-        ++n_equal;
-        continue;
-      }
-
-      std::cout << "QUERY=" << x.id_ << "\n";
-      utl::sorted_diff(
-          ref, uut,
-          [&](api::Itinerary const& a, api::Itinerary const& b) {
-            return params(a) < params(b);
-          },
-          [&](api::Itinerary const&, api::Itinerary const&) {
-            return false;  // always call for equal
-          },
-          utl::overloaded{
-              [&](utl::op op, api::Itinerary const& j) {
-                if (op == utl::op::kAdd) {
-                  print_none();
-                  std::cout << "\t\t\t";
-                  print_params(j);
-                  std::cout << "\n";
-                } else {
-                  print_params(j);
-                  std::cout << "\t\t\t";
-                  print_none();
-                  std::cout << "\n";
-                }
-              },
-              [&](api::Itinerary const& a, api::Itinerary const& b) {
-                print_params(a);
-                std::cout << "\t\t\t";
-                print_params(b);
-                std::cout << "\n";
-              }});
-      std::cout << "\n\n";
-    }
-  };
-  auto n_consumed = 0U;
-  auto const consume_if_finished = [&](info const& x) {
-    if (!is_finished(x)) {
-      return;
-    }
-    print_differences(x);
-    response_buf.erase(x.id_);
-    ++n_consumed;
-  };
-
-  auto query_file = open_file(queries_path);
-  auto responses_files =
-      utl::to_vec(responses_paths, [&](auto&& p) { return open_file(p); });
-
-  auto query_id = 0U;
-  auto done = false;
-  while (!done) {
-    done = true;
-
-    if (auto const q = read_line(query_file); q.has_value()) {
-      auto& info = get(query_id++);
-      info.params_ = api::plan_params{boost::urls::url{*q}.params()};
-      consume_if_finished(info);
-      done = false;
-    }
-
-    for (auto const [i, res_file] : utl::enumerate(responses_files)) {
-      if (auto const r = read_line(res_file); r.has_value()) {
-        auto res =
-            boost::json::value_to<api::plan_response>(boost::json::parse(*r));
-        utl::sort(res.itineraries_,
-                  [&](auto&& a, auto&& b) { return params(a) < params(b); });
-        auto const id = res.debugOutput_.at("id");
-        auto& info = get(static_cast<unsigned>(id));
-        info.responses_[i] = std::move(res);
-        consume_if_finished(info);
-        done = false;
-      }
-    }
-  }
-
-  std::cout << "consumed: " << n_consumed << "\n";
-  std::cout << "buffered: " << response_buf.size() << "\n";
-  std::cout << "   equal: " << n_equal << "\n";
 
   return 0;
 }
