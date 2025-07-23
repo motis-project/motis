2e2773ad3e11d93ced3555c8118f2135407557b4
mority <mority@posteo.net>
2025-07-23 19:05:33 +0200

    intermodal queries

--- exe/generate.cc
+++ exe/generate.cc
@@ -8,12 +8,9 @@
 #include <ranges>
 
 #include "boost/json/serialize.hpp"
-#include "boost/timer/timer.hpp"
 #include "boost/url/url.hpp"
 
 #include "fmt/std.h"
-
-#include "utl/enumerate.h"
 
 #include "nigiri/routing/query.h"
 #include "nigiri/timetable.h"
@@ -22,7 +19,7 @@
 #include "motis/config.h"
 #include "motis/data.h"
 #include "motis/endpoints/routing.h"
-#include "motis/place.h"
+#include "motis/odm/bounds.h"
 #include "motis/point_rtree.h"
 #include "motis/tag_lookup.h"
 
@@ -76,6 +73,12 @@ int generate(int ac, char** av) {
   auto n = 100U;
   auto first_day = std::optional<date::sys_days>{};
   auto last_day = std::optional<date::sys_days>{};
+  auto modes = std::optional<std::vector<api::ModeEnum>>{};
+  auto max_dist = 800.0;  // m
+  auto use_walk = false;
+  auto use_bike = false;
+  auto use_car = false;
+  auto use_odm = false;
 
   auto const parse_date = [](std::string_view const s) {
     std::stringstream in;
@@ -86,22 +89,48 @@ int generate(int ac, char** av) {
     return d;
   };
 
-  auto const set_first_day = [&](std::string_view const s) {
+  auto const parse_first_day = [&](std::string_view const s) {
     first_day = parse_date(s);
   };
 
-  auto const set_last_day = [&](std::string_view const s) {
+  auto const parse_last_day = [&](std::string_view const s) {
     last_day = parse_date(s);
+  };
+
+  auto const parse_modes = [&](std::string_view const s) {
+    modes = std::vector<api::ModeEnum>{};
+    if (s.contains("WALK")) {
+      modes->emplace_back(api::ModeEnum::WALK);
+      use_walk = true;
+    }
+    if (s.contains("BIKE")) {
+      modes->emplace_back(api::ModeEnum::BIKE);
+      use_bike = true;
+    }
+    if (s.contains("CAR")) {
+      modes->emplace_back(api::ModeEnum::CAR);
+      use_car = true;
+    }
+    if (s.contains("ODM")) {
+      modes->emplace_back(api::ModeEnum::ODM);
+      use_odm = true;
+    }
   };
 
   auto desc = po::options_description{"Options"};
   desc.add_options()  //
       ("help", "Prints this help message")  //
       ("n,n", po::value(&n)->default_value(n), "number of queries")  //
-      ("first_day", po::value<std::string>()->notifier(set_first_day),
+      ("first_day", po::value<std::string>()->notifier(parse_first_day),
        "first day of query generation, format: YYYY-MM-DD")  //
-      ("last_day", po::value<std::string>()->notifier(set_last_day),
-       "last day of query generation, format: YYYY-MM-DD");
+      ("last_day", po::value<std::string>()->notifier(parse_last_day),
+       "last day of query generation, format: YYYY-MM-DD")  //
+      ("modes,m", po::value<std::string>()->notifier(parse_modes),
+       "comma-separated list of modes for first/last mile and direct (requires "
+       "street routing), supported: WALK, BIKE, CAR, ODM")  //
+      ("max_dist", po::value(&max_dist)->default_value(max_dist),
+       "maximum distance from a public transit stop in meters, only used for "
+       "intermodal queries");
   add_data_path_opt(desc, data_path);
   auto vm = parse_opt(ac, av, desc);
 
@@ -112,6 +141,8 @@ int generate(int ac, char** av) {
 
   auto const c = config::read(data_path / "config.yml");
   utl::verify(c.timetable_.has_value(), "timetable required");
+  utl::verify(!modes || c.use_street_routing(),
+              "intermodal requires street routing");
 
   auto d = data{data_path, c};
   utl::verify(d.tt_, "timetable required");
@@ -123,32 +154,83 @@ int generate(int ac, char** av) {
                       : d.tt_->date_range_.clamp(*first_day + date::days{14U});
   if (*first_day == *last_day) {
     fmt::println(
-        "can not generate queries: date range [{}, {}] has zero "
-        "length after clamping",
+        "can not generate queries: date range [{}, {}] has zero length after "
+        "clamping",
         *first_day, *last_day);
-    return -1;
+    return 1;
   }
-  fmt::println("generating queries for date range [{}, {}]", *first_day,
-               *last_day);
+  fmt::println("date range: [{}, {}]", *first_day, *last_day);
+
+  auto const use_odm_bounds = modes && use_odm && d.odm_bounds_ != nullptr;
+  auto node_rtree = point_rtree<osr::node_idx_t>{};
+  if (modes) {
+    if (modes->empty()) {
+      fmt::println(
+          "can not generate queries: provided modes option without valid "
+          "mode");
+      return 1;
+    }
+    std::cout << "modes:";
+    for (auto const m : *modes) {
+      std::cout << " " << m;
+    }
+    std::cout << "\n";
+
+    for (auto i = osr::node_idx_t{0U}; i < d.w_->n_nodes(); ++i) {
+      auto const& props = d.w_->r_->node_properties_[i];
+      if ((props.is_walk_accessible() && use_walk) ||
+          (props.is_bike_accessible() && use_bike) ||
+          (props.is_car_accessible() && (use_car || use_odm))) {
+        if (auto const& pos = d.w_->get_node_pos(i);
+            !use_odm_bounds || d.odm_bounds_->contains(pos)) {
+          node_rtree.add(pos, i);
+        }
+      }
+    }
+  }
 
   auto stops = std::vector<n::location_idx_t>{};
-  stops.resize(d.tt_->n_locations());
-  for (auto i = 0U; i != stops.size(); ++i) {
-    stops[i] = n::location_idx_t{i};
+  for (auto i = 0U; i != d.tt_->n_locations(); ++i) {
+    auto const l = n::location_idx_t{i};
+    if (use_odm_bounds &&
+        !d.odm_bounds_->contains(d.tt_->locations_.coordinates_[l])) {
+      continue;
+    }
+    stops.emplace_back(l);
   }
+
+  auto const random_place = [&]() {
+    if (!modes) {
+      return d.tags_->id(*d.tt_, random_stop(*d.tt_, stops));
+    }
+
+    auto nodes = std::vector<osr::node_idx_t>{};
+    do {
+      nodes = node_rtree.in_radius(
+          d.tt_->locations_.coordinates_[random_stop(*d.tt_, stops)], max_dist);
+    } while (nodes.empty());
+
+    auto pos = d.w_->get_node_pos(rand_in(nodes));
+    return fmt::format("{},{}", pos.lat(), pos.lng());
+  };
 
   {
     auto out = std::ofstream{"queries.txt"};
     for (auto i = 0U; i != n; ++i) {
       auto p = api::plan_params{};
       using namespace std::chrono_literals;
-      p.fromPlace_ = d.tags_->id(*d.tt_, random_stop(*d.tt_, stops));
-      p.toPlace_ = d.tags_->id(*d.tt_, random_stop(*d.tt_, stops));
+      p.fromPlace_ = random_place();
+      p.toPlace_ = random_place();
       p.time_ = *first_day +
                 rand_in(0U, static_cast<std::uint32_t>(
                                 (*last_day - *first_day).count())) *
                     date::days{1U} +
                 rand_in(6U, 18U) * 1h;
+      if (modes) {
+        p.directModes_ = *modes;
+        p.preTransitModes_ = *modes;
+        p.postTransitModes_ = *modes;
+      }
       out << p.to_url("/api/v1/plan") << "\n";
     }
   }
