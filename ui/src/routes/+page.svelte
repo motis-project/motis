5a9c8eb403968c853d81bcddf877a1acc0ebf7ca
Felix Gündling <felix.guendling@gmail.com>
2025-07-28 10:59:36 +0200

    Revert "ui: always use coordinates"
    This reverts commit 14a4317c293c7c476b39d0d641e39be4a16c61ea.

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -218,7 +218,9 @@
 	});
 
 	const toPlaceString = (l: Location) => {
-		if (l.match?.level) {
+		if (l.match?.type === 'STOP') {
+			return l.match.id;
+		} else if (l.match?.level) {
 			return `${lngLatToStr(l.match!)},${l.match.level}`;
 		} else {
 			return `${lngLatToStr(l.match!)}`;
