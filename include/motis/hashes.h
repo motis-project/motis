0dcab6c6ab34a3eb4a0cabd1576335b0cc494bb7
Felix Gündling <felix.guendling@gmail.com>
2025-09-19 09:55:36 +0200

    update nigiri (fares: check presence of rule_priority column)

--- include/motis/hashes.h
+++ include/motis/hashes.h
@@ -17,7 +17,7 @@ constexpr auto const adr_version = []() {
   return meta_entry_t{"adr_bin_ver", 8U};
 };
 constexpr auto const n_version = []() {
-  return meta_entry_t{"nigiri_bin_ver", 22U};
+  return meta_entry_t{"nigiri_bin_ver", 23U};
 };
 constexpr auto const tbd_version = []() {
   return meta_entry_t{"tbd_bin_ver", 1U};
