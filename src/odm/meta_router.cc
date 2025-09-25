5de6837349c46ac9bba98739de39a34699e114bc
mority <mority@posteo.net>
2025-09-25 15:50:43 +0200

    erase_duplicates requires total order

--- src/odm/meta_router.cc
+++ src/odm/meta_router.cc
@@ -651,10 +651,7 @@ api::plan_response meta_router::run() {
   shorten(p_->odm_journeys_, p_->from_rides_, p_->to_rides_, *tt_, rtt_,
           query_);
   utl::erase_duplicates(
-      p_->odm_journeys_,
-      [](auto const& a, auto const& b) {
-        return a.start_time_ < b.start_time_;
-      },
+      p_->odm_journeys_, std::less<n::routing::journey>{},
       [](auto const& a, auto const& b) {
         return a == b &&
                odm_time(a.legs_.front()) == odm_time(b.legs_.front()) &&
