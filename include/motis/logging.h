0b6b94f9ebf0d49d856829d4ec0d9b652dd3067e
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-10-07 10:35:00 +0200

    Remove not public function declaration

--- include/motis/logging.h
+++ include/motis/logging.h
@@ -1,13 +1,10 @@
 #pragma once
 
 #include <string>
-#include <string_view>
 
 #include "motis/config.h"
 
 namespace motis {
-
-int set_log_level(std::string_view log_lvl);
 
 int set_log_level(config const&);
 
--- src/logging.cc
+++ src/logging.cc
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <string_view>
 
 #include "fmt/ostream.h"
 
