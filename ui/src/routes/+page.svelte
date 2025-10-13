e1b8641a63072295f38dc3fc50a4eb37dd0a00b2
Christian Schärf <schaerfo@users.noreply.github.com>
2025-10-14 00:04:04 +0200

    show names of intermediate stops (#1093)
    Co-authored-by: Christian Schärf <11158353+schaerfo@users.noreply.github.com>

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -645,7 +645,7 @@
 				</Control>
 				{#if showMap}
 					<ItineraryGeoJson itinerary={page.state.selectedItinerary} {level} />
-					<StopGeoJSON itinerary={page.state.selectedItinerary} />
+					<StopGeoJSON itinerary={page.state.selectedItinerary} {theme} />
 				{/if}
 			{/if}
 
