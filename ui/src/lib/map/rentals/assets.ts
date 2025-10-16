42a5daa9798b6b7a59c67d3c1cc7bc7d07333519
Pablo Hoch <pablo@melaxis.com>
2025-10-16 18:02:52 +0200

    colored rental icons on the map (WIP)

--- ui/src/lib/map/rentals/assets.ts
+++ ui/src/lib/map/rentals/assets.ts
@@ -3,7 +3,7 @@ import { t } from '$lib/i18n/translation';
 
 export type FormFactorAssets = {
 	svg: string;
-	station: string;
+	icon: string;
 	vehicle: string;
 	cluster: string;
 	label: string;
@@ -14,49 +14,49 @@ export const DEFAULT_FORM_FACTOR: RentalFormFactor = 'BICYCLE';
 export const formFactorAssets: Record<RentalFormFactor, FormFactorAssets> = {
 	BICYCLE: {
 		svg: 'bike',
-		station: 'bike_station',
+		icon: 'bike',
 		vehicle: 'floating_bike',
 		cluster: 'floating_bike_cluster',
 		label: t.RENTAL_BICYCLE
 	},
 	CARGO_BICYCLE: {
 		svg: 'cargo_bike',
-		station: 'cargo_bike_station',
+		icon: 'cargo_bike',
 		vehicle: 'floating_cargo_bike',
 		cluster: 'floating_cargo_bike_cluster',
 		label: t.RENTAL_CARGO_BICYCLE
 	},
 	CAR: {
 		svg: 'car',
-		station: 'car_station',
+		icon: 'car',
 		vehicle: 'floating_car',
 		cluster: 'floating_car_cluster',
 		label: t.RENTAL_CAR
 	},
 	MOPED: {
 		svg: 'moped',
-		station: 'moped_station',
+		icon: 'moped',
 		vehicle: 'floating_moped',
 		cluster: 'floating_moped_cluster',
 		label: t.RENTAL_MOPED
 	},
 	SCOOTER_SEATED: {
 		svg: 'scooter',
-		station: 'scooter_station',
+		icon: 'scooter',
 		vehicle: 'floating_scooter',
 		cluster: 'floating_scooter_cluster',
 		label: t.RENTAL_SCOOTER_SEATED
 	},
 	SCOOTER_STANDING: {
 		svg: 'scooter',
-		station: 'scooter_station',
+		icon: 'scooter',
 		vehicle: 'floating_scooter',
 		cluster: 'floating_scooter_cluster',
 		label: t.RENTAL_SCOOTER_STANDING
 	},
 	OTHER: {
 		svg: 'bike',
-		station: 'bike_station',
+		icon: 'bike',
 		vehicle: 'floating_bike',
 		cluster: 'floating_bike_cluster',
 		label: t.RENTAL_OTHER
