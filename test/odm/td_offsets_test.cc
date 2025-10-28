08df8c179965625dc090027bdb610351e9f732e0
mority <mority@posteo.net>
2025-10-28 17:20:32 +0100

    wip

--- test/odm/td_offsets_test.cc
+++ test/odm/td_offsets_test.cc
@@ -9,6 +9,17 @@ using namespace std::chrono_literals;
 
 namespace motis::odm {
 
+void print(td_offsets_t const& tdos) {
+  for (auto const& [l, tdo] : tdos) {
+    std::cout << "l: " << l << ":\n";
+    for (auto const& t : tdo) {
+      std::cout << "[valid_from_: " << t.valid_from_
+                << ", duration_: " << t.duration_
+                << ", transport_mode_id_: " << t.transport_mode_id_ << "]\n";
+    }
+  }
+}
+
 TEST(odm, get_td_offsets_basic) {
   auto const rides = std::vector<start>{{.time_at_start_ = unixtime_t{10h},
                                          .time_at_stop_ = unixtime_t{11h},
@@ -16,6 +27,8 @@ TEST(odm, get_td_offsets_basic) {
 
   auto const td_offsets =
       motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
 
   ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
   ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 2U);
@@ -48,6 +61,74 @@ TEST(odm, get_td_offsets_extension) {
   auto const td_offsets =
       motis::odm::get_td_offsets(rides, kOdmTransportModeId);
 
+  print(td_offsets);
+
+  ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
+  ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 2U);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].valid_from_, unixtime_t{10h});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].valid_from_,
+            unixtime_t{10h + 3min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].duration_,
+            footpath::kMaxDuration);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].transport_mode_id_,
+            kOdmTransportModeId);
+}
+
+TEST(odm, get_td_offsets_extension_reverse) {
+  auto const rides =
+      std::vector<start>{{.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{11h + 2min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 1min},
+                          .time_at_stop_ = unixtime_t{11h + 1min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h},
+                          .time_at_stop_ = unixtime_t{11h},
+                          .stop_ = location_idx_t{1U}}};
+
+  auto const td_offsets =
+      motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
+
+  ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
+  ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 2U);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].valid_from_, unixtime_t{10h});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].valid_from_,
+            unixtime_t{10h + 3min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].duration_,
+            footpath::kMaxDuration);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].transport_mode_id_,
+            kOdmTransportModeId);
+}
+
+TEST(odm, get_td_offsets_extension_fill_gap) {
+  auto const rides =
+      std::vector<start>{{.time_at_start_ = unixtime_t{10h},
+                          .time_at_stop_ = unixtime_t{11h},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{11h + 2min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 1min},
+                          .time_at_stop_ = unixtime_t{11h + 1min},
+                          .stop_ = location_idx_t{1U}}};
+
+  auto const td_offsets =
+      motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
+
   ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
   ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 2U);
 
@@ -77,6 +158,8 @@ TEST(odm, get_td_offsets_intermittent) {
 
   auto const td_offsets =
       motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
 
   ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
   ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 6U);
@@ -112,6 +195,141 @@ TEST(odm, get_td_offsets_intermittent) {
   EXPECT_EQ(td_offsets.at(location_idx_t{1U})[5].duration_,
             footpath::kMaxDuration);
   EXPECT_EQ(td_offsets.at(location_idx_t{1U})[5].transport_mode_id_,
+            kOdmTransportModeId);
+}
+
+TEST(odm, get_td_offsets_long_short_long) {
+  auto const rides =
+      std::vector<start>{{.time_at_start_ = unixtime_t{10h},
+                          .time_at_stop_ = unixtime_t{11h},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{11h + 2min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 1min},
+                          .time_at_stop_ = unixtime_t{10h + 31min},
+                          .stop_ = location_idx_t{1U}}};
+
+  auto const td_offsets =
+      motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
+
+  ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
+  ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 4U);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].valid_from_, unixtime_t{10h});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].valid_from_,
+            unixtime_t{10h + 1min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].duration_, 30min);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[2].valid_from_,
+            unixtime_t{10h + 2min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[2].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[2].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[3].valid_from_,
+            unixtime_t{10h + 3min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[3].duration_,
+            footpath::kMaxDuration);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[3].transport_mode_id_,
+            kOdmTransportModeId);
+}
+
+TEST(odm, get_td_offsets_late_improvement) {
+  auto const rides =
+      std::vector<start>{{.time_at_start_ = unixtime_t{10h},
+                          .time_at_stop_ = unixtime_t{11h},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 1min},
+                          .time_at_stop_ = unixtime_t{11h + 1min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{11h + 2min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 3min},
+                          .time_at_stop_ = unixtime_t{11h + 3min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{10h + 32min},
+                          .stop_ = location_idx_t{1U}}};
+
+  auto const td_offsets =
+      motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
+
+  ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
+  ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 4U);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].valid_from_, unixtime_t{10h});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].valid_from_,
+            unixtime_t{10h + 2min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].duration_, 30min);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[2].valid_from_,
+            unixtime_t{10h + 3min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[2].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[2].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[3].valid_from_,
+            unixtime_t{10h + 4min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[3].duration_,
+            footpath::kMaxDuration);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[3].transport_mode_id_,
+            kOdmTransportModeId);
+}
+
+TEST(odm, get_td_offsets_late_worse) {
+  auto const rides =
+      std::vector<start>{{.time_at_start_ = unixtime_t{10h},
+                          .time_at_stop_ = unixtime_t{11h},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 1min},
+                          .time_at_stop_ = unixtime_t{11h + 1min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{11h + 2min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 3min},
+                          .time_at_stop_ = unixtime_t{11h + 3min},
+                          .stop_ = location_idx_t{1U}},
+                         {.time_at_start_ = unixtime_t{10h + 2min},
+                          .time_at_stop_ = unixtime_t{12h + 2min},
+                          .stop_ = location_idx_t{1U}}};
+
+  auto const td_offsets =
+      motis::odm::get_td_offsets(rides, kOdmTransportModeId);
+
+  print(td_offsets);
+
+  ASSERT_TRUE(td_offsets.contains(location_idx_t{1U}));
+  ASSERT_EQ(td_offsets.at(location_idx_t{1U}).size(), 2U);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].valid_from_, unixtime_t{10h});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].duration_, 1h);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[0].transport_mode_id_,
+            kOdmTransportModeId);
+
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].valid_from_,
+            unixtime_t{10h + 4min});
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].duration_,
+            footpath::kMaxDuration);
+  EXPECT_EQ(td_offsets.at(location_idx_t{1U})[1].transport_mode_id_,
             kOdmTransportModeId);
 }
 
