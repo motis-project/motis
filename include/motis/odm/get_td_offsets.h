1a73b90b3310a7803b7c54f71e2cfdd42eb63805
mority <mority@posteo.net>
2025-10-28 13:41:54 +0100

    wip

--- include/motis/odm/get_td_offsets.h
+++ include/motis/odm/get_td_offsets.h
@@ -15,28 +15,16 @@ nigiri::routing::td_offsets_t get_td_offsets(
       [&](auto&& from_it, auto&& to_it) {
         td_offsets.emplace(from_it->stop_,
                            std::vector<nigiri::routing::td_offset>{});
+
         for (auto const& r : nigiri::it_range{from_it, to_it}) {
-          auto const dep = std::min(r.time_at_stop_, r.time_at_start_);
-          auto const dur = std::chrono::abs(r.time_at_stop_ - r.time_at_start_);
-          if (td_offsets.at(from_it->stop_).size() > 1) {
-            auto last = rbegin(td_offsets.at(from_it->stop_));
-            auto const second_last = std::next(last);
-            if (dep ==
-                std::clamp(dep, second_last->valid_from_, last->valid_from_)) {
-              // increase validity interval of last offset
-              last->valid_from_ = dep + dur;
-              continue;
-            }
+          auto const tdo = nigiri::routing::td_offset{.valid_from_ = std::min(r.time_at_stop_, r.time_at_start_), .duration_ = std::chrono::abs(r.time_at_stop_ - r.time_at_start_), .transport_mode_id_ = mode};
+          auto const i = std::lower_bound(begin(td_offsets.at(r.stop_)), end(td_offsets.at(r.stop_)), tdo, [](auto const& a, auto const& b) {
+            return a.valid_from_ < b.valid_from_;
+          });
+
+          if (i == end(td_offsets.at(r.stop_)) || ) {
+            td_offsets.at(r.stop_).insert(i,tdo);
           }
-          // add new offset
-          td_offsets.at(from_it->stop_)
-              .push_back({.valid_from_ = dep,
-                          .duration_ = dur,
-                          .transport_mode_id_ = mode});
-          td_offsets.at(from_it->stop_)
-              .push_back({.valid_from_ = dep + 1min,
-                          .duration_ = nigiri::footpath::kMaxDuration,
-                          .transport_mode_id_ = mode});
         }
       });
   return td_offsets;
