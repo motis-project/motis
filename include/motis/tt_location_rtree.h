b04984cce9bb83a3628a411e9176259f301b2c9f
Felix Gündling <felix.guendling@gmail.com>
2025-11-04 10:02:39 +0100

    bugfix: special stations don't need adr place assigned

--- include/motis/tt_location_rtree.h
+++ include/motis/tt_location_rtree.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "nigiri/special_stations.h"
 #include "nigiri/timetable.h"
 
 #include "motis/point_rtree.h"
@@ -9,7 +10,8 @@ namespace motis {
 inline point_rtree<nigiri::location_idx_t> create_location_rtree(
     nigiri::timetable const& tt) {
   auto t = point_rtree<nigiri::location_idx_t>{};
-  for (auto i = nigiri::location_idx_t{0U}; i != tt.n_locations(); ++i) {
+  for (auto i = nigiri::location_idx_t{nigiri::kNSpecialStations};
+       i != tt.n_locations(); ++i) {
     t.add(tt.locations_.coordinates_[i], i);
   }
   return t;
