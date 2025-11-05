75d1a32830dab1a4177d9cc331f406d18ac975f4
Traines <git@traines.eu>
2025-11-05 13:46:15 +0100

    http_req revert (#1141)
    * revert to http_req for rt update
    
    * keepmoved stuff in http_client

---
+++ include/motis/http_req.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <chrono>
+#include <map>
+#include <string>
+
+#include "boost/asio/awaitable.hpp"
+#include "boost/beast/http/dynamic_body.hpp"
+#include "boost/beast/http/message.hpp"
+#include "boost/url/url.hpp"
+
+namespace motis {
+
+using http_response =
+    boost::beast::http::response<boost::beast::http::dynamic_body>;
+
+boost::asio::awaitable<http_response> http_GET(
+    boost::urls::url,
+    std::map<std::string, std::string> const& headers,
+    std::chrono::seconds timeout);
+
+boost::asio::awaitable<http_response> http_POST(
+    boost::urls::url,
+    std::map<std::string, std::string> const& headers,
+    std::string const& body,
+    std::chrono::seconds timeout);
+
+std::string get_http_body(http_response const&);
+
+}  // namespace motis
