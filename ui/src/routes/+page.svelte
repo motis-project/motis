02510c8a51dc37db94a24dd1726457e65ceb6168
Felix Gündling <felix@Silvias-MacBook-Pro.local>
2025-11-06 11:27:04 +0100

    update nigiri (PONG respects restrictions), use PONG by default in UI

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -172,7 +172,7 @@
 	);
 	let arriveBy = $state<boolean>(urlParams?.get('arriveBy') == 'true');
 	let algorithm = $state<PlanData['query']['algorithm']>(
-		(urlParams?.get('algorithm') ?? 'RAPTOR') as PlanData['query']['algorithm']
+		(urlParams?.get('algorithm') ?? 'PONG') as PlanData['query']['algorithm']
 	);
 	let useRoutedTransfers = $state(
 		urlParams?.get('useRoutedTransfers') == 'true' || defaultQuery.useRoutedTransfers
