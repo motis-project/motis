68da911b3994d99e437b90ddfb00c02b20273db7
Traines <git@traines.eu>
2025-11-12 13:16:03 +0100

    add one_to_many post endpoint (#1164)
    * add one_to_many post endpoint
    
    * openapi liniting

--- src/endpoints/one_to_many_post.cc
+++ src/endpoints/one_to_many_post.cc
@@ -1,10 +1,11 @@
+#include "motis/endpoints/one_to_many_post.h"
+
 #include "motis/endpoints/one_to_many.h"
 
 namespace motis::ep {
 
-api::oneToMany_response one_to_many::operator()(
-    boost::urls::url_view const& url) const {
-  auto const query = api::oneToMany_params{url.params()};
+api::oneToMany_response one_to_many_post::operator()(
+    api::OneToManyParams const& query) const {
   return one_to_many_handle_request(query, w_, l_, elevations_);
 }
 
