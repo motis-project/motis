1077fd5389e7831e27063191e650a1a9d986bd5c
Felix Gündling <felix.guendling@gmail.com>
2025-12-16 18:50:51 +0100

    wip

--- src/journey_to_response.cc
+++ src/journey_to_response.cc
@@ -544,10 +544,10 @@ api::Itinerary journey_to_response(
                   leg.alerts_ = std::vector<api::Alert>{};
                 }
                 for (auto const& a : attributes) {
-                  leg.alerts_->push_back(
-                      api::Alert{.code_ = tt.attributes_[a].code_,
-                                 .headerText_ = std::string{tt.translate(
-                                     lang, tt.attributes_[a].text_)}});
+                  leg.alerts_->push_back(api::Alert{
+                      .code_ = std::string{tt.attributes_[a].code_.view()},
+                      .headerText_ = std::string{
+                          tt.translate(lang, tt.attributes_[a].text_)}});
                 }
 
                 leg.from_.vertexType_ = api::VertexTypeEnum::TRANSIT;
