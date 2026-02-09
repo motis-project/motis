ee3a52f2c7fb76154da513919ffdbb9faa793dc5
DIDONAX <didounimedamine@gmail.com>
2026-02-09 14:09:15 +0100

    Stoptimes fix (#1258)
    * fix loading circle
    
    * fix layout
    
    * fix
    
    * fix
    
    * fix

--- ui/src/lib/StopTimes.svelte
+++ ui/src/lib/StopTimes.svelte
@@ -68,135 +68,138 @@
 		});
 </script>
 
-<div class="gap-y-3 mb-1 text-base grid grid-cols-[auto_1fr] items-start content-start">
-	<div class="col-span-full w-full flex items-center justify-center">
-		<Button
-			class="font-bold"
-			variant="outline"
-			onclick={() => {
-				onClickStop(stopName, stopId, queryTime, !arriveBy);
-			}}
-		>
-			{#if arriveBy}
-				{t.switchToDepartures}
-			{:else}
-				{t.switchToArrivals}
-			{/if}
-		</Button>
-	</div>
-	{#each responses as r, rI (rI)}
-		{#await r}
-			<div class="flex items-center justify-center">
-				<LoaderCircle class="animate-spin w-12 h-12 m-20" />
+<div class="flex justify-center mb-4">
+	<Button
+		class="font-bold"
+		variant="outline"
+		onclick={() => {
+			onClickStop(stopName, stopId, queryTime, !arriveBy);
+		}}
+	>
+		{#if arriveBy}
+			{t.switchToDepartures}
+		{:else}
+			{t.switchToArrivals}
+		{/if}
+	</Button>
+</div>
+{#each responses as r, rI (rI)}
+	{#await r}
+		<div class="flex items-center justify-center">
+			<LoaderCircle
+				class="animate-spin w-20 h-20 my-60
+				"
+			/>
+		</div>
+	{:then r}
+		{#if rI === 0 && r.previousPageCursor.length}
+			<div class="col-span-full flex justify-center items-center border-b pb-4">
+				<button
+					onclick={() => {
+						responses.splice(
+							0,
+							0,
+							throwOnError(stoptimes({ query: { ...query, pageCursor: r.previousPageCursor } }))
+						);
+					}}
+					class="px-2 py-1 bg-blue-600 hover:!bg-blue-700 text-white font-bold text-sm border rounded-lg text-nowrap"
+				>
+					{t.earlier}
+				</button>
 			</div>
-		{:then r}
-			{#if rI === 0 && r.previousPageCursor.length}
-				<div class="col-span-full w-full flex justify-between items-center space-x-4">
-					<div class="border-t w-full h-0"></div>
-					<button
-						onclick={() => {
-							responses.splice(
-								0,
-								0,
-								throwOnError(stoptimes({ query: { ...query, pageCursor: r.previousPageCursor } }))
-							);
-						}}
-						class="px-2 py-1 bg-blue-600 hover:!bg-blue-700 text-white font-bold text-sm border rounded-lg text-nowrap"
-					>
-						{t.earlier}
-					</button>
-					<div class="border-t w-full h-0"></div>
+		{/if}
+		{#each r.stopTimes as stopTime, i (i)}
+			{@const timestamp = arriveBy ? stopTime.place.arrival! : stopTime.place.departure!}
+			{@const scheduledTimestamp = arriveBy
+				? stopTime.place.scheduledArrival!
+				: stopTime.place.scheduledDeparture!}
+
+			<div
+				class="gap-y-2 p-3 w-full text-base grid grid-cols-[auto_1fr] border-b duration-500 ease-out transition-all"
+			>
+				<div class="flex flex-col w-24">
+					<Time
+						variant="schedule"
+						timeZone={stopTime.place.tz}
+						isRealtime={stopTime.realTime}
+						{timestamp}
+						{scheduledTimestamp}
+						queriedTime={queryTime.toISOString()}
+						{arriveBy}
+					/>
+					<Time
+						variant="realtime"
+						timeZone={stopTime.place.tz}
+						isRealtime={stopTime.realTime}
+						{timestamp}
+						{scheduledTimestamp}
+						{arriveBy}
+					/>
 				</div>
-			{/if}
-			{#each r.stopTimes as stopTime, i (i)}
-				{@const timestamp = arriveBy ? stopTime.place.arrival! : stopTime.place.departure!}
-				{@const scheduledTimestamp = arriveBy
-					? stopTime.place.scheduledArrival!
-					: stopTime.place.scheduledDeparture!}
-				<div class="">
-					<div class="flex col justify-between">
-						<Route class="max-w-30 text-ellipsis overflow-hidden" l={stopTime} {onClickTrip} />
-						<div class="mx-4">
-							<Time
-								variant="schedule"
-								timeZone={stopTime.place.tz}
-								isRealtime={stopTime.realTime}
-								{timestamp}
-								{scheduledTimestamp}
-								queriedTime={queryTime.toISOString()}
-								{arriveBy}
-							/>
-							<Time
-								variant="realtime"
-								timeZone={stopTime.place.tz}
-								isRealtime={stopTime.realTime}
-								{timestamp}
-								{scheduledTimestamp}
-								{arriveBy}
-							/>
-						</div>
-					</div>
-				</div>
-				<div class="w-full">
-					<div class="flex items-start justify-between text-base">
-						<div class="flex items-start gap-1">
-							<ArrowRight class="mt-1 shrink-0 stroke-muted-foreground h-4 w-4" />
-							{stopTime.headsign}
-							{#if !stopTime.headsign || !stopTime.tripTo.name.startsWith(stopTime.headsign)}
-								({stopTime.tripTo.name})
-							{/if}
-						</div>
+				<div class="flex-col text-base">
+					<div class="flex justify-between">
+						<Route class="text-ellipsis mb-2" l={stopTime} {onClickTrip} />
 						{#if stopTime.place.track}
-							<span class="mt-1 text-nowrap px-1 border text-xs rounded-xl">
+							<span class="text-nowrap ml-3 h-fit px-1 text-base border font-semibold rounded-lg">
 								{getModeLabel(stopTime.mode) == 'Track' ? t.trackAbr : t.platformAbr}
 								{stopTime.place.track}
 							</span>
 						{/if}
-						<Alerts tz={stopTime.place.tz} alerts={stopTime.place.alerts} />
 					</div>
-					{#if stopTime.pickupDropoffType == 'NOT_ALLOWED'}
-						<div class="flex items-center text-destructive text-sm">
-							<CircleX class="stroke-destructive h-4 w-4" />
-							<span class="ml-1 leading-none">
-								{stopTime.tripCancelled
-									? t.tripCancelled
-									: stopTime.cancelled
-										? t.stopCancelled
-										: arriveBy
-											? t.outDisallowed
-											: t.inDisallowed}
+					<div class="flex items-center justify-between">
+						<div class="flex items-center gap-3">
+							<ArrowRight class="shrink-0 stroke-muted-foreground h-4 w-4" />
+							<span class="leading-none">
+								{stopTime.headsign}
+								{#if !stopTime.headsign}
+									{stopTime.tripTo.name}
+								{:else if !stopTime.tripTo.name.startsWith(stopTime.headsign)}
+									<span class="stroke-muted-foreground">({stopTime.tripTo.name})</span>
+								{/if}
 							</span>
 						</div>
-					{/if}
+						<Alerts tz={stopTime.place.tz} alerts={stopTime.place.alerts} />
+					</div>
 				</div>
-				<div class="border col-span-full"></div>
-			{/each}
-			{#if !r.stopTimes.length}
-				<div class="col-span-full w-full flex items-center justify-center">
-					<ErrorMessage message={t.noItinerariesFound} status={404} />
-				</div>
-			{/if}
-
-			{#if rI === responses.length - 1 && r.nextPageCursor.length}
-				<div class="col-span-full w-full flex justify-between items-center space-x-4">
-					<div class="border-t w-full h-0"></div>
-					<button
-						onclick={() => {
-							responses.push(
-								throwOnError(stoptimes({ query: { ...query, pageCursor: r.nextPageCursor } }))
-							);
-						}}
-						class="px-2 py-1 bg-blue-600 hover:!bg-blue-700 text-white text-sm font-bold border rounded-lg text-nowrap"
-					>
-						{t.later}
-					</button>
-					<div class="border-t w-full h-0"></div>
-				</div>
-			{/if}
-		{:catch e}
-			<div class="col-span-full w-full flex items-center justify-center">
-				<ErrorMessage message={e.error} status={e.status} />
+				{#if stopTime.pickupDropoffType == 'NOT_ALLOWED'}
+					<div class="flex ml-24 items-center col-span-full text-destructive text-sm">
+						<CircleX class="stroke-destructive h-4 w-4" />
+						<span class="ml-1 leading-none">
+							{stopTime.tripCancelled
+								? t.tripCancelled
+								: stopTime.cancelled
+									? t.stopCancelled
+									: arriveBy
+										? t.outDisallowed
+										: t.inDisallowed}
+						</span>
+					</div>
+				{/if}
 			</div>
-		{/await}
-	{/each}
-</div>
+		{/each}
+		{#if !r.stopTimes.length}
+			<div class="col-span-full w-full flex items-center justify-center">
+				<ErrorMessage message={t.noItinerariesFound} status={404} />
+			</div>
+		{/if}
+
+		{#if rI === responses.length - 1 && r.nextPageCursor.length}
+			<div class="col-span-full flex mt-4 justify-center items-center">
+				<button
+					onclick={() => {
+						responses.push(
+							throwOnError(stoptimes({ query: { ...query, pageCursor: r.nextPageCursor } }))
+						);
+					}}
+					class="px-2 py-1 bg-blue-600 hover:!bg-blue-700 text-white text-sm font-bold border rounded-lg text-nowrap"
+				>
+					{t.later}
+				</button>
+			</div>
+		{/if}
+	{:catch e}
+		<div class="col-span-full w-full flex items-center justify-center">
+			<ErrorMessage message={e.error} status={e.status} />
+		</div>
+	{/await}
+{/each}
