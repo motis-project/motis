ed65989f72ba51e26f375a7bf89e9f329d6b56f4
Traines <git@traines.eu>
2026-02-10 20:49:47 +0100

    get rid of pesky top bar (#1281)
    * get rid of pesky top bar
    
    * commit api schemas

--- ui/src/lib/map/Drawer.svelte
+++ ui/src/lib/map/Drawer.svelte
@@ -91,7 +91,7 @@
 <div
 	bind:this={ref}
 	class={cn(
-		'bg-card text-card-foreground rounded-xl pb-5 h-full border shadow max-w-full transition-transform duration-200 ease-out',
+		'bg-card text-card-foreground rounded-xl pb-3 h-full border shadow max-w-full transition-transform duration-200 ease-out',
 		className
 	)}
 	{...restProps}
