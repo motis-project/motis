08c1e00d804dfc0dac76e5bd5ba0cf3fef37bff9
Felix Gündling <felix.guendling@gmail.com>
2026-02-12 15:09:20 +0100

    formatting

--- src/rt_update.cc
+++ src/rt_update.cc
@@ -340,9 +340,8 @@ void run_rt_update(boost::asio::io_context& ioc, config const& c, data& d) {
             auto elevators = c.has_elevators() && c.get_elevators()->url_
                                  ? co_await update_elevators(c, d, *rtt)
                                  : std::move(d.rt_->e_);
-            auto new_rt = std::make_shared<rt>(std::move(rtt),
-                                               std::move(elevators),
-                                               std::move(railviz_rt));
+            auto new_rt = std::make_shared<rt>(
+                std::move(rtt), std::move(elevators), std::move(railviz_rt));
             std::atomic_store(&d.rt_, std::move(new_rt));
           }
 
