139ff95f7c7fdb7380ce1c2c040e06e9b2f0cb67
Felix Gündling <felix.guendling@gmail.com>
2026-02-24 00:10:09 +0100

    update adr (new categories)
    https://wiki.openstreetmap.org/wiki/Key:place
      "country",
      "state",
      "region",
      "province",
      "district",
      "county",
      "subdistrict",
      "municipality",
      "city",
      "borough",
      "suburb",
      "quarter",
      "neighbourhood",
      "city_block",
      "plot",
      "town",
      "village",
      "hamlet",
      "isolated_dwelling",
      "farm",
      "allotments",
      "continent",
      "archipelago",
      "island",
      "islet",
      "square",
      "locality",
      "polder",
      "sea",
      "ocean",
      "extra"

--- include/motis/hashes.h
+++ include/motis/hashes.h
@@ -14,7 +14,7 @@ constexpr auto const osr_version = []() {
   return meta_entry_t{"osr_bin_ver", 26U};
 };
 constexpr auto const adr_version = []() {
-  return meta_entry_t{"adr_bin_ver", 13U};
+  return meta_entry_t{"adr_bin_ver", 14U};
 };
 constexpr auto const adr_ext_version = []() {
   return meta_entry_t{"adr_ext_bin_ver", 4U};
