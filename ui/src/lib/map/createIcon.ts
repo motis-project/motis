3c3de16b6b0262d79051d099f71b09b69005b05a
DIDONAX <didounimedamine@gmail.com>
2026-03-20 18:27:03 +0100

    format

--- ui/src/lib/map/createIcon.ts
+++ ui/src/lib/map/createIcon.ts
@@ -63,4 +63,3 @@ export const createStopIcon = (size: number) => {
 
 	return canvas;
 };
-
--- ui/src/lib/updateOverlay.ts
+++ ui/src/lib/updateOverlay.ts
@@ -2,7 +2,7 @@ import { IconLayer } from '@deck.gl/layers';
 import { MapboxOverlay } from '@deck.gl/mapbox';
 
 export const updateOverlayLayers = (l: IconLayer, layers: IconLayer[], overlay: MapboxOverlay) => {
-	const idx = layers.findIndex(layer => layer.id === l.id);
+	const idx = layers.findIndex((layer) => layer.id === l.id);
 	if (idx !== -1) {
 		layers[idx] = l;
 	} else {
