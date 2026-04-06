073f6f00a8c896d00a144573ba23b817a92d2aee
Felix Gündling <felixguendling@gmail.com>
2026-04-07 00:15:55 +0200

    wip

--- include/motis/import.h
+++ include/motis/import.h
@@ -8,6 +8,6 @@ namespace motis {
 
 void import(config const&,
             std::filesystem::path const& data_path,
-            std::optional<std::vector<std::string>> const& task_filter);
+            std::optional<std::vector<std::string>> const& task_filter = {});
 
 }  // namespace motis
