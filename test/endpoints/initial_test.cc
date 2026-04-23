3230f3747951dd5e74b9c22ea7d76383bc22bded
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2026-04-23 08:26:17 +0200

    Merge both tests

--- test/endpoints/initial_test.cc
+++ test/endpoints/initial_test.cc
@@ -6,7 +6,7 @@
 
 #include "../test_case.h"
 
-TEST(initial, motis_version) {
+TEST(initial, server_config) {
   // Pick any test case, preferably the smallest
   auto [d, _config] = get_test_case<test_case::FFM_one_to_many>();
   auto const version = "v1.2.3-test";
@@ -15,23 +15,14 @@ TEST(initial, motis_version) {
 
   auto const res = ep("");
 
-  EXPECT_EQ(version, res.serverConfig_.motisVersion_);
-}
-
-TEST(initial, server_config) {
-  auto [d, _config] = get_test_case<test_case::FFM_one_to_many>();
-  d.init_initial("initial-test");
-  auto const ep = utl::init_from<motis::ep::initial>(d).value();
-
-  auto const res = ep("");
-
   auto const& config = res.serverConfig_;
-  EXPECT_TRUE(config.hasRoutedTransfers_);
-  EXPECT_TRUE(config.hasStreetRouting_);
-  EXPECT_FALSE(config.hasElevation_);
-  EXPECT_FALSE(config.shapesDebugEnabled_);
+  EXPECT_EQ(version, config.motisVersion_);
   EXPECT_EQ(128.0, config.maxOneToManySize_);
   EXPECT_EQ(90.0, config.maxOneToAllTravelTimeLimit_);
   EXPECT_EQ(3600.0, config.maxPrePostTransitTimeLimit_);
   EXPECT_EQ(21600.0, config.maxDirectTimeLimit_);
+  EXPECT_TRUE(config.hasRoutedTransfers_);
+  EXPECT_TRUE(config.hasStreetRouting_);
+  EXPECT_FALSE(config.hasElevation_);
+  EXPECT_FALSE(config.shapesDebugEnabled_);
 }
