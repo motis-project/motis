5de243ad320cb1cb52d025b20944d41908dccdcb
Felix Gündling <felix.guendling@gmail.com>
2026-05-20 19:37:23 +0200

    wip

--- include/motis/motis_instance.h
+++ include/motis/motis_instance.h
@@ -130,13 +130,13 @@ struct motis_instance {
     GET<ep::one_to_all>("/api/v1/one-to-all", d);
     GET<ep::one_to_all>("/api/v6/one-to-all", d);
     GET<ep::one_to_many>("/api/v1/one-to-many", d);
-    GET<ep::refresh_itinerary>("/api/v5/refresh-itinerary", d);
+    GET<ep::refresh_itinerary>("/api/v6/refresh-itinerary", d);
     GET<ep::one_to_many_intermodal>("/api/experimental/one-to-many-intermodal",
                                     d);
     POST<ep::one_to_many_intermodal_post>(
         "/api/experimental/one-to-many-intermodal", d);
     POST<ep::one_to_many_post>("/api/v1/one-to-many", d);
-    POST<ep::refresh_itinerary_post>("/api/v5/refresh-itinerary", d);
+    POST<ep::refresh_itinerary_post>("/api/v6/refresh-itinerary", d);
 
     if (!c.requires_rt_timetable_updates()) {
       // Elevator updates are not compatible with RT-updates.
