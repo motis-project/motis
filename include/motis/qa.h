065a076395851bb2e9a93b528fa77370247d9b95
mority <mority@posteo.net>
2026-07-09 17:50:06 +0200

    wip

--- include/motis/qa.h
+++ include/motis/qa.h
@@ -1,3 +1,16 @@
 #pragma once
+#include <functional>
+#include <vector>
 
-namespace motis {}  // namespace motis
+#include "itinerary_id.h"
+
+namespace motis::qa {
+
+constexpr auto kMaxRating = std::numeric_limits<double>::max();
+constexpr auto kMinRating = std::numeric_limits<double>::min();
+
+double rate(std::vector<api::Itinerary> const&,
+            std::vector<api::Itinerary> const&,
+            std::vector<std::function<double(api::Itinerary const&)>> const&);
+
+}  // namespace motis::qa
--- src/qa.cc
+++ src/qa.cc
@@ -1,5 +1,43 @@
 #include "motis/qa.h"
 
-namespace motis {
+namespace motis::qa {
 
-} // namespace motis
+double set_improvement(
+    std::vector<api::Itinerary> const& ref,
+    std::vector<api::Itinerary> const& uut,
+    std::vector<std::function<double(api::Itinerary const&)>> const& criteria) {
+  static std::vector<std::reference_wrapper<api::Itinerary>> a;
+  static std::vector<std::reference_wrapper<api::Itinerary>> b;
+  a.clear();
+  b.clear();
+  a.emplace_back(ref.begin(), ref.end());
+  b.emplace_back(uut.begin(), uut.end());
+
+  auto impr = double{0.0};
+
+  while (!a.empty()) {
+    auto max_impr_a = kMinRating;
+    auto max_a
+  }
+}
+
+double rate(
+    std::vector<api::Itinerary> const& ref,
+    std::vector<api::Itinerary> const& uut,
+    std::vector<std::function<double(api::Itinerary const&)>> const& criteria) {
+  if (ref.empty() && uut.empty()) {
+    return double{0.0};
+  }
+  if (ref.empty()) {
+    return kMaxRating;
+  }
+  if (uut.empty()) {
+    return kMinRating;
+  }
+
+  auto const LR = set_improvement(ref, uut, criteria);
+  auto const RL = set_improvement(uut, ref, criteria);
+  return LR - RL;
+}
+
+}  // namespace motis::qa
