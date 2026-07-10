99b8237cc14b6f34f6d4adfb75e1dea2ef156902
mority <mority@posteo.net>
2026-07-10 17:19:05 +0200

    wip

--- src/qa.cc
+++ src/qa.cc
@@ -1,24 +1,94 @@
 #include "motis/qa.h"
 
+#include "absl/strings/str_format.h"
+
 namespace motis::qa {
 
-double set_improvement(
-    std::vector<api::Itinerary> const& ref,
-    std::vector<api::Itinerary> const& uut,
+constexpr auto p = double{30.0};
+constexpr auto q = double{0.1};
+
+double improvement(
+    api::Itinerary const& a,
+    api::Itinerary const& b,
     std::vector<std::function<double(api::Itinerary const&)>> const& criteria) {
-  static std::vector<std::reference_wrapper<api::Itinerary>> a;
-  static std::vector<std::reference_wrapper<api::Itinerary>> b;
-  a.clear();
-  b.clear();
-  a.emplace_back(ref.begin(), ref.end());
-  b.emplace_back(uut.begin(), uut.end());
+  auto dist = double{0.0};
+  auto impr = double{0.0};
+
+  for (auto const& criterion : criteria) {
+    auto const crit_dist = criterion(a) - criterion(b);
+    dist += std::pow(crit_dist, 2);
+    if (crit_dist < 0) {
+      impr += std::pow(crit_dist, 2);
+    }
+  }
+
+  dist = std::sqrt(dist);
+  impr = std::sqrt(impr);
+
+  if (impr == 0.0) {
+    return 0.0;
+  }
+
+  return std::log2(std::pow(impr, 2) / dist) *
+         (std::atan(p * (dist - q)) + std::numbers::pi / 2.0);
+}
+
+double min_improvement(
+    api::Itinerary const* i,
+    std::vector<api::Itinerary const*> const& js,
+    std::vector<std::function<double(api::Itinerary const&)>> const& criteria) {
+  auto min_impr = kMaxRating;
+
+  for (auto const j : js) {
+    auto const impr = improvement(*i, *j, criteria);
+    if (impr < min_impr) {
+      min_impr = impr;
+    }
+  }
+
+  return min_impr;
+}
+
+double set_improvement(
+    std::vector<api::Itinerary> const& a,
+    std::vector<api::Itinerary> const& b,
+    std::vector<std::function<double(api::Itinerary const&)>> const& criteria) {
+  static std::vector<api::Itinerary const*> ap;
+  static std::vector<api::Itinerary const*> bp;
+
+  auto const reset = [](std::vector<api::Itinerary> const& v,
+                        std::vector<api::Itinerary const*>& p) {
+    p.clear();
+    for (auto const& i : v) {
+      p.emplace_back(&i);
+    }
+  };
+
+  reset(a, ap);
+  reset(b, bp);
 
   auto impr = double{0.0};
 
-  while (!a.empty()) {
+  while (!ap.empty()) {
     auto max_impr_a = kMinRating;
-    auto max_a
+    auto max_a = 0U;
+
+    for (auto i = 0U; i != ap.size(); ++i) {
+      auto const min_impr = min_improvement(ap[i], bp, criteria);
+      if (min_impr > max_impr_a) {
+        max_impr_a = min_impr;
+        max_a = i;
+      }
+    }
+
+    impr += max_impr_a;
+
+    std::swap(ap[max_a], ap.back());
+    bp.emplace_back(ap.back());
+    ap.pop_back();
   }
+
+  return impr;
 }
 
 double rate(
