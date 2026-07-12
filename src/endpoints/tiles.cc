cd4f64e3b87e50607d1e3665e805cd7a55478d42
Felix Gündling <felixguendling@gmail.com>
2026-07-13 01:20:44 +0200

    shortbread tiles experiment (#1523)
    * shortbread tiles experiment
    
    * wip
    
    * wip

--- src/endpoints/tiles.cc
+++ src/endpoints/tiles.cc
@@ -2,49 +2,16 @@
 
 #include <string>
 
-#include "net/web_server/url_decode.h"
-
 #include "tiles/get_tile.h"
 #include "tiles/parse_tile_url.h"
 #include "tiles/perf_counter.h"
 
 #include "motis/tiles_data.h"
 
-#include "pbf_sdf_fonts_res.h"
-
-using namespace std::string_view_literals;
-
 namespace motis::ep {
 
 net::reply tiles::operator()(net::route_request const& req, bool) const {
   auto const url = boost::url_view{req.target()};
-  if (url.path().starts_with("/tiles/glyphs")) {
-    std::string decoded;
-    net::url_decode(url.path(), decoded);
-
-    // Rewrite old font name "Noto Sans Display Regular" to "Noto Sans Regular".
-    constexpr auto kDisplay = " Display"sv;
-    auto res_name = decoded.substr(14);
-    if (auto const display_pos = res_name.find(kDisplay);
-        display_pos != std::string::npos) {
-      res_name.erase(display_pos, kDisplay.length());
-    }
-
-    try {
-      auto const mem = pbf_sdf_fonts_res::get_resource(res_name);
-      auto res = net::web_server::string_res_t{boost::beast::http::status::ok,
-                                               req.version()};
-      net::set_response_body(
-          res, req,
-          std::string_view{reinterpret_cast<char const*>(mem.ptr_), mem.size_});
-      res.insert(boost::beast::http::field::content_type,
-                 "application/x-protobuf");
-      res.keep_alive(req.keep_alive());
-      return res;
-    } catch (std::out_of_range const&) {
-      throw net::not_found_exception{res_name};
-    }
-  }
 
   if (req[boost::beast::http::field::accept_encoding].find("gzip") ==
       std::string_view::npos) {
